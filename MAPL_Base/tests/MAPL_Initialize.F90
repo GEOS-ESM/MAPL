bb1d526c0b3fea82288a6ba5c8df6c3ff4596b79
Weiyuan Jiang <weiyuan.jiang@nasa.gov>
2019-11-14 11:21:49 -0500

    fix for pfunit-4.0

--- MAPL_Base/tests/MAPL_Initialize.F90
+++ MAPL_Base/tests/MAPL_Initialize.F90
@@ -1,9 +1,12 @@
-subroutine MAPL_Initialize()
-   use ESMF
-   use MAPL_ThrowMod
-   use MAPL_pFUnit_ThrowMod
+module MAPL_pFUNIT_Initialize
+contains
+   subroutine Initialize()
+      use ESMF
+      use MAPL_ThrowMod
+      use MAPL_pFUnit_ThrowMod
    
-   call ESMF_Initialize(logKindFlag=ESMF_LOGKIND_NONE)
-   call MAPL_set_throw_method(throw)
+      call ESMF_Initialize(logKindFlag=ESMF_LOGKIND_NONE)
+      call MAPL_set_throw_method(throw)
 
-end subroutine MAPL_Initialize
+   end subroutine Initialize
+end module
