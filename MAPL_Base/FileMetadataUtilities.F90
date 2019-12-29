366633aa5d430bd8b9134a39fbb7a04bcf72cb88
Benjamin Auer <benjamin.m.auer@nasa.gov>
2019-12-29 10:35:41 -0500

    remove print

--- MAPL_Base/FileMetadataUtilities.F90
+++ MAPL_Base/FileMetadataUtilities.F90
@@ -131,7 +131,6 @@ module MAPL_FileMetadataUtilsMod
              min  = 0
              sec  = 0
            else
-             print *, 'ParseTimeUnits: Assuming a starting time of 00z'
              hour = 0
              min  = 0
              sec  = 0
