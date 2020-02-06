3fe0b2db113591cb51cce97a23eb22c7137e6bc1
Tom Clune <Thomas.L.Clune@nasa.gov>
2020-02-06 13:04:51 -0500

    Fixes #215
    Updating files and correcting directory structure so that the
    generator script at least works on discover.
    
    Really should add proper unit tests to this so that it can be
    efficiently refactored as we explor uses.

--- Python/MAPL_SpecsCodeGenerator.py
+++ Python/MAPL_SpecsCodeGenerator.py
@@ -1,6 +1,12 @@
 import argparse
-import DataSpec
-import DataSpecsReader
+import sys
+import os
+
+my_tool = os.path.dirname(os.path.abspath(__file__)) + '/src'
+sys.path.append(my_tool)
+
+import MAPL_DataSpec
+import reader
 
 # command line arguments
 parser = argparse.ArgumentParser(description='Generate import/export/internal specs for MAPL Gridded Component')
@@ -15,19 +21,14 @@ f_spec = open(args.declare_specs,'w')
 f_local = open(args.declare_local,'w') 
 f_get_pointer = open(args.get_pointer,'w') 
 
-specs = DataSpecsReader.read(args.input)
+specs = reader.read_specs(args.input)
 for category in ('IMPORT','EXPORT','INTERNAL'):
     for item in specs[category].to_dict('records'):
-        spec = DataSpec.DataSpec(category.capitalize(), item)
-        f_spec.write(spec.emit_declare_spec())
-        f_local.write(spec.emit_declare_local_variable())
+        spec = MAPL_DataSpec.MAPL_DataSpec(category.capitalize(), item)
+        f_spec.write(spec.emit_spec())
+        f_local.write(spec.emit_declare_local())
         f_get_pointer.write(spec.emit_get_pointer())
 
 f_spec.close()
 f_local.close()
 f_get_pointer.close()
-
-
-
-            
-            
