346cf225c2aed8b0b66339694e879eb4471784c0
Tom Clune <Thomas.L.Clune@nasa.gov>
2020-05-28 11:01:41 -0400

    Fixes #387
    The add2export column now correctly interprets T and F as logical values.

--- Apps/MAPL_GridCompSpecs_ACG.py
+++ Apps/MAPL_GridCompSpecs_ACG.py
@@ -16,14 +16,28 @@ class MAPL_DataSpec:
                    'refresh_interval', 'averaging_interval', 'halowidth',
                    'precision','default','restart', 'ungridded_dims',
                    'field_type', 'staggering', 'rotation',
-                   'friendlyto']
+                   'friendlyto', 'add2export']
 
     # The following arguments are skipped if value is empty string
     optional_options = [ 'dims', 'vlocation', 'num_subtiles',
                          'refresh_interval', 'averaging_interval', 'halowidth',
                          'precision','default','restart', 'ungridded_dims',
                          'field_type', 'staggering', 'rotation',
-                         'friendlyto']
+                         'friendlyto', 'add2export']
+
+    entry_aliases = {'dims': {'z'  : 'MAPL_DimsVertOnly',
+                              'xy' : 'MAPL_DimsHorzOnly',
+                              'xyz': 'MAPL_DimsHorzVert'},
+                     'vlocation': {'C': 'MAPL_VlocationCenter',
+                                   'E': 'MAPL_VlocationEdge',
+                                   'N': 'MAPL_VlocationNone'},
+                     'restart': {'OPT'  : 'MAPL_RestartOptional',
+                                 'SKIP' : 'MAPL_RestartSkip',
+                                 'REQ'  : 'MAPL_RestartRequired',
+                                 'BOOT' : 'MAPL_RestartBoot',
+                                 'SKIPI': 'MAPL_RestartSkipInitial'},
+                     'add2export': {'T': '.true.', 'F': '.false.'}
+                 }
 
     # The following options require quotes in generated code
     stringlike_options = ['short_name', 'long_name', 'units', 'friendlyto']
@@ -108,6 +122,12 @@ class MAPL_DataSpec:
                 value = "'" + value + "'"
             elif option in MAPL_DataSpec.arraylike_options:
                 value = '[' + value + ']' # convert to Fortran 1D array
+            else:
+                if (option in MAPL_DataSpec.entry_aliases) and (
+                        self.args[option] in MAPL_DataSpec.entry_aliases[option]):
+                    value = MAPL_DataSpec.entry_aliases[option][self.args[option]]
+                else:
+                    value = self.args[option]
             text = text + value + ", " + self.continue_line()
         return text
 
@@ -149,7 +169,8 @@ def read_specs(specs_filename):
         'COND'       : 'condition',
         'DEFAULT'    : 'default',
         'RESTART'    : 'restart',
-        'FRIENDLYTO' : 'friendlyto'
+        'FRIENDLYTO' : 'friendlyto',
+        'ADD2EXPORT' : 'add2export'
     }
 
     specs = {}
@@ -175,23 +196,10 @@ def read_specs(specs_filename):
             except StopIteration:
                 break
 
-    entry_aliases = {'z'     : 'MAPL_DimsVertOnly',
-                     'xy'    : 'MAPL_DimsHorzOnly',
-                     'xyz'   : 'MAPL_DimsHorzVert',
-                     'C'     : 'MAPL_VlocationCenter',
-                     'E'     : 'MAPL_VlocationEdge',
-                     'N'     : 'MAPL_VlocationNone',
-                     'OPT'   : 'MAPL_RestartOptional',
-                     'SKIP'  : 'MAPL_RestartSkip',
-                     'REQ'   : 'MAPL_RestartRequired',
-                     'BOOT'  : 'MAPL_RestartBoot',
-                     'SKIPI' : 'MAPL_RestartSkipInitial'
 
-    }
-
-    specs['IMPORT'].replace(entry_aliases,inplace=True)
-    specs['EXPORT'].replace(entry_aliases,inplace=True)
-    specs['INTERNAL'].replace(entry_aliases,inplace=True)
+#    specs['IMPORT'].replace(entry_aliases,inplace=True)
+#    specs['EXPORT'].replace(entry_aliases,inplace=True)
+#    specs['INTERNAL'].replace(entry_aliases,inplace=True)
 
     return specs
 
