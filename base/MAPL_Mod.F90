b30690dc1bf0567dcfed57c143dd223d5deea4bb
Raffaele Montuoro <raffaele.montuoro@noaa.gov>
2021-03-01 11:54:55 -0600

    Restore visibility of MAPL_GRID_INTERIOR symbol.

--- base/MAPL_Mod.F90
+++ base/MAPL_Mod.F90
@@ -5,6 +5,7 @@ module MAPL_Mod
   use ESMFL_Mod         !  Stopgap
   use MAPL_ExceptionHandling
   use MAPL_BaseMod
+  use MAPL_BaseMod, only: MAPL_GRID_INTERIOR
 ! For temporary backward compatibility after moving/renaming:
   use MAPL_BaseMod, only: ESMF_GRID_INTERIOR => MAPL_GRID_INTERIOR
   use MAPL_IOMod
