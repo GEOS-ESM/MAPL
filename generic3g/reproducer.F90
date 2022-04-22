7d88c896384042efd5428bb17fba840ea1f74531
Tom Clune <thomas.l.clune@nasa.gov>
2022-04-22 09:01:48 -0400

    whittling reproducer.

--- generic3g/reproducer.F90
+++ generic3g/reproducer.F90
@@ -1,187 +1,5 @@
-module r_mapl_FileSystemUtilities
-   implicit none
-   private
-
-   public :: get_file_extension
-   public :: get_file_basename
-
-contains
-
-   pure integer function find_extension_index(filename) result(dot_index)
-      character(len=*), intent(in) :: filename
-      dot_index = scan(trim(filename),'.', back=.true.)
-   end function find_extension_index
-
-   pure function get_file_extension(filename) result(extension)
-      ! Function that returns the extension of a string filename
-      ! where filename is "basename.extension"
-      character(len=*), intent(in) :: filename
-      character(len=:), allocatable :: extension
-      integer :: dot_index
-
-      dot_index = find_extension_index(trim(filename))
-      ! If the filename has no extension, return blank string
-      if (dot_index > 0) then
-         extension = trim(filename(dot_index:))
-      else
-         extension = ''
-      endif
-   end function get_file_extension
-
-   pure function get_file_basename(filename) result(basename)
-      ! Function that returns the basename of a string filename
-      ! where filename is "basename.extension"
-      character(len=*), intent(in) :: filename
-      character(len=:), allocatable :: basename
-      integer :: dot_index
-
-      dot_index = find_extension_index(trim(filename))
-      ! If the filename has no extension, return the filename
-      if (dot_index > 0) then
-         basename = trim(filename(1:dot_index-1))
-      else
-         basename = trim(filename)
-      end if
-   end function get_file_basename
-
-end module r_Mapl_FileSystemUtilities
-
-module r_mapl_DSO_Utilities
-   use r_mapl_FileSystemUtilities
-   implicit none
-
-   public :: is_valid_dso_name
-   public :: is_valid_dso_extension
-   public :: is_supported_dso_name
-   public :: is_supported_dso_extension
-   public :: adjust_dso_name
-
-   public :: SYSTEM_DSO_EXTENSION
-
-   ! NOTE: SYSTEM_DSO_SUFFIX is a preprocessor macro set by CMake
-   character(*), parameter :: SYSTEM_DSO_EXTENSION = '.dylib'
-
-contains
-
-   pure logical function is_valid_dso_name(name)
-      character(*), intent(in) :: name
-      is_valid_dso_name = is_valid_dso_extension(get_file_extension(name))
-   end function is_valid_dso_name
-
-   ! An empty extension is valid, as we can supply the system-specific one.
-   pure logical function is_valid_dso_extension(extension)
-      character(len=*), intent(in) :: extension
-      is_valid_dso_extension = (extension == '' .or. extension == SYSTEM_DSO_EXTENSION)
-   end function is_valid_dso_extension
-
-   ! We allow users to specify a DSO extensions that is only valid on
-   ! some other OS.  This allows things to work on say OSX if the user
-   ! puts a Linux DSO in a resource file.
-   pure logical function is_supported_dso_name(name)
-      character(len=*), intent(in) :: name
-      is_supported_dso_name = is_supported_dso_extension(get_file_extension(get_file_extension(name)))
-   end function is_supported_dso_name
-
-   ! We allow users to specify a DSO extensions that is only valid on
-   ! some other OS.  This allows things to work on say OSX if the user
-   ! puts a Linux DSO in a resource file.
-   pure logical function is_supported_dso_extension(extension)
-      character(len=*), intent(in) :: extension
-      character(len=6), dimension(*), parameter :: SUPPORTED_DSO_EXTENSIONS = [character(len=6) :: '.so','.dylib','.dll', '']
-      is_supported_dso_extension = any(extension == SUPPORTED_DSO_EXTENSIONS)
-   end function is_supported_dso_extension
-
-   ! We allow users to specify DSO file names with or without the
-   ! suffix.  This function creates the full name appropriate to a
-   ! given system.
-   pure function adjust_dso_name(guess)
-      character(:), allocatable :: adjust_dso_name
-      character(*), intent(in) :: guess
-
-      adjust_dso_name = get_file_basename(guess) // SYSTEM_DSO_EXTENSION
-
-  end function adjust_dso_name
-
-end module r_mapl_DSO_Utilities
-
-
-! The interfaces here are mandated by ESMF.  Unfortunately they do
-! actually provide a named Fortran interface to use.
-
-module r_mapl3g_ESMF_Interfaces
-   implicit none
-   private
-
-   public :: I_SetServices
-   public :: I_Run
-
-   public :: I_CplSetServices
-   public :: I_CplRun
-
-   abstract interface
-
-      subroutine I_SetServices(gridcomp, rc)
-         use ESMF, only: ESMF_GridComp
-         implicit none
-         type(ESMF_GridComp)  :: gridcomp
-         integer, intent(out) :: rc
-      end subroutine I_SetServices
-
-      subroutine I_Run(gridcomp, importState, exportState, clock, rc)
-         use :: esmf, only: ESMF_GridComp
-         use :: esmf, only: ESMF_State
-         use :: esmf, only: ESMF_Clock
-         implicit none
-         type(ESMF_GridComp)   :: gridcomp
-         type(ESMF_State)      :: importState
-         type(ESMF_State)      :: exportState
-         type(ESMF_Clock)      :: clock      
-         integer, intent(out)  :: rc         
-      end subroutine I_Run
-
-      subroutine I_CplSetServices(cplcomp, rc)
-         use ESMF, only: ESMF_CplComp
-         implicit none
-         type(ESMF_CplComp)   :: cplcomp
-         integer, intent(out) :: rc
-      end subroutine I_CplSetServices
-
-
-      subroutine I_CplRun(cplcomp, importState, exportState, clock, rc)
-         use :: esmf, only: ESMF_CplComp
-         use :: esmf, only: ESMF_State
-         use :: esmf, only: ESMF_Clock
-         implicit none
-         type(ESMF_CplComp)    :: cplcomp
-         type(ESMF_State)      :: importState
-         type(ESMF_State)      :: exportState
-         type(ESMF_Clock)      :: clock      
-         integer, intent(out)  :: rc         
-      end subroutine I_CplRun
-
-   end interface
-
-
-end module r_mapl3g_ESMF_Interfaces
-
-
-! This module provides a family of classes that encapsulate variant
-! methods of specifying/running SetServices on a user gridcomp.
-
-! Note that the subclasses (type extensions) are themselves private to
-! the module.  Client code is expected to use the overloaded factory
-! procedure user_setservices() and assign the result to an object of
-! the base class AbstractUserSetServices:
-!
-!    class(AbstractUserSetServices), allocatable :: ss
-!    ss = user_setservices(...)
-!
 
 module r_mapl3g_UserSetServices
-   use :: ESMF, only: ESMF_GridComp
-   use :: ESMF, only: ESMF_GridCompSetServices
-   use :: ESMF, only: ESMF_SUCCESS
-   use :: r_mapl3g_ESMF_Interfaces, only: I_SetServices
    implicit none
    private
 
@@ -198,27 +16,14 @@ module r_mapl3g_UserSetServices
 
    abstract interface
 
-      subroutine I_RunSetServices(this, gridcomp, rc)
-         use esmf, only: ESMF_GridComp
+      subroutine I_RunSetServices(this, rc)
          import AbstractUserSetServices
          class(AbstractUserSetServices), intent(in) :: this
-         type(ESMF_GridComp) :: gridcomp
          integer, intent(out) :: rc
       end subroutine I_RunSetServices
 
    end interface
 
-   ! Concrete subclass to encapsulate a traditional user setservices
-   ! consisting of a procuder conforming to the I_SetServices
-   ! interface.
-   type, extends(AbstractUserSetServices) :: ProcSetServices
-      procedure(I_SetServices), nopass, pointer :: userRoutine
-   contains
-      procedure :: run => run_proc_setservices
-   end type ProcSetServices
-
-   ! Concrete subclass to encapsulate a user setservices procedure
-   ! contained in a DSO.
    type, extends(AbstractUserSetServices) :: DSOSetServices
       character(:), allocatable :: sharedObj
       character(:), allocatable :: userRoutine
@@ -227,7 +32,6 @@ module r_mapl3g_UserSetServices
    end type DSOSetServices
 
    interface user_setservices
-      module procedure new_proc_setservices
       module procedure new_dso_setservices
    end interface user_setservices
 
@@ -244,32 +48,12 @@ contains
    !----------------------------------
    ! Direct procedure support
 
-   function new_proc_setservices(userRoutine) result(proc_setservices)
-      type(ProcSetServices) :: proc_setservices
-      procedure(I_SetServices) :: userRoutine
-
-      proc_setservices%userRoutine => userRoutine
-   end function new_proc_setservices
-
-   subroutine run_proc_setservices(this, gridcomp, rc)
-      class(ProcSetServices), intent(in) :: this
-      type(ESMF_GridComp) :: gridComp
-      integer, intent(out) :: rc
-
-      integer :: status, userRC
-
-!!$      call ESMF_GridCompSetServices(gridcomp, this%userRoutine, userRC=userRC, _RC)
-!!$      _VERIFY(userRC)
-!!$
-!!$      _RETURN(ESMF_SUCCESS)
-   end subroutine run_proc_setservices
 
    !----------------------------------
    ! DSO support
    
    ! Argument names correspond to ESMF arguments.
    function new_dso_setservices(sharedObj, userRoutine) result(dso_setservices)
-      use r_mapl_DSO_Utilities
       type(DSOSetServices) :: dso_setservices
       character(len=*), intent(in) :: sharedObj
       character(len=*), intent(in) :: userRoutine
@@ -279,23 +63,13 @@ contains
 
    end function new_dso_setservices
 
-   subroutine run_dso_setservices(this, gridcomp, rc)
-      use r_mapl_DSO_Utilities
+   subroutine run_dso_setservices(this, rc)
       class(DSOSetservices), intent(in) :: this
-      type(ESMF_GridComp) :: GridComp
       integer, intent(out) :: rc
 
       integer :: status, userRC
       logical :: found
 
-!!$      _ASSERT(is_supported_dso_name(this%sharedObj), 'unsupported dso name:: <'//this%sharedObj//'>')
-!!$      call ESMF_GridCompSetServices(gridcomp, sharedObj=adjust_dso_name(this%sharedObj), &
-!!$           userRoutine=this%userRoutine, userRoutinefound=found, userRC=userRC, rc=status)
-!!$
-!!$      _VERIFY(userRC)
-!!$      _VERIFY(rc)
-!!$
-!!$      _RETURN(ESMF_SUCCESS)
    end subroutine run_dso_setservices
 
 
@@ -310,13 +84,6 @@ contains
          class default
             equal = .false.
          end select
-      type is (ProcSetServices)
-         select type(b)
-         type is (ProcSetservices)
-            equal = equal_ProcSetServices(a,b)
-         class default
-            equal = .false.
-         end select
       class default
          equal = .false.
       end select
@@ -328,28 +95,16 @@ contains
       not_equal = .not. (a == b)
    end function not_equal_setServices
 
-   logical function equal_ProcSetServices(a, b) result(equal)
-      type(ProcSetServices), intent(in) :: a, b
-      equal = associated(a%userRoutine, b%userRoutine)
-   end function equal_ProcSetServices
-
    logical function equal_DSOSetServices(a, b) result(equal)
       type(DSOSetServices), intent(in) :: a, b
       
       equal = (a%sharedObj == b%sharedObj) .and. (a%userRoutine == b%userRoutine)
    end function equal_DSOSetServices
 
-   logical function not_equal_ProcSetServices(a, b) result(not_equal)
-      type(ProcSetServices), intent(in) :: a, b
-      not_equal = .not. (a == b)
-   end function not_equal_ProcSetServices
-
    logical function not_equal_DSOSetServices(a, b) result(not_equal)
       type(DSOSetServices), intent(in) :: a, b
       not_equal = .not. (a == b)
    end function not_equal_DSOSetServices
-   
-
    
 end module r_mapl3g_UserSetServices
 
@@ -2594,7 +2349,6 @@ contains
       type(ChildSpec) :: child_spec
 
       integer :: counter
-      counter = 0
       
       do counter = 1, 2
            select case(counter)
@@ -2635,6 +2389,3 @@ program main
 
 end program main
 
-   
-      
-   
