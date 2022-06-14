9d2a685fd94abd352faf6c52af766c63ca8cb17e
Benjamin Auer <benjamin.m.auer@nasa.gov>
2022-06-14 13:32:59 -0400

    get testing framework working

---
+++ Tests/ExtData_Testing_Framekwork/run_extdata1g.cmake
@@ -0,0 +1,25 @@
+macro(run_case CASE)
+    string(RANDOM LENGTH 24 tempdir)
+    execute_process(
+      COMMAND ${CMAKE_COMMAND} -E make_directory ${tempdir}
+      COMMAND ${CMAKE_COMMAND} -E copy_directory ${CMAKE_CURRENT_LIST_DIR}/test_cases/${CASE} ${tempdir}
+      )
+    if (EXISTS "${tempdir}/nproc.rc")
+      file(READ "${tempdir}/nproc.rc" num_procs)
+    else()
+      set(num_procs "1")
+    endif() 
+    execute_process(
+      COMMAND ${MPIEXEC_EXECUTABLE} ${MPIEXEC_NUMPROC_FLAG} ${num_procs} ${MY_BINARY_DIR}/ExtDataDriver.x
+      RESULT_VARIABLE CMD_RESULT
+      WORKING_DIRECTORY ${tempdir}
+      #COMMAND_ECHO STDOUT
+      )
+    execute_process(
+      COMMAND ${CMAKE_COMMAND} -E rm -rf ${tempdir}
+      )
+    if(CMD_RESULT)
+       message(FATAL_ERROR "Error running ${CASE}")
+    endif()
+endmacro()
+run_case(${TEST_CASE})
