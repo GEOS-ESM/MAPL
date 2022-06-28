49453354ceb4caaa8e63c09666a695e995cdb86e
Benjamin Auer <benjamin.m.auer@nasa.gov>
2022-06-28 15:54:02 -0400

    remove unneccessary lines, add 3D export to no child

--- tutorial/grid_comps/parent_with_no_children/ParentNoChildren_GridComp.F90
+++ tutorial/grid_comps/parent_with_no_children/ParentNoChildren_GridComp.F90
@@ -25,6 +25,10 @@ module ParentNoChild_GridComp
      call MAPL_AddExportSpec(gc,short_name='output1', long_name='NA',units='NA', &
                                  dims = MAPL_DimsHorzOnly, &
                                  vlocation = MAPL_VLocationNone, _RC)
+     call MAPL_AddExportSpec(gc,short_name='output2', long_name='NA',units='NA', &
+                                 dims = MAPL_DimsHorzVert, &
+                                 vlocation = MAPL_VLocationCenter, _RC)
+ 
  
 
      call MAPL_GenericSetServices(gc, _RC)
@@ -57,7 +61,7 @@ module ParentNoChild_GridComp
      type(ESMF_Clock), intent(inout) :: clock
      integer, intent(out), optional :: rc
 
-     real, pointer :: ptr_2d(:,:)
+     real, pointer :: ptr_2d(:,:), ptr_3d(:,:)
      type (MAPL_MetaComp),      pointer  :: MAPL
      real :: my_constant
      integer :: status
@@ -65,7 +69,9 @@ module ParentNoChild_GridComp
      call MAPL_GetObjectFromGC ( GC, MAPL, _RC)
      call MAPL_GetResource(MAPL, my_constant, Label="my_value:", default=17.0,_RC)
      call MAPL_GetPointer(export,ptr_2d,'output1',_RC)
-     ptr_2d = my_constant
+     if (associated(ptr_2d)) ptr_2d = my_constant
+     call MAPL_GetPointer(export,ptr_3d,'output2',_RC)
+     if (associated(ptr_3d)) ptr_3d = my_constant
 
      _RETURN(_SUCCESS)
 
