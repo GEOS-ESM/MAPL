41057ec5a0394430efb292c8f40d84d7f2488ed3
Matthew Thompson <matthew.thompson@nasa.gov>
2022-07-11 16:16:16 -0400

    Fix tutorial FLAPCLI

--- tutorial/driver_app/Example_Driver.F90
+++ tutorial/driver_app/Example_Driver.F90
@@ -8,13 +8,11 @@ program Example_Driver
    implicit none
 
    type (MAPL_Cap) :: cap
-   type (MAPL_FlapCLI) :: cli
    type (MAPL_CapOptions) :: cap_options
    integer :: status
 
-   cli = MAPL_FlapCLI(description = 'GEOS AGCM', &
-                                     authors     = 'GMAO')
-   cap_options = MAPL_CapOptions(cli)
+   cap_options = MAPL_FlapCLI(description = 'GEOS AGCM', &
+                                  authors = 'GMAO')
    cap = MAPL_Cap('example', cap_options = cap_options)
    call cap%run(_RC)
 
