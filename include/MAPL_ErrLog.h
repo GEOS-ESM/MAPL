8a4090fa1c26906e6898abc03d2ea75810417626
Tom Clune <thomas.l.clune@nasa.gov>
2022-10-05 08:58:16 -0400

    Update MAPL_ErrLog.h

--- include/MAPL_ErrLog.h
+++ include/MAPL_ErrLog.h
@@ -62,9 +62,11 @@
 #  ifdef I_AM_MAIN
 #    define __return call MAPL_abort()
 #    define __rc(rc)
+#    define _RETURN(A)  Do NOT use "_RETURN" macro in main program.
 #  else
 #    define __return return
 #    define __rc(rc) ,rc
+#    define _RETURN(A)     call MAPL_Return(A,_FILE_,__LINE__ __rc(rc)); __return
 #  endif
 
 #  ifdef ANSI_CPP
@@ -94,11 +96,6 @@
 #    elif defined(I_AM_PFUNIT)
 #       define _VERIFY(A)     call assert_that(A, is(0), SourceLocation(_FILE_,__LINE__));if(anyExceptions(this%context))return
 #    else
-#       if defined(I_AM_MAIN)
-#          define _RETURN(A)  Do NOT use "_RETURN" macro in main program.
-#       else
-#          define _RETURN(A)     call MAPL_Return(A,_FILE_,__LINE__ __rc(rc)); __return
-#       endif
 #       define _VERIFY(A)     if(MAPL_Verify(A,_FILE_,__LINE__ __rc(rc))) __return
 #    endif
 #    define _RC_(rc,status) rc=status);_VERIFY(status
