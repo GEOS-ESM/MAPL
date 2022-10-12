a62c12498fd252c463ed0a9f9417a932f29175c4
Tom Clune <thomas.l.clune@nasa.gov>
2022-10-12 16:37:37 -0400

    Child component methods now run by ESMF
    Previously this was done through the meta object which worked but
    violated the spirit of ESMF and possibly could cause errors in ESMF
    accessors down the road.

--- generic3g/ChildComponent_run_smod.F90
+++ generic3g/ChildComponent_run_smod.F90
@@ -3,6 +3,7 @@
 submodule(mapl3g_ChildComponent) ChildComponent_run_smod
    use :: mapl_ErrorHandling
    use :: mapl3g_OuterMetaComponent
+   use :: mapl3g_MethodPhasesMapUtils
    use :: mapl_KeywordEnforcer
    implicit none
 
@@ -17,14 +18,17 @@ contains
       character(len=*), optional, intent(in) :: phase_name
       integer, optional, intent(out) :: rc
 
-      integer :: status
+      integer :: status, userRC
+      integer :: phase
       type(OuterMetaComponent), pointer :: outer_meta
 
       outer_meta => get_outer_meta(this%gridcomp, _RC)
+      phase = get_phase_index(outer_meta%get_phases(ESMF_METHOD_RUN), phase_name=phase_name, _RC)
 
-      call outer_meta%run( &
-           importState=this%import_state, exportState=this%export_state, &
-           clock=clock, phase_name=phase_name, _RC)
+      call ESMF_GridCompRun(this%gridcomp, &
+           importState=this%import_state, exportState=this%export_state, clock=clock, &
+           phase=phase, userRC=userRC, _RC)
+      _VERIFY(userRC)
 
       _RETURN(_SUCCESS)
       _UNUSED_DUMMY(unusable)
@@ -45,15 +49,8 @@ contains
       type(OuterMetaComponent), pointer :: outer_meta
 
       outer_meta => get_outer_meta(this%gridcomp, _RC)
+      phase = get_phase_index(outer_meta%get_phases(ESMF_METHOD_INITIALIZE), phase_name=phase_name, _RC)
 
-      select case (phase_name)
-      case ('GENERIC_INIT_GRID')
-         phase = GENERIC_INIT_GRID
-      case ('DEFAULT')
-         phase = GENERIC_INIT_USER
-      case default
-         _FAIL('Unsupported initialize phase: <'//phase_name//'>')
-      end select
       call ESMF_GridCompInitialize(this%gridcomp, &
            importState=this%import_state, exportState=this%export_state, clock=clock, &
            phase=phase, userRC=userRC, _RC)
@@ -63,22 +60,26 @@ contains
       _UNUSED_DUMMY(unusable)
    end subroutine initialize_self
 
-   module subroutine finalize_self(this, clock, unusable, rc)
+   module subroutine finalize_self(this, clock, unusable, phase_name, rc)
       use mapl3g_OuterMetaComponent, only: get_outer_meta
       use mapl3g_OuterMetaComponent, only: OuterMetaComponent
       class(ChildComponent), intent(inout) :: this
       type(ESMF_Clock), intent(inout) :: clock
       class(KeywordEnforcer), optional, intent(in) :: unusable
+      character(len=*), optional, intent(in) :: phase_name
       integer, optional, intent(out) :: rc
 
-      integer :: status
+      integer :: status, userRC
+      integer :: phase
       type(OuterMetaComponent), pointer :: outer_meta
 
       outer_meta => get_outer_meta(this%gridcomp, _RC)
+      phase = get_phase_index(outer_meta%get_phases(ESMF_METHOD_FINALIZE), phase_name=phase_name, _RC)
 
-      call outer_meta%finalize( &
-           importState=this%import_state, exportState=this%export_state, &
-           clock=clock, _RC)
+      call ESMF_GridCompFinalize(this%gridcomp, &
+           importState=this%import_state, exportState=this%export_state, clock=clock, &
+           phase=phase, userRC=userRC, _RC)
+      _VERIFY(userRC)
 
       _RETURN(_SUCCESS)
       _UNUSED_DUMMY(unusable)
