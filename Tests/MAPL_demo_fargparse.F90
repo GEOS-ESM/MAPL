a218b32f2ed784339d74ede4eca8df39cfd2bce3
Matthew Thompson <matthew.thompson@nasa.gov>
2022-10-26 10:25:42 -0400

    Add example of extra arguments

--- Tests/MAPL_demo_fargparse.F90
+++ Tests/MAPL_demo_fargparse.F90
@@ -7,10 +7,9 @@
 #include "unused_dummy.H"
 
 program main
-      use, intrinsic :: iso_fortran_env, only: REAL32
       use MAPL
-      use ESMF
       use mpi
+      use fargparse
 
       implicit none
 
@@ -24,22 +23,27 @@ program main
 
    contains
 
-      subroutine run(rc)
-
 #undef I_AM_MAIN
 #include "MAPL_ErrLog.h"
+      subroutine run(rc)
+
          integer, intent(out), optional :: rc
 
          integer :: status
+         character(len=:), allocatable :: input_file
+         class(*), pointer :: option
+         type(MAPL_FargparseCLI) :: newcli
 
          call MPI_Init(status)
          _VERIFY(status)
 
          ! Read and parse the command line, and set parameters
-         cli = MAPL_FargparseCLI()
-         cap_options = MAPL_CapOptions(cli, rc=status)
-         write (*,*) "rc = ", status
-         _VERIFY(STATUS)
+         ! If you have extra options you make a procedure as seen below and add arguments
+         ! there and pass in here
+         cli = MAPL_FargparseCLI(extra=extra_options)
+
+         ! This does the casting of arguments into cap_options for CAP
+         cap_options = MAPL_CapOptions(cli, _RC)
 
          write(*,*) "done with MAPL_FargparseCLI"
          write(*,*) "  cap_options%with_esmf_moab = ", cap_options%with_esmf_moab
@@ -49,8 +53,30 @@ program main
          write(*,*) "  cap_options%nodes_output_server = ", cap_options%nodes_output_server
          write(*,*) "  cap_options%egress_file = ", cap_options%egress_file
 
+         ! For our extra options we have to explicitly cast them
+         call cast(cli%options%at('file'), input_file, _RC)
+
+         write(*,*) ""
+         write(*,*) "Extra arguments"
+         write(*,*) "  input file = ", input_file
+
          _RETURN(_SUCCESS)
 
       end subroutine run
+
+      subroutine extra_options(parser, rc)
+         type (ArgParser), intent(inout) :: parser
+         integer, intent(out), optional :: rc
+
+         call parser%add_argument('-f', '--file', &
+              help='A file to read', &
+              type='string', &
+              default='default.config', &
+              action='store')
+
+         !_RETURN(_SUCCESS)
+         if (present(rc)) rc = 0
+
+      end subroutine extra_options
 
 end program main
