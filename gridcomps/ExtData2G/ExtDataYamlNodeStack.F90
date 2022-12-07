3a352335d3d38e2fcd8931e6e198d84193da3665
Benjamin Auer <benjamin.m.auer@nasa.gov>
2022-12-07 11:46:37 -0500

    fix bug in previous commit

--- gridcomps/ExtData2G/ExtDataYamlNodeStack.F90
+++ gridcomps/ExtData2G/ExtDataYamlNodeStack.F90
@@ -3,10 +3,12 @@ module MAPL_ExtDataYamlNodeStack
    use MAPL_ExtDataYamlNodeWrapper
    implicit none
 
+   public :: max_file_depth 
    public :: stack_depth
    public :: yaml_node_stack
 
    integer, save :: stack_depth = 0
-   type(YamlNodeWrapper), save :: yaml_node_stack(20)
+   integer, parameter :: max_file_depth = 20
+   type(YamlNodeWrapper), save :: yaml_node_stack(max_file_depth)
 
 end module MAPL_ExtDataYamlNodeStack
