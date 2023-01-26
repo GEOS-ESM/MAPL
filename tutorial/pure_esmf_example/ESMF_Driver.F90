7e3b143879575dae85e091c9613abb98d8fab197
Benjamin Auer <benjaminauer@Benjamins-MacBook-Pro.local>
2023-01-25 20:39:16 -0500

    more clock

--- tutorial/pure_esmf_example/ESMF_Driver.F90
+++ tutorial/pure_esmf_example/ESMF_Driver.F90
@@ -19,14 +19,38 @@ program Example_Driver
       integer, intent(out), optional :: rc
 
       integer :: status, start_hms, start_ymd
+      type(ESMF_Time) :: start_time, stop_time
+      type(ESMF_TimeInterval) :: time_step
+      integer :: year,month,day,hour,minute,second
 
       ! get some information from the ESMF config file so we can create
       ! a clock, we can't do anything without a clock
-      call ESMF_ConfigGetAttribute(driver_cf,value=start_hms,lable="Start_Time:",rc=status)
-      call ESMF_ConfigGetAttribute(driver_cf,value=start_ymd,lable="Start_Date:",rc=status)
+      call ESMF_ConfigGetAttribute(driver_cf,value=start_hms,label="Start_Time:",rc=status)
+      call ESMF_ConfigGetAttribute(driver_cf,value=start_ymd,label="Start_Date:",rc=status)
+      call unpack_triple(start_ymd,year,month,day)
+      call unpack_triple(start_hms,hour,minute,second)
+
+      call ESMF_TimeSet(start_time,yy=year,mm=month,dd=day,h=hour,m=minute,s=second,rc=status)
+
+      call ESMF_ConfigGetAttribute(driver_cf,value=start_hms,label="End_Time:",rc=status)
+      call ESMF_ConfigGetAttribute(driver_cf,value=start_ymd,label="End_Date:",rc=status)
+      call unpack_triple(end_ymd,year,month,day)
+      call unpack_triple(end_hms,hour,minute,second)
+      call ESMF_TimeSet(end_time,yy=year,mm=month,dd=day,h=hour,m=minute,s=second,rc=status)
 
    end function
 
+   !
+   subroutine unpack_triple(packed_input, i1, i2, i3)
+      integer, intent(in) :: packed_input
+      integer, intent(out) :: i1,i2,i3
+
+      i1 = packed_input/10000
+      i2 = mod(packed_input/100,100)
+      i3 = mod(packed_input,100)
+
+   end subroutine
+     
    subroutine abort_program()
      integer :: status, mpi_stat
 
