447a6c3be1ab955b3cb86bc8eff66a651c7e52e4
Darian Boggs <william.d.boggs@nasa.gov>
2023-02-22 00:12:41 -0500

    Update to print array resources

--- base/MAPL_Resource.F90
+++ base/MAPL_Resource.F90
@@ -220,7 +220,7 @@ contains
          else
             label_to_print = trim(label)
          end if
-         call print_resource(printrc, label_to_print, val, default=default, _RC)
+         call print_resource_scalar(printrc, label_to_print, val, default=default, _RC)
       end if
 
       _RETURN(ESMF_SUCCESS)
@@ -284,7 +284,7 @@ contains
    ! Print the resource value according to the value of printrc
    ! printrc = 0 - Only print non-default values
    ! printrc = 1 - Print all values
-   subroutine print_resource(printrc, label, val, default, rc)
+   subroutine print_resource_scalar(printrc, label, val, default, rc)
       integer, intent(in) :: printrc
       character(len=*), intent(in) :: label
       class(*), intent(in) :: val
@@ -331,7 +331,7 @@ contains
 
       _RETURN(_SUCCESS)
 
-   end subroutine print_resource
+   end subroutine print_resource_scalar
 
    ! Check if vector contains string
    logical function vector_contains_str(vector, string)
@@ -380,6 +380,226 @@ contains
       _RETURN(_SUCCESS)
 
    end function intrinsic_to_string
+
+   subroutine print_resource_array(printrc, label, vals, default, rc)
+      integer, intent(in) :: printrc
+      character(len=*), intent(in) :: label
+      class(*), intent(in) :: vals(:)
+      class(*), optional, intent(in) :: default(:)
+      integer, optional, intent(out) :: rc
+
+      character(len=:), allocatable :: val_str, default_str, output_format
+      character(len=:), allocatable :: type_str, type_format
+      type(StringVector), pointer, save :: already_printed_labels => null()
+      integer :: i
+      character(len=2) :: size_vals_str
+      logical :: value_equals_default
+      character(len=:), allocatable :: default_tag
+
+      if (.not. associated(already_printed_labels)) then
+         allocate(already_printed_labels)
+      end if
+
+      ! Do not print label more than once
+      if (.not. vector_contains_str(already_printed_labels, trim(label))) then
+         call already_printed_labels%push_back(trim(label))
+      else
+         return
+      end if
+
+      ! Cast the size(vals) to a string making sure to strip any spaces
+      ! We assume we'll never have more than 99 values
+      write(size_vals_str, '(i2)') size(vals)
+
+      select type(vals)
+      type is(integer(int32))
+         type_str = "'Integer*4 '"
+         type_format = '('//trim(size_vals_str)//'(i0.1,1X))'
+         val_str = intrinsic_array_to_string(vals, type_format)
+         if (present(default)) then
+            default_str = intrinsic_array_to_string(default, type_format)
+         end if
+      type is(integer(int64))
+         type_str = "'Integer*8 '"
+         type_format = '('//trim(size_vals_str)//'(i0.1,1X))'
+         val_str = intrinsic_array_to_string(vals, type_format)
+         if (present(default)) then
+            default_str = intrinsic_array_to_string(default, type_format)
+         end if
+      type is(real(real32))
+         type_str = "'Real*4 '"
+         type_format = '('//trim(size_vals_str)//'(f0.6,1X))'
+         val_str = intrinsic_array_to_string(vals, type_format)
+         if (present(default)) then
+            default_str = intrinsic_array_to_string(default, type_format)
+         end if
+      type is(real(real64))
+         type_str = "'Real*8 '"
+         type_format = '('//trim(size_vals_str)//'(f0.6,1X))'
+         val_str = intrinsic_array_to_string(vals, type_format)
+         if (present(default)) then
+            default_str = intrinsic_array_to_string(default, type_format)
+         end if
+       type is(logical)
+         type_str = "'Logical '"
+         type_format = '('//trim(size_vals_str)//'(l1,1X))'
+         val_str = intrinsic_array_to_string(vals, type_format)
+         if (present(default)) then
+            default_str = intrinsic_array_to_string(default, type_format)
+         end if
+      type is(character(len=*))
+         type_str = "'Character '"
+         do i = 1, size(vals)
+            val_str(i:i) = trim(vals(i))
+         end do
+         if (present(default)) then
+            default_str = intrinsic_array_to_string(default, 'a')
+         end if
+      class default
+         _FAIL("Unsupported type")
+      end select
+
+      if(allocated(default_str)) then
+         output_parameter(printrc, type_str, val_str, default_str)
+      else
+         output_parameter(printrc, type_str, val_str)
+      end if
+
+   end subroutine print_resource_array
+
+   function intrinsic_array_to_string(vals, str_format, rc) result(formatted_str)
+      class(*), intent(in) :: vals(:)
+      character(len=*), intent(in) :: str_format
+      character(len=1024) :: formatted_str
+      integer, optional, intent(out) :: rc
+
+      integer :: i
+
+      select type(vals)
+      type is(integer(int32))
+         write(formatted_str, str_format) vals
+      type is(integer(int64))
+         write(formatted_str, str_format) vals
+      type is(real(real32))
+         write(formatted_str, str_format) vals
+      type is(real(real64))
+         write(formatted_str, str_format) vals
+      type is(logical)
+         write(formatted_str, str_format) vals
+      type is(character(len=*))
+         do i = 1, size(vals)
+            formatted_str(i:i) = vals(i)
+         end do
+      class default
+         _FAIL( "Unsupported type in intrinsic_array_to_string")
+      end select
+
+   end function intrinsic_array_to_string
+
+   subroutine output_parameter(printrc, type_str, val_str, default_str)
+      character(len=*), intent(in) :: type_str
+      character(len=*), intent(in) :: val_str
+      character(len=*), intent(in), optional :: default_str
+      integer, intent(in) :: printrc
+      character(len=:), allocatable :: output_format
+      character(len=*), parameter :: DEFAULT_ = ", (default value)"
+      character(len=*), parameter :: NOT_DEFAULT = ''
+      character(len=*), parameter :: NO_DEFAULT = ''
+
+      output_format = "(1x, " // type_str // ", 'Resource Parameter: '" // ", a" // ", a" // "a)"
+
+      ! printrc = 0 - Only print non-default values
+      ! printrc = 1 - Print all values with label if default value
+      if (present(default_str)) then
+         if trim(val_str) == trim(default_str) then
+            if printrc == 1 then
+               print output_format, trim(label), trim(val_str), trim(DEFAULT_)
+            end if
+         else
+            print output_format, trim(label), trim(val_str), trim(NOT_DEFAULT)
+         end if 
+      else
+         print output_format, trim(label), trim(val_str), trim(NO_DEFAULT)
+      end if
+   end subroutine output_parameter
+!!!!!!!!!!!!!!!!!!!
+   ! Find value of scalar variable in config
+   subroutine MAPL_GetResource_config_scalar(config, val, label, value_is_set, unusable, default, component_name, rc)
+      type(ESMF_Config), intent(inout) :: config
+      class(*), intent(inout) :: val
+      character(len=*), intent(in) :: label
+      logical , intent(out) :: value_is_set
+      class(KeywordEnforcer), optional, intent(in) :: unusable
+      class(*), optional, intent(in) :: default
+      character(len=*), optional, intent(in) :: component_name
+      integer, optional, intent(out) :: rc
+
+      integer :: status, printrc
+      logical :: default_is_present, label_is_present
+      logical :: do_print_value
+      character(len=:), allocatable :: label_to_print
+      character(len=:), allocatable :: actual_label
+
+      _UNUSED_DUMMY(unusable)
+
+      value_is_set = .FALSE.
+      do_print_value = .FALSE.
+
+      default_is_present = present(default)
+
+      if (default_is_present) then
+         _ASSERT(same_type_as(val, default), "Value and default must have same type")
+      end if
+
+      call get_actual_label(config, label, label_is_present, actual_label, component_name = component_name, _RC)
+
+      ! No default and not in config, error
+      ! label or default must be present
+      if (.not. (label_is_present .or. default_is_present)) then
+         value_is_set = .FALSE.
+         return
+      end if
+
+      ! Can set printrc to negative to not print at all
+      if (do_print_value .and. MAPL_AM_I_Root()) then
+         call ESMF_ConfigGetAttribute(config, printrc, label = 'PRINTRC:', default = 0, _RC)  
+         do_print_value = (printrc >= 0)
+      end if
+
+      if do_print_value then
+         if label_is_present then
+            label_to_print = actual_label
+         else
+            label_to_print = trim(label)
+         end if
+      end if
+
+      if label_is_present then
+         if default_is_present then
+            ! use config setter with value checker
+         else
+            ! use config setter without checker
+         end if
+      else
+         ! use default setter without checker
+      end if
+
+      select type(val)
+      SET_VAL(integer(int32), val)
+      SET_VAL(integer(int64), val)
+      SET_VAL(real(real32), val)
+      SET_VAL(real(real64), val)
+      SET_VAL(character(len=*), val)
+      SET_VAL(logical, val)
+      class default
+         _FAIL( "Unsupported type")
+      end select
+      
+      value_is_set = .TRUE.
+
+      _RETURN(ESMF_SUCCESS)
+
+   end subroutine MAPL_GetResource_config_scalar
 
 end module MAPL_ResourceMod
 
@@ -441,7 +661,7 @@ module HandlerMod
 contains
    
    subroutine get_string_int32(this) result(formatted_string)
-      class(Int32EResource), intent(in) :: this
+      class(Int32Resource), intent(in) :: this
       character(len=MAX_LEN) :: formatted_string
       character(len=*), parameter :: fstring='(i0.1)' 
       write(formatted_string, fstring) this%value
@@ -467,7 +687,7 @@ contains
    subroutine set_value_from config(value)
    end subroutine set_value_from config
 
-   subroutine print_resource_int32(PRINT_RESOURCE(value, integer(int32), '(i0.1)', type_string, formatted_string)
+   subroutine print_resource_int32(print_resource_scalar(value, integer(int32), '(i0.1)', type_string, formatted_string)
    end subroutine print_resource_int32
 
 ! derived type that accepts value/type, a setter, and a printer
@@ -492,4 +712,3 @@ contains
 !     printer: null
 !  end if
 end module HandlerMod
-
