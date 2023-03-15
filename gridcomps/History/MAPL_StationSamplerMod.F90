f693eccf763ebb72e57337315aabbe83655afbab
Yonggang Yu <yonggang.yu@colorado.edu>
2023-03-15 14:00:09 -0600

    Modification with Tom and Ben for output at history prescribed ESMF_Time

--- gridcomps/History/MAPL_StationSamplerMod.F90
+++ gridcomps/History/MAPL_StationSamplerMod.F90
@@ -1,12 +1,6 @@
-!  15-March-2023:
-!     time series output for station data
-!
-!
 !  Input Data:
 !_ 1. station_id_file: static
 !     plain text format: [id,name,lat,lon,elev]
-!_ 2. station_data_file: time series
-!     plain text format: [time, AOD..., id]
 
 #include "MAPL_Generic.h"
 module StationSamplerMod
@@ -30,15 +24,14 @@ module StationSamplerMod
 
   public :: StationSampler
   type :: StationSampler
-     type(LocStreamFactory) :: LSF   !? redundancy
+     type(LocStreamFactory) :: LSF
      type(ESMF_LocStream) :: esmf_ls 
      type(LocstreamRegridder) :: regridder
-     type(ESMF_time), allocatable :: times(:) ! nobs
-     integer, allocatable :: ids(:)         ! ...
+     type(ESMF_time), allocatable :: times(:)
+     integer, allocatable :: ids(:)
      integer :: nobs
      integer :: nstation
      integer, allocatable :: station_id(:)
-     integer, allocatable :: id2rank(:)     
      character(len=ESMF_MAXSTR), allocatable :: station_name(:)
      real(kind=REAL64), allocatable :: lons(:)
      real(kind=REAL64), allocatable :: lats(:)
@@ -70,13 +63,13 @@ module StationSamplerMod
      module procedure new_StationSampler_readfile
   end interface StationSampler
   integer :: maxstr = 2048  ! because ESMF_MAXSTR=256  
-  integer :: maxnstation = 10000 
 contains
 
-
-  function new_StationSampler_readfile (filename1,filename2,rc) result(sampler)
+  !-- remove file2
+  !
+  function new_StationSampler_readfile (filename1,filename2,rc) result(sampler)  
     type(StationSampler) :: sampler
-    character(len=*), intent(in) :: filename1, filename2  ! 1:station_name, 2:station_data
+    character(len=*), intent(in) :: filename1, filename2  ! 1:station_name, 2: time series : contains station_name YES!
     integer, optional, intent(out) :: rc
     ! loc
     character(len=40) :: str, sdmy, shms
@@ -109,51 +102,16 @@ contains
        read(unit, *) sampler%station_id(i), &
             sampler%station_name(i), &
             sampler%lats(i), &
-            sampler%lons(i), &
-            sampler%elevs(i)
+            sampler%lons(i)
     enddo
     close(unit)
-    allocate(sampler%id2rank(maxnstation))
-    do i=1, nstation
-       sampler%id2rank(sampler%station_id(i))=i
-    enddo
-
-    
+    ! debug
     write(6,*) 'sampler%station_name(1:2) : ', &
          trim(sampler%station_name(1)), ' ', trim(sampler%station_name(2))
     write(6,*) 'sampler%lons(1:2) : ', sampler%lons(1:2)
 
     
-    !_ 2. read from station_data_file: time series
-    open(newunit=unit, file=trim(filename2), form='formatted', access='sequential', status='old')
-    ios=0; i=0
-    do while (ios==0)
-       read (unit, *, IOSTAT=ios) str,str,str,j,t,id,str
-       if (ios==0) i=i+1
-       !! write(6,*) 'id=',id
-    enddo
-    nobs=i; sampler%nobs=nobs
-    allocate(sampler%times(nobs))
-    allocate(sampler%ids(nobs))
-    rewind(unit)
-    do i=1, nobs
-       read(unit, *) str, sdmy, shms, j, t, sampler%ids(i)
-       read(sdmy,'(i2,a,i2,a,i4)') iday, s1, imonth, s1, iyear
-       read(shms,'(i2,a,i2,a,i2)') ihr, s1, imin, s1, isec
-       if (mod(i,3000)==1) then
-          print*, str, sdmy, shms, j, t, sampler%ids(i)
-          print*, iday, imonth, iyear
-          print*, ihr, imin, isec
-       endif
-       call ESMF_TimeSet(sampler%times(i),yy=iyear,mm=imonth,dd=iday,h=ihr,m=imin,s=isec,_RC)
-    enddo
-    close(unit)
-    write(6,*) 'sampler%station_name(1:2) : ', &
-         trim(sampler%station_name(1)), ' ', trim(sampler%station_name(2))
-    write(6,*) 'sampler%lons(1:2) : ', sampler%lons(1:2)
-
-    
-    !_ 3. create LocStreamFactory, then esmf_ls including route_handle
+    !_ 2. create LocStreamFactory, then esmf_ls including route_handle
     !
     sampler%LSF = LocStreamFactory(sampler%lons, sampler%lats, _RC)
     sampler%esmf_ls = sampler%LSF%create_locstream(_RC)
@@ -190,25 +148,28 @@ contains
     else
        this%vdata=VerticalData(_RC)
     end if
-    call this%vdata%append_vertical_metadata(this%fmd,this%bundle,_RC)
+    call this%vdata%append_vertical_metadata(this%fmd,this%bundle,_RC) !?
     this%do_vertical_regrid = (this%vdata%regrid_type /= VERTICAL_METHOD_NONE)
     if (this%vdata%regrid_type == VERTICAL_METHOD_ETA2LEV) call this%vdata%get_interpolating_variable(this%bundle,_RC)
 
     call timeInfo%add_time_to_metadata(this%fmd,_RC)
     this%time_info = timeInfo
     
-    !call this%fmd%add_dimension('nstation',this%nstation)
-    !call this%fmd%add_dimension('time', 1)  ! time = UNLIMITED
-    !call this%fmd%add_dimension('lev',  1)  ! lev = 1: aeronet on surface 
+    call this%fmd%add_dimension('station_index',this%nstation)
+!? check vdata vertical !!    call this%fmd%add_dimension('lev',  1)  ! lev = 1: aeronet on surface 
     !
-    v = Variable(type=pFIO_REAL32, dimensions='time')
+    v = Variable(type=pFIO_REAL32, dimensions='nstation')  ! nstation
     call v%add_attribute('long_name','longitude')
     call v%add_attribute('unit','degree_east')    
     call this%fmd%add_variable('longitude',v)
-    v = Variable(type=pFIO_REAL32, dimensions='time')
+    v = Variable(type=pFIO_REAL32, dimensions='nstation')
     call v%add_attribute('long_name','latitude')
-    call v%add_attribute('unit','degree_north')    
+    call v%add_attribute('unit','degree_north')
     call this%fmd%add_variable('latitude',v)
+    !??
+    v = Variable(type=pFIO_INT32, dimensions='station_index')
+    call this%fmd%add_variable('station_id',v)
+    
     
     !-- add field in bundle to filemetadata
     !
@@ -233,11 +194,13 @@ contains
        endif
        !
        if (field_rank==2) then
-          vdims = "time"
+          vdims = "nstation,time"
+          v = variable(type=PFIO_REAL32,dimensions=trim(vdims),chunksizes=[nstation,1])          
        else if (field_rank==3) then
-          vdims = "time,lev"    ! how is lev defined in fmd
+          vdims = "lev,nstation,time"
+          v = variable(type=PFIO_REAL32,dimensions=trim(vdims),chunksizes=[??,1,1])
        end if
-       v = variable(type=PFIO_REAL32,dimensions=trim(vdims))
+
        call v%add_attribute('units',trim(units))
        call v%add_attribute('long_name',trim(long_name))
        call v%add_attribute('missing_value',MAPL_UNDEF)
@@ -247,17 +210,12 @@ contains
     enddo  ! fieldCount
     deallocate (fieldNameList)
 
+    
     ! locstream route handle
     call ESMF_FieldBundleGet(bundle,grid=grid,_RC)
     this%regridder = LocStreamRegridder(grid,this%esmf_ls,_RC)
 
-    this%obs_written=0
-    this%previous_index = lbound(this%times,1)-1
-    call timeInfo%get(clock=clock,_RC)
-    call ESMF_ClockGet(clock,currTime=this%previous_time,_RC)
-
     write(6,*) 'end of add_metadata_route_handle'
-    
   end subroutine add_metadata_route_handle
 
   
@@ -267,8 +225,6 @@ contains
      integer, optional, intent(out) :: rc
      !
      integer :: status
-     type(GriddedIOitemVectorIterator) :: iter
-     type(GriddedIOitem), pointer :: item
      type(ESMF_Field) :: src_field,dst_field
      integer :: rank,interval(2),number_to_write,previous_day,current_day
      real(kind=REAL32), allocatable :: p_new_lev(:,:,:)
@@ -281,37 +237,20 @@ contains
      real(kind=ESMF_KIND_R8), allocatable :: rtimes(:)
      integer :: i, id, iobs, ix
 
-
      write(6,*) 'bg: append_file'
 
-     interval = this%get_current_interval(current_time)
-     write(6,*) 'interval=', interval(1:2)
-     
-     if (all(interval==0)) then
-        number_to_write = 0
-     else
-        number_to_write = interval(2)-interval(1)+1
-     end if
-     if (number_to_write>0) then
-        !_ s1. put_var: time/gridded variables
-        rtimes = this%compute_times_for_interval(interval,_RC)
-        if (this%vdata%regrid_type==VERTICAL_METHOD_ETA2LEV) then
-           call this%vdata%setup_eta_to_pressure(_RC)
+
+     this%obs_written=this%obs_written+1  ! increment first
+     number_to_write=1
+
+     !_ s1. put_var: time/gridded variables
+     rtimes = this%compute_times_from_open_to_now(this%currtime)   ! real 
+     if (this%vdata%regrid_type==VERTICAL_METHOD_ETA2LEV) then
+        call this%vdata%setup_eta_to_pressure(_RC)
         end if
         if (mapl_am_i_root()) then
-           call this%formatter%put_var('time',rtimes,&
-                start=[this%obs_written+1],count=[number_to_write],_RC)
-           !!call this%formatter%put_var('longitude',this%lons(interval(1):interval(2)),&
-           !!     start=[this%obs_written+1],count=[number_to_write],_RC)
-           !!call this%formatter%put_var('latitude',this%lats(interval(1):interval(2)),&
-           !!     start=[this%obs_written+1],count=[number_to_write],_RC)
-           do iobs=interval(1), interval(2)
-              ix=this%id2rank(this%ids(iobs))
-              call this%formatter%put_var('longitude',this%lons(ix:ix),&
-                   start=[this%obs_written+iobs],count=[1],_RC)
-              call this%formatter%put_var('latitude',this%lats(ix:ix),&
-                   start=[this%obs_written+iobs],count=[1],_RC)
-           enddo
+           call this%formatter%put_var('time',rtimes,&             ! rtimes: seconds from opening file 3600s
+                start=[this%obs_written],count=[1],_RC)
         end if
         deallocate(rtimes)
 
@@ -330,15 +269,14 @@ contains
                    typekind=ESMF_TYPEKIND_R4,_RC)
               call ESMF_FieldGet(dst_field,farrayptr=p_dst_2d,_RC)
               call this%regridder%regrid(p_src_2d,p_dst_2d,_RC)
+              call ESMF_FieldDestroy(dst_field,nogarbage=.true.)
               if (mapl_am_i_root()) then
                  !!call this%formatter%put_var(trim(xname),p_dst_2d(interval(1):interval(2)),&
                  !!     start=[this%obs_written+1],count=[number_to_write])
-                 do iobs=interval(1), interval(2)
-                    ix=this%id2rank(this%ids(iobs))
-                    write(6,'(2x,a,2x,2i5)') 'id, ix=', this%ids(iobs), ix
-                    call this%formatter%put_var(xname,p_dst_2d(ix:ix),&
-                         start=[this%obs_written+iobs],count=[1],_RC)                         
-                 enddo
+
+                 !write(6,'(2x,a,2x,2i5)') 'id, ix=', this%ids(iobs), ix
+                    call this%formatter%put_var(xname,p_dst_2d,&
+                         start=[1,this%obs_written],count=[nstation,1],_RC)                         
               end if
            else if (rank==3) then
               !!STOP 'grid2LS regridder for rank==3 not implemented'
@@ -352,29 +290,27 @@ contains
               dst_field = ESMF_FieldCreate(this%esmf_ls,name=xname,&
                    typekind=ESMF_TYPEKIND_R4,ungriddedLBound=lb,ungriddedUBound=ub,_RC)
               call ESMF_FieldGet(dst_field,farrayptr=p_dst_3d,_RC)
-              if (this%vdata%regrid_type==VERTICAL_METHOD_ETA2LEV) then
-                 allocate(p_new_lev(size(p_src_3d,1),size(p_src_3d,2),this%vdata%lm),_STAT)
-                 call this%vdata%regrid_eta_to_pressure(p_src_3d,p_new_lev,_RC)
-                 call this%regridder%regrid(p_new_lev,p_dst_3d,_RC)
-              else
+
+!              if (this%vdata%regrid_type==VERTICAL_METHOD_ETA2LEV) then
+!                 allocate(p_new_lev(size(p_src_3d,1),size(p_src_3d,2),this%vdata%lm),_STAT)
+!                 call this%vdata%regrid_eta_to_pressure(p_src_3d,p_new_lev,_RC)
+!                 call this%regridder%regrid(p_new_lev,p_dst_3d,_RC)
+!              else
                  call this%regridder%regrid(p_src_3d,p_dst_3d,_RC)
-              end if
+              !              end if
+
+                 call ESMF_FieldDestroy(dst_field,nogarbage=.true.)
+
+                 
               if (mapl_am_i_root()) then
-                 do iobs=interval(1), interval(2)
-                    id=this%id2rank(this%ids(iobs))
-                    call this%formatter%put_var(xname,p_dst_3d(id:id,1:size(p_dst_3d,2)),&
-                         start=[this%obs_written+iobs,1],count=[1,size(p_dst_3d,2)],_RC)
-                    !                                   lev            nlev
-                 enddo
-                 !!call this%formatter%put_var(trim(item%xname),p_dst_3d(interval(1):interval(2),:),&
-                 !!    start=[this%obs_written+1,1],count=[number_to_write,size(p_dst_3d,2)])                 
+                 call this%formatter%put_var(xname,p_dst_3d,&
+                      start=[1,1,this%obs_written],count=[size(p_dst_3d,2),nstation,1],_RC)
+                 !            lev, station,time
               end if
            else
               STOP 'grid2LS regridder: rank > 3 not implemented'
            end if  ! rank
         enddo  !  loop fieldCount
-        this%obs_written=this%obs_written+number_to_write
-     endif     !  if number_to_write > 0
 
      
      !? call ESMF_TimeGet(this%previous_time,dd=previous_day,_RC)
@@ -397,12 +333,15 @@ contains
      integer :: status
 
      this%ofile = trim(filename)
-!     v = this%time_info%define_time_variable(_RC)
-!     call this%fmd%modify_variable('time',v,_RC)
+     v = this%time_info%define_time_variable(_RC)
+     call this%fmd%modify_variable('time',v,_RC)
 
      if (mapl_am_I_root()) then
         call this%formatter%create(trim(filename),_RC)
         call this%formatter%write(this%fmd,_RC)
+        call this%formatter%put_var('longitude',this%lons,_RC)
+        call this%formatter%put_var('latitude',this%lats,_RC)
+        call this%formatter%put_var('station_id',this%station_id,_RC)                
      end if
      this%obs_written = 0
    end subroutine create_file_handle
@@ -443,6 +382,9 @@ contains
     found = .false.
     nfound = 0
     interval = 0
+
+    write(6,'(2x,a,2x,2i5)')  'this%previous_index+1,size(this%times)', this%previous_index+1,size(this%times)
+    
     do i=this%previous_index+1,size(this%times)
        if (this%times(i) .ge. this%previous_time .and. this%times(i) .le. current_time) then
           if (.not.found) then
