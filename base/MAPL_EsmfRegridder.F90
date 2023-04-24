74a09eeeae5f7449f314e96d8b8df2e02fe1cc96
Atanas Trayanov <atanas.l.trayanov@nasa.gov>
2023-04-24 15:18:53 -0400

    Fixes the regridding of vertor field with subdomains without DE. Also fixes no DE case for vertical regridding

--- base/MAPL_EsmfRegridder.F90
+++ base/MAPL_EsmfRegridder.F90
@@ -825,8 +825,12 @@ contains
 
       km = size(u_in,3)
       _ASSERT(km == size(v_in,3),'inconsistent array shape')
-      _ASSERT(km == size(u_out,3),'inconsistent array shape')
-      _ASSERT(km == size(v_out,3),'inconsistent array shape')
+      if (size(u_out) /= 0) then
+         _ASSERT(km == size(u_out,3),'inconsistent array shape')
+      end if
+      if (size(u_out) /= 0) then
+         _ASSERT(km == size(v_out,3),'inconsistent array shape')
+      end if
 
       im_src = size(u_in,1)
       jm_src = size(u_in,2)
@@ -938,8 +942,12 @@ contains
 
      km = size(u_in,3)
      _ASSERT(km == size(v_in,3),'inconsistent array shape')
-     _ASSERT(km == size(u_out,3),'inconsistent array shape')
-     _ASSERT(km == size(v_out,3),'inconsistent array shape')
+      if (size(u_out) /= 0) then
+         _ASSERT(km == size(u_out,3),'inconsistent array shape')
+      end if
+      if (size(u_out) /= 0) then
+         _ASSERT(km == size(v_out,3),'inconsistent array shape')
+      end if
 
      im_src = size(u_in,1)
      jm_src = size(u_in,2)
@@ -1046,8 +1054,12 @@ contains
 
       km = size(u_in,3)
       _ASSERT(km == size(v_in,3),'inconsistent array shape')
-      _ASSERT(km == size(u_out,3),'inconsistent array shape')
-      _ASSERT(km == size(v_out,3),'inconsistent array shape')
+      if (size(u_out) /= 0) then
+         _ASSERT(km == size(u_out,3),'inconsistent array shape')
+      end if
+      if (size(u_out) /= 0) then
+         _ASSERT(km == size(v_out,3),'inconsistent array shape')
+      end if
 
       im_src = size(u_in,1)
       jm_src = size(u_in,2)
