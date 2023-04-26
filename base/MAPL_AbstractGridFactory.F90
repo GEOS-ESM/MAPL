62c4e6cdb05fde80fe49a8b90e61da4e8b27ea22
Tom Clune <thomas.l.clune@nasa.gov>
2023-04-26 14:51:01 -0400

    Update MAPL_AbstractGridFactory.F90

--- base/MAPL_AbstractGridFactory.F90
+++ base/MAPL_AbstractGridFactory.F90
@@ -83,7 +83,7 @@ module MAPL_AbstractGridFactoryMod
       procedure(decomps_are_equal), deferred :: decomps_are_equal
       procedure(physical_params_are_equal), deferred :: physical_params_are_equal
 
-      procedure(I_get_subset), deferred :: get_subset
+      procedure :: get_subset
       procedure :: destroy
    end type AbstractGridFactory
 
@@ -241,14 +241,7 @@ module MAPL_AbstractGridFactoryMod
          type(FileMetadata), intent(in), optional :: metaData
       end function generate_file_reference3D
 
-      subroutine I_get_subset(this, interval, subset, rc)
-         use ESMF
-         import AbstractGridFactory
-         class(AbstractGridFactory), intent(in) :: this
-         type(ESMF_Time), intent(in) :: interval
-         integer, intent(out) :: subset(2,2)
-         integer, optional, intent(out) :: rc
-      end subroutine I_get_subset
+
    end interface
 
    character(len=*), parameter :: MOD_NAME = 'MAPL_AbstractGridFactory::'
@@ -1043,6 +1036,19 @@ contains
 
    end function get_grid
 
+   ! This procedure should only be called for time dependent grids.
+   ! A default implementation is to fail for other grid types, so we do not 
+   ! have to explicitly add methods to all of the existing subclasses.  
+   subroutine I_get_subset(this, interval, subset, rc)
+      class(AbstractGridFactory), intent(in) :: this
+      type(ESMF_Time), intent(in) :: interval
+      integer, intent(out) :: subset(2,2)
+      integer, optional, intent(out) :: rc
+         
+      integer :: status
+      
+      _RETURN(_FAILURE)
+   end subroutine I_get_subset
 
    ! Probably don't need to do anything more for subclasses unless they have
    ! other objects that don't finalize well.  (NetCDF, ESMF, MPI, ...)
