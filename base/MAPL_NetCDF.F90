e4c0a3979fdf03d804536b159303e3750ff80ad3
Darian Boggs <william.d.boggs@nasa.gov>
2023-06-13 18:12:39 -0400

    All MAPL.base ctests work for gfortran

--- base/MAPL_NetCDF.F90
+++ base/MAPL_NetCDF.F90
@@ -29,9 +29,9 @@ module MAPL_NetCDF
 
    private
 
-   ! LOW-LEVEL
-!   public :: convert_NetCDF_DateTimeString_to_ESMF_Time, make_NetCDF_DateTime_duration, is_digit_string, get_shift_sign
-!   public :: make_ESMF_TimeInterval, is_valid_netcdf_datetime_string, convert_to_integer, convert_to_real, split, split_all
+   ! LOW-LEVEL - keep commented out (private) unless debugging these procedures
+   public :: convert_NetCDF_DateTimeString_to_ESMF_Time, make_NetCDF_DateTime_duration, is_digit_string, get_shift_sign
+   public :: make_ESMF_TimeInterval, is_valid_netcdf_datetime_string, convert_to_integer, convert_to_real, split
 
    interface make_ESMF_TimeInterval
       module procedure :: make_ESMF_TimeInterval_integer
@@ -44,18 +44,13 @@ module MAPL_NetCDF
    end interface make_NetCDF_DateTime_duration
    
    interface split
-      module procedure :: split_chars
+      module procedure :: split_characters
    end interface split
 
-   interface split_all
-      module procedure :: split_all_iterative
-   end interface split_all
-
-
    character, parameter :: PART_DELIM = ' '
-   character, parameter :: ISO_DELIM  = 'T'
    character, parameter :: DATE_DELIM = '-'
    character, parameter :: TIME_DELIM = ':'
+   character, parameter :: DELIMS(3) = [PART_DELIM, DATE_DELIM, TIME_DELIM]
    character, parameter :: POINT = '.'
    character(len=*), parameter :: NETCDF_DATE = '0000' // DATE_DELIM // '00' // DATE_DELIM // '00'
    character(len=*), parameter :: NETCDF_TIME = '00' // TIME_DELIM // '00' // TIME_DELIM // '00'
@@ -74,27 +69,27 @@ module MAPL_NetCDF
    character, parameter :: MINUS = '-'
    character(len=*), parameter :: SIGNS = PLUS // MINUS
    character(len=*), parameter :: EMPTY_STRING = ''
+   integer, parameter :: MAX_CHARACTER_LENGTH = 64
 
 contains
 
 !===============================================================================
-!========================= NEW HIGH-LEVEL PROCEDURES ===========================
+!========================= HIGH-LEVEL PROCEDURES ===========================
 
    ! Get NetCDF DateTime duration from ESMF_Time and units_string (integer)
    subroutine get_NetCDF_duration_from_ESMF_Time_integer(time, units_string, duration, unusable, rc)
       type(ESMF_Time),  intent(inout) :: time
-      character(len=:), allocatable, intent(in) :: units_string
+      character(len=*), intent(in) :: units_string
       integer, intent(out) :: duration
       class (KeywordEnforcer), optional, intent(in) :: unusable
       integer, optional, intent(out) :: rc
 
       type(ESMF_Time) :: start_time
       type(ESMF_TimeInterval) :: interval
-      character(len=:), allocatable :: parts(:)
-      character(len=:), allocatable :: units
-      character(len=:), allocatable :: preposition
-      character(len=:), allocatable :: date_string
-      character(len=:), allocatable :: time_string
+      character(len=MAX_CHARACTER_LENGTH) :: units
+      character(len=MAX_CHARACTER_LENGTH) :: preposition
+      character(len=MAX_CHARACTER_LENGTH) :: datetime_string
+      character(len=MAX_CHARACTER_LENGTH) :: remainder
       integer :: status
       integer(ESMF_KIND_I8) :: sign_factor
 
@@ -102,16 +97,15 @@ contains
       
       _ASSERT((len_trim(adjustl(units_string)) > 0), 'units_string empty')
 
-      parts = split_all(trim(units_string), PART_DELIM)
-      _ASSERT(size(parts) == NUM_PARTS_UNITS_STRING, 'Invalid number of parts in units_string')
+      call split(trim(units_string), units, remainder, PART_DELIM)
+      call split(trim(remainder), preposition, remainder, PART_DELIM)
+      datetime_string = trim(remainder)
 
-      units       = adjustl(parts(1))
-      preposition = adjustl(parts(2))
-      date_string = adjustl(parts(3))
-      time_string = adjustl(parts(4))
 
-      call convert_NetCDF_DateTimeString_to_ESMF_Time(date_string // PART_DELIM // time_string, start_time, _RC)
+      call convert_NetCDF_DateTimeString_to_ESMF_Time(trim(datetime_string), start_time, _RC)
       interval = time - start_time
+      call ESMF_TimeIntervalValidate(interval, rc = status)
+      _ASSERT(status == ESMF_SUCCESS, 'Invalid ESMF_TimeInterval')
 
       call make_NetCDF_DateTime_duration(interval, start_time, units, duration, _RC)
       sign_factor = get_shift_sign(preposition)
@@ -132,11 +126,10 @@ contains
 
       type(ESMF_Time) :: start_time
       type(ESMF_TimeInterval) :: interval
-      character(len=:), allocatable :: parts(:)
-      character(len=:), allocatable :: units
-      character(len=:), allocatable :: preposition
-      character(len=:), allocatable :: date_string
-      character(len=:), allocatable :: time_string
+      character(len=MAX_CHARACTER_LENGTH) :: units
+      character(len=MAX_CHARACTER_LENGTH) :: preposition
+      character(len=MAX_CHARACTER_LENGTH) :: datetime_string
+      character(len=MAX_CHARACTER_LENGTH) :: remainder
       integer :: status
       integer(ESMF_KIND_I8) :: sign_factor
 
@@ -144,15 +137,11 @@ contains
       
       _ASSERT((len_trim(adjustl(units_string)) > 0), 'units_string empty')
 
-      parts = split_all(units_string, PART_DELIM)
-      _ASSERT(size(parts) == NUM_PARTS_UNITS_STRING, 'Invalid number of parts in units_string')
+      call split(trim(units_string), units, remainder, PART_DELIM)
+      call split(trim(remainder), preposition, remainder, PART_DELIM)
+      datetime_string = trim(remainder)
 
-      units       = adjustl(parts(1))
-      preposition = adjustl(parts(2))
-      date_string = adjustl(parts(3))
-      time_string = adjustl(parts(4))
-
-      call convert_NetCDF_DateTimeString_to_ESMF_Time(date_string // PART_DELIM // time_string, start_time, _RC)
+      call convert_NetCDF_DateTimeString_to_ESMF_Time(datetime_string, start_time, _RC)
       interval = time - start_time
 
       call make_NetCDF_DateTime_duration(interval, start_time, units, duration, _RC)
@@ -174,14 +163,12 @@ contains
       type(ESMF_Time), intent(inout) :: time
       integer, optional, intent(out) :: rc
 
-      character(len=:), allocatable :: parts(:)
       type(ESMF_TimeInterval) :: interval
       type(ESMF_Time) :: start_time
-      character(len=:), allocatable :: units
-      character(len=:), allocatable :: preposition
-      character(len=LEN_DATE) :: date_string
-      character(len=LEN_TIME) :: time_string
-      character(len=LEN_DATETIME) :: datetime_string
+      character(len=MAX_CHARACTER_LENGTH) :: units
+      character(len=MAX_CHARACTER_LENGTH) :: preposition
+      character(len=MAX_CHARACTER_LENGTH) :: datetime_string
+      character(len=MAX_CHARACTER_LENGTH) :: remainder
       integer :: signed_duration, sign_factor
       integer :: status
 
@@ -190,14 +177,9 @@ contains
       _ASSERT(duration >= 0, 'Negative duration not supported')
       _ASSERT((len_trim(adjustl(units_string)) > 0), 'units_string empty')
 
-      parts = split_all(units_string, PART_DELIM)
-      _ASSERT(size(parts) == NUM_PARTS_UNITS_STRING, 'Invalid number of parts in units_string')
-
-      units       = adjustl(parts(1))
-      preposition = adjustl(parts(2))
-      date_string = adjustl(parts(3))
-      time_string = adjustl(parts(4))
-      datetime_string = date_string // PART_DELIM // time_string
+      call split(trim(units_string), units, remainder, PART_DELIM)
+      call split(trim(remainder), preposition, remainder, PART_DELIM)
+      datetime_string = trim(remainder)
 
       sign_factor = get_shift_sign(preposition)
       _ASSERT(sign_factor /= 0, 'Unrecognized preposition')
@@ -222,14 +204,12 @@ contains
       type(ESMF_Time), intent(inout) :: time
       integer, optional, intent(out) :: rc
 
-      character(len=:), allocatable :: parts(:)
       type(ESMF_TimeInterval) :: interval
       type(ESMF_Time) :: start_time
-      character(len=:), allocatable :: units
-      character(len=:), allocatable :: preposition
-      character(len=:), allocatable :: date_string
-      character(len=:), allocatable :: time_string
-      character(len=:), allocatable :: datetime_string
+      character(len=MAX_CHARACTER_LENGTH) :: units
+      character(len=MAX_CHARACTER_LENGTH) :: preposition
+      character(len=MAX_CHARACTER_LENGTH) :: datetime_string
+      character(len=MAX_CHARACTER_LENGTH) :: remainder
       real(kind=ESMF_KIND_R8) :: signed_duration, sign_factor
       integer :: status
 
@@ -238,14 +218,9 @@ contains
       _ASSERT(duration >= 0, 'Negative duration not supported')
       _ASSERT((len_trim(adjustl(units_string)) > 0), 'units_string empty')
 
-      parts = split_all(units_string, PART_DELIM)
-      _ASSERT(size(parts) == NUM_PARTS_UNITS_STRING, 'Invalid number of parts in units_string')
-
-      units       = adjustl(parts(1))
-      preposition = adjustl(parts(2))
-      date_string = adjustl(parts(3))
-      time_string = adjustl(parts(4))
-      datetime_string = date_string // PART_DELIM // time_string
+      call split(trim(units_string), units, remainder, PART_DELIM)
+      call split(trim(remainder), preposition, remainder, PART_DELIM)
+      datetime_string = trim(remainder)
 
       sign_factor = get_shift_sign(preposition)
       _ASSERT(sign_factor /= 0, 'Unrecognized preposition')
@@ -260,9 +235,9 @@ contains
 
    end subroutine get_ESMF_Time_from_NetCDF_DateTime_real
 
-!======================= END NEW HIGH-LEVEL PROCEDURES =========================
+!======================= END HIGH-LEVEL PROCEDURES =========================
 !===============================================================================
-!========================= NEW LOWER-LEVEL PROCEDURES ==========================
+!========================= LOWER-LEVEL PROCEDURES ==========================
 
    ! Convert NetCDF datetime to ESMF_Time
    subroutine convert_NetCDF_DateTimeString_to_ESMF_Time_prior(datetime_string, datetime, unusable, rc)
@@ -330,67 +305,47 @@ contains
       class (KeywordEnforcer), optional, intent(in) :: unusable
       integer, optional, intent(out) :: rc
       integer :: status 
-      integer :: yy, mm, dd, h, m, s
+      integer :: yy, mm, dd, h, m
       real(kind=ESMF_KIND_R8) :: s_r8
-      character(len=:), allocatable :: part(:)
-      character(len=:), allocatable :: date_string
-      character(len=:), allocatable :: time_string
-      character(len=:), allocatable :: msg
+      character(len=MAX_CHARACTER_LENGTH) :: part
+      character(len=MAX_CHARACTER_LENGTH) :: remainder
 
       _UNUSED_DUMMY(unusable)
 
-      msg = 'Invalid datetime string: ' // datetime_string
-      _ASSERT(is_valid_netcdf_datetime_string(datetime_string), msg)
-
-      part = split_all(datetime_string, PART_DELIM)
-      date_string = part(1)
-      time_string = part(2)
+      _ASSERT(is_valid_netcdf_datetime_string(datetime_string), 'Invalid NetCDF datetime string')
 
       ! convert first 3 substrings to year, month, day
-      part = split_all(date_string, DATE_DELIM)
+      remainder = datetime_string
 
-      call convert_to_integer(part(1), yy, rc = status)
+      call split(trim(remainder), part, remainder, DATE_DELIM)
+      call convert_to_integer(trim(part), yy, rc = status)
       _ASSERT(status == 0, 'Unable to convert year string')
 
-      call convert_to_integer(part(2), mm, rc = status)
+      call split(trim(remainder), part, remainder, DATE_DELIM)
+      call convert_to_integer(trim(part), mm, rc = status)
       _ASSERT(status == 0, 'Unable to convert month string')
 
-      call convert_to_integer(part(3), dd, rc = status)
+      call split(trim(remainder), part, remainder, PART_DELIM)
+      call convert_to_integer(trim(part), dd, rc = status)
       _ASSERT(status == 0, 'Unable to convert day string')
 
       ! convert second 3 substrings to hour, minute, second
-      part = split_all(time_string, TIME_DELIM)
-
-      call convert_to_integer(part(1), h, rc = status)
+      call split(trim(remainder), part, remainder, TIME_DELIM)
+      call convert_to_integer(part, h, rc = status)
       _ASSERT(status == 0, 'Unable to convert hour string')
 
-      call convert_to_integer(part(2), m, rc = status)
+      call split(trim(remainder), part, remainder, TIME_DELIM)
+      call convert_to_integer(trim(part), m, rc = status)
       _ASSERT(status == 0, 'Unable to convert minute string')
 
-      ! no need to call this unless larger time units are correct
+      part = remainder
+      call convert_to_real(trim(part), s_r8, rc = status)
+      _ASSERT(status == 0, 'Unable to convert second string')
+
+      ! no need to call this unless datetime units are correct
       call ESMF_CalendarSetDefault(CALKIND_FLAG, _RC)
 
-      ! Need to see if the seconds portion has fractional portion and handle it.
-      ! Split seconds string to see if it has a fractional part.
-      select case(size(split_all(part(3), POINT)))
-
-         ! no fractional portion => use integer
-         case(1)
-            call convert_to_integer(part(3), s, rc = status)
-            _ASSERT(status == 0, 'Unable to convert second string')
-            call ESMF_TimeSet(datetime, yy=yy, mm=mm, dd=dd, h=h, m=m, s=s, _RC)
-
-         ! fractional portion => use real(kind=ESMF_KIND_R8)
-         case(2)
-            call convert_to_real(part(3), s_r8, rc = status)
-            _ASSERT(status == 0, 'Unable to convert second string')
-            call ESMF_TimeSet(datetime, yy=yy, mm=mm, dd=dd, h=h, m=m, s_r8=s_r8, _RC)
-
-         ! wrong number of substrings => FAIL
-         case default
-            _FAIL('Incorrect number of second parts')
-
-      end select
+      call ESMF_TimeSet(datetime, yy=yy, mm=mm, dd=dd, h=h, m=m, s_r8=s_r8, _RC)
 
       _RETURN(_SUCCESS)
 
@@ -509,10 +464,8 @@ contains
       select case(trim(adjustl(units))) 
          case('years')
             _FAIL('Real values for years are not supported.')
-!            call ESMF_TimeIntervalGet(interval, start_time, yy=duration, _RC)
          case('months')
             _FAIL('Real values for months are not supported.')
-!            call ESMF_TimeIntervalGet(interval, start_time, mm=duration, _RC)
          case('days')
             _FAIL('Real values for days are not supported.')
          case('hours')
@@ -529,7 +482,7 @@ contains
 
    end subroutine make_NetCDF_DateTime_duration_real
 
-!======================= END NEW LOWER-LEVEL PROCEDURES ========================
+!======================= END LOWER-LEVEL PROCEDURES ========================
 !===============================================================================
 !============================= UTILITY PROCEDURES ==============================
 
@@ -578,52 +531,34 @@ contains
       character(len=*), intent(in) :: preposition
       integer :: get_shift_sign
       integer, parameter :: POSITIVE = 1
+
       get_shift_sign = 0
       if(adjustl(preposition) == 'since') get_shift_sign = POSITIVE
+
    end function get_shift_sign
 
-   ! Split string at delimiter
-   function split_chars(chars, delimiter) result(pair)
-      character(len=*), intent(in) :: chars
-      character(len=*), intent(in) :: delimiter
-      character(len=len(chars)) :: pair(2)
-      integer start
- 
-      pair = ['', '']
+   subroutine split_characters(characters, token, remainder, delimiters)
+      character(len=*), intent(in) :: characters
+      character(len=MAX_CHARACTER_LENGTH), intent(out) :: token
+      character(len=MAX_CHARACTER_LENGTH), intent(out) :: remainder
+      character(len=*), optional, intent(in) :: delimiters
+      character(len=:), allocatable :: delims
+      integer :: i
 
-      start = index(chars, delimiter)
-      if(start == 0) then
-         pair(1) = chars
-         return
-      end if
+      delims = ' '
+      if(present(delimiters)) delims = delimiters
+      
+      i = scan(characters, delims)
 
-      pair(1) = chars(1:(start - 1))
-      pair(2) = chars((start+len(delimiter)):len_trim(chars))
+      if(i > 0) then
+         token = characters(:(i-1))
+         remainder = characters((i+1):) 
+      else
+         token = characters
+         remainder = EMPTY_STRING
+      endif
 
-   end function split_chars
-
-   function split_all_iterative(string, delimiter) result(parts)
-      character(len=*), intent(in) :: string
-      character(len=*), intent(in) :: delimiter
-      character(len=:), allocatable :: parts(:)
-      character(len=:), allocatable :: pair(:)
-      character(len=:), allocatable :: head
-      character(len=:), allocatable :: tail
-         
-      parts = [trim(string)]
-
-      if((len(string) == 0) .or. (len(delimiter) == 0)) return
-
-      tail = parts(1)
-      parts = [character::]
-      do while (len(tail) > 0)
-         pair = split(tail, delimiter)
-         head = trim(pair(1))
-         tail = trim(pair(2))
-         if(len(head) > 0) parts = [parts, head]
-      end do
-
-   end function split_all_iterative
+   end subroutine split_characters
 
    ! Convert string representing an integer to the integer
    subroutine convert_to_integer(string, n, rc)
