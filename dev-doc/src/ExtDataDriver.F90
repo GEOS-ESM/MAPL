5c403fd0e8a3f4347df4731dbf9ea4d95d527029
tclune <thomas.l.clune@nasa.gov>
2023-07-18 17:26:50 +0000

    Deploying to gh-pages from @ GEOS-ESM/MAPL@46a6dd2c658200321a7d87f7bb8ce3ebb31fcd1c 🚀

---
+++ dev-doc/src/ExtDataDriver.F90
@@ -0,0 +1,29 @@
+#define I_AM_MAIN
+
+#include "MAPL_Generic.h"
+
+program ExtData_Driver
+  use MPI
+  use ESMF
+  use ExtData_DriverGridCompMod, only: ExtData_DriverGridComp, new_ExtData_DriverGridComp
+  use ExtDataUtRoot_GridCompMod, only:  ROOT_SetServices => SetServices
+  use ExtDataDriverMod
+  use MAPL
+
+  implicit none
+
+  integer :: status
+  character(len=*), parameter :: Iam="ExtData_Driver"
+  type(ExtDataDriver) :: Driver
+  type (MAPL_CapOptions) :: cap_options
+  type (MAPL_FlapCLI) :: cli
+
+  cli = MAPL_FlapCLI(description='extdata driver',authors='gmao')
+  cap_options=MAPL_CapOptions(cli)
+
+  driver = ExtDataDriver('ExtDataApp',Root_SetServices,cap_options=cap_options,_RC)
+  call driver%run(_RC)
+
+  stop
+
+end program ExtData_Driver
