812f3a8647e296909e4dc81536bc228e01ecfe3a
mathomp4 <fortran@gmail.com>
2023-08-03 18:57:11 +0000

    Deploying to gh-pages from @ GEOS-ESM/MAPL@f8d54ace6e41edf7323abff6753069e1ec6e9838 🚀

---
+++ dev-doc/src/AbstractStateItemSpec.F90
@@ -0,0 +1,10 @@
+module oomph_AbstractStateItemSpec
+   implicit none
+   private
+
+   public :: AbstractStateItemSpec
+
+   type, abstract :: AbstractStateItemSpec
+   end type AbstractStateItemSpec
+
+end module oomph_AbstractStateItemSpec
