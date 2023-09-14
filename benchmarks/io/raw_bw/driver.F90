55089883940110f01f8f2527ac73f98e59ad5f29
Tom Clune <thomas.l.clune@nasa.gov>
2023-09-14 09:42:27 -0400

    Initial attempt at high-quality benchmark suite.

---
+++ benchmarks/io/raw_bw/driver.F90
@@ -0,0 +1,166 @@
+#define I_AM_MAIN
+#include "MAPL_ErrLog.h"
+program main
+   use mapl_BW_BenchmarkSpec
+   use mapl_BW_Benchmark
+   use mapl_ErrorHandlingMod
+   use mpi
+   implicit none
+
+   type(BW_BenchmarkSpec) :: spec
+   integer :: status
+      
+   call mpi_init(_IERROR)
+   spec = make_BW_BenchmarkSpec() ! CLI
+
+   call run(spec, _RC)
+
+   call MPI_Barrier(MPI_COMM_WORLD, _IERROR)
+   call mpi_finalize(_IERROR)
+
+   stop
+
+contains
+
+
+#undef I_AM_MAIN
+#include "MAPL_ErrLog.h"
+   subroutine run(spec, rc)
+      type(BW_BenchmarkSpec), intent(in) :: spec
+      integer, optional, intent(out) :: rc
+
+      integer :: status
+
+      real :: tot_time
+      real :: tot_time_sq
+      real :: avg_time
+      real :: std_time
+      type(BW_Benchmark) :: benchmark
+      integer :: writer_comm
+      integer :: i
+      real :: t
+
+      writer_comm = split_comm(MPI_COMM_WORLD, spec%n_streams, _RC)
+      _RETURN_IF(writer_comm == MPI_COMM_NULL)
+
+      benchmark = make_BW_Benchmark(spec, writer_comm, _RC)
+
+      call write_header(writer_comm, _RC)
+
+      tot_time = 0
+      tot_time_sq = 0
+      associate (n => spec%n_tries)
+        do i = 1, n
+           t = time(benchmark, writer_comm, _RC)
+           tot_time = tot_time + t
+           tot_time_sq = tot_time_sq + t**2
+        end do
+        avg_time = tot_time / n
+
+        std_time = -1 ! unless
+        if (n > 1) then
+           std_time = sqrt((tot_time_sq - spec%n_tries*avg_time**2)/(n-1))
+        end if
+      end associate
+
+      call report(spec, avg_time, std_time, writer_comm, _RC)
+
+      _RETURN(_SUCCESS)
+   end subroutine run
+
+
+   real function time(benchmark, comm, rc)
+      type(BW_Benchmark), intent(in) :: benchmark
+      integer, intent(in) :: comm
+      integer, optional, intent(out) :: rc
+
+      integer :: status
+      real :: t0, t1
+      integer :: rank
+
+      call MPI_Barrier(comm, _IERROR)
+      t0 = MPI_Wtime()
+      call benchmark%run(_RC)
+      call MPI_Barrier(comm, _IERROR)
+      t1 = MPI_Wtime()
+
+      time = t1 - t0
+
+      _RETURN(_SUCCESS)
+   end function time
+
+
+   ! Spread writers apart in rank rather than clump on any given node.
+   function split_comm(comm_world, n_streams, rc) result(new_comm)
+      integer :: new_comm
+      integer, intent(in) :: comm_world
+      integer, intent(in) :: n_streams
+      integer, optional, intent(out) :: rc
+
+      integer :: status
+      integer :: color
+      integer :: team_size
+      integer :: npes, rank
+
+      call MPI_Comm_size(comm_world, npes, _IERROR)
+      call MPI_Comm_rank(comm_world, rank, _IERROR)
+      team_size = 1 + (npes-1)/n_streams
+      color = mod(rank, team_size) + (rank/(team_size*n_streams))
+      if (color /= 0) color = MPI_UNDEFINED
+
+      call MPI_Comm_split(comm_world, color, 0, new_comm, _IERROR)
+
+      _RETURN(_SUCCESS)
+   end function split_comm
+
+   subroutine write_header(comm, rc)
+      integer, intent(in) :: comm
+      integer, optional, intent(out) :: rc
+
+      integer :: status
+      integer :: rank
+
+      call MPI_Comm_rank(comm, rank, _IERROR)
+      _RETURN_UNLESS(rank == 0)
+      
+      write(*,'(6(a15,:,","))',iostat=status) 'Write (GB)', 'Packet (GB)', '# writers', 'Time (s)', 'Eff. BW (GB/s)', 'Avg. BW (GB/s)'
+
+      _RETURN(status)
+   end subroutine write_header
+
+
+   subroutine report(spec, avg_time, std_time, comm, rc)
+      type(BW_BenchmarkSpec), intent(in) :: spec
+      real, intent(in) :: avg_time
+      real, intent(in) :: std_time
+      integer, intent(in) :: comm
+      integer, optional, intent(out) :: rc
+
+      integer :: status
+      real :: packet_gb
+      real :: total_gb
+      real :: bw
+      integer :: npes
+      integer :: rank
+      integer, parameter :: WORD_SIZE = 4
+      integer :: packet_size
+
+      call MPI_Comm_size(comm, npes, _IERROR)
+      call MPI_Comm_rank(comm, rank, _IERROR)
+      _RETURN_UNLESS(rank == 0)
+
+      packet_size = int(spec%nx)**2 * spec%n_levs / spec%n_packets
+      packet_gb = 1.e-9*(WORD_SIZE * packet_size)
+      total_gb = packet_gb * spec%n_packets * npes
+      bw = total_gb / avg_time
+
+      call MPI_Comm_size(comm, npes, _IERROR)
+
+      write(*,'(2(f15.4,","),i15.0,",",3(f15.4,:,","))') total_gb, packet_gb, npes, avg_time, bw, bw/npes
+
+      _RETURN(_SUCCESS)
+   end subroutine report
+
+
+end program main
+   
