c3097e6b1d2f60f7c029a4d968a521ef7f89c1a2
Darian Boggs <william.d.boggs@nasa.gov>
2023-10-25 16:33:36 -0400

    Further cleaning

--- Apps/OverwriteConfig_FullTest.F90
+++ Apps/OverwriteConfig_FullTest.F90
@@ -13,32 +13,28 @@ program overwrite_config_fulltest
 !      procedure :: append_character
 !   end interface append
 
-   integer, parameter :: MAX_LENGTH = 1024
    integer, parameter :: SUCCESS = ESMF_SUCCESS
    integer, parameter :: FAILURE = SUCCESS - 1
    integer, parameter :: IOSUCCESS = 0
-   integer, parameter :: ATTRIB_SIZE = 2
-   integer, parameter :: NUM_ATTRIBS = 3
-   character(len=*), parameter :: EMPTY_STRING = ''
-   character(len=*), parameter :: FMT_ = '(A)'
-   character(len=*), parameter :: LABEL1 = 'SwathGrid.Epoch_init:'
-!   character(len=*), parameter :: VALUE1 = "'20121113T000000'"
-   character(len=*), parameter :: LABEL2 = 'SwathGrid.nc_Time:'
-!   character(len=*), parameter :: VALUE2 = "'time'"
-   character(len=*), parameter :: LABEL3 = 'SwathGrid.nc_Longitude:'
-!   character(len=*), parameter :: VALUE3 = "'cell_across_swath'"
-   character(len=*), parameter :: NEW_VALUE = '19991231235959'
+   character(len=*), parameter :: AFMT = '(A)'
+   character(len=*), parameter :: AIFMT = '(A,I6)'
+   character(len=*), parameter :: ALIFMT = '(A, L1, I6)'
+   character(len=80)  :: filename
+!   integer, parameter :: ATTRIB_SIZE = 2
+!   integer, parameter :: NUM_ATTRIBS = 3
+!   character(len=*), parameter :: EMPTY_STRING = ''
+!   character(len=*), parameter :: FMT_ = '(A)'
 !   character(len=*), parameter :: SUCCESS_MESSAGE = 'Success: '
 !   character(len=*), parameter :: FAILURE_MESSAGE = 'Failure: '
-   character(len=*), parameter :: DELIMITER = ' :: '
+!   character(len=*), parameter :: DELIMITER = ' :: '
 
-   character(len=MAX_LENGTH) :: filename!, message
 !   character(len=10) :: date, time
 !   integer :: stat 
 !   logical :: passed
 
    call get_command_argument(1, filename)
-   write(*, *) 'filename: ' // trim(filename) !// ': ', stat
+!   write(*, *) 'filename: ' // trim(filename) !// ': ', stat
+   write(*, fmt=AFMT) 'filename ' // trim(filename)
 !   if(stat /= SUCCESS) error stop 'Failed to get filename'
 !   if(len_trim(filename) == 0) error stop 'Filename empty'
 !   call get_command_argument(1, filename, length, stat)
@@ -48,6 +44,7 @@ program overwrite_config_fulltest
 !   end if
 
 !   write(*, *) 'temporary file: ' // trim(filename)
+!   call main(trim(filename))
    call main(trim(filename))
 
 !   if(passed) then
@@ -62,62 +59,71 @@ contains
 
    subroutine main(filename)
       character(len=*), intent(in) :: filename
+      character(len=*), parameter :: LABEL1 = 'SwathGrid.Epoch_init:'
+      character(len=*), parameter :: VALUE1 = '20121113T000000'
+      character(len=*), parameter :: LABEL2 = 'SwathGrid.nc_Time:'
+      character(len=*), parameter :: VALUE2 = 'time'
+      character(len=*), parameter :: LABEL3 = 'SwathGrid.nc_Longitude:'
+      character(len=*), parameter :: VALUE3 = 'cell_across_swath'
+      character(len=*), parameter :: NEW_VALUE = '19991231235959'
       type(ESMF_Config) :: config
       integer :: stat
 
       call ESMF_Initialize(rc=stat)
-      write(*, *) 'initialize: ', stat
+      write(*, fmt=AIFMT) 'initialize', stat
 !      passed = (stat == SUCCESS)
 !      if(.not. passed) message = 'Failed to initialize ESMF'
 
 !      if(passed) then
-         call setup(config, filename, rc=stat)
-         if(stat == SUCCESS) then
-            write(*, *) 'Setup succeeded'
-         else
-            write(*, *) 'Setup failed'
-         end if
+         call setup(config, filename, LABEL1, LABEL2, LABEL3, rc=stat)
+         write(*, fmt=AIFMT) 'SETUP', stat
+!         if(stat == SUCCESS) then
+!            write(*, fmt=AFMT) 'Setup succeeded'
+!         else
+!            write(*, fmt=AFMT) 'Setup failed'
+!         end if
 !         passed = (stat == SUCCESS)
 !         if(.not. passed) message = append('Setup failed', submessage, DELIMITER)
 !      end if
       
 !      if(passed) then
          call test_overwrite(config, LABEL1, NEW_VALUE, LABEL2, rc=stat)
-         if(stat == SUCCESS) then
-            write(*, *) 'Test exited normally'
-         else
-            write(*, *) 'Test exited abnormally'
-         end if
+         write(*, fmt=AIFMT) 'TEST', stat
+!         if(stat == SUCCESS) then
+!            write(*, fmt=AFMT) 'Test exited normally'
+!         else
+!            write(*, fmt=AFMT) 'Test exited abnormally'
+!         end if
 !         passed = passed .and. (stat == SUCCESS)
 !         if(.not. passed) message = append('test_overwrite failed: ', submessage, DELIMITER)
 !      end if
 
       call ESMF_Finalize(rc=stat)
-      write(*, *) 'finalize: ', stat
+      write(*, fmt=AIFMT) 'finalize', stat
 !      if(stat /= SUCCESS) message = append(message, 'Failed to finalize ESMF', DELIMITER)
 
    end subroutine main
 
-   subroutine setup(config, filename, rc)
+   subroutine setup(config, filename, label_1, label_2, label_3, rc)
       type(ESMF_Config), intent(inout) :: config
-      character(len=*), intent(in) :: filename
+      character(len=*), intent(in) :: filename, label_1, label_2, label_3
 !      character(len=*), optional, intent(inout) :: message
       integer, intent(out) :: rc
       integer :: stat
       logical :: config_created, is_present!, make_message, 
       logical :: setup_succeeded
-      character(len=MAX_LENGTH) :: label!, val
+!      character(len=MAX_LENGTH) :: label!, val
 
 !      make_message = present(message)
-      write(*, *) 'SETUP ========================================================================='
+      write(*, fmt=AFMT) 'SETUP ========================================================================='
 
       config = ESMF_ConfigCreate(rc=stat)
-      write(*, *) 'create: ', stat
+      write(*, fmt=AIFMT) 'create', stat
       setup_succeeded = (stat == SUCCESS)
 
 !      if(failed(stat, SUCCESS, rc)) return
       config_created = ESMF_ConfigIsCreated(config, rc=stat)
-      write(*, *) 'is created: ', stat
+      write(*, fmt=ALIFMT) 'is created ', config_created, stat
       setup_succeeded = setup_succeeded .and. (stat == SUCCESS)
 !      if(failed(FAILURE, config_created, rc)) return
 !      if(failed(stat, SUCCESS, rc)) return
@@ -127,35 +133,32 @@ contains
 !      if(failed(stat, SUCCESS, rc=stat)) return
 !      if(make_message) message = append(message, 'write_attributes successful', DELIMITER)
 
-      call ESMF_ConfigLoadFile(config, trim(filename), rc=stat)
-      write(*, *) 'load ' // trim(filename) // ': ', stat
+      call ESMF_ConfigLoadFile(config, filename, rc=stat)
+      write(*, fmt=AIFMT) 'load ' // filename, stat
       setup_succeeded = setup_succeeded .and. (stat == SUCCESS)
 !      if(failed(stat, SUCCESS, rc=stat)) return
 !      if(make_message) message = append(message, 'ESMF_ConfigLoadFile successful', DELIMITER)
       
-      call ESMF_ConfigPrint(config, rc=stat)
-      write(*, *) 'present: ', stat
+!      call ESMF_ConfigPrint(config, rc=stat)
+!      write(*, fmt=AIFMT ) 'present ', stat
 
-      label = LABEL1
-      call ESMF_ConfigFindLabel(config, label=trim(label), isPresent=is_present, rc=stat)
-      write(*, *) 'Label 1 ' // trim(label) // ' is present: ', is_present, ': ', stat
+      call ESMF_ConfigFindLabel(config, label=label_1, isPresent=is_present, rc=stat)
+      write(*, fmt=ALIFMT) 'Label 1 ' // label_1 // ' is present ', is_present, stat
       setup_succeeded = setup_succeeded .and. (stat == SUCCESS)
 
 !      call ESMF_ConfigGetAttribute(config, value=val, label=trim(label), rc=stat)
 !      if(stat /= SUCCESS) error stop "Cannot get attribute 1: " // trim(label)
 !      write(*, fmt='(A, " ", A)') trim(label), trim(val)
 
-      label = LABEL2
-      call ESMF_ConfigFindLabel(config, label=trim(label), isPresent=is_present, rc=stat)
-      write(*, *) 'Label 2 ' // trim(label) // ' is present: ', is_present, ': ', stat
+      call ESMF_ConfigFindLabel(config, label=label_2, isPresent=is_present, rc=stat)
+      write(*, fmt=ALIFMT) 'Label 2 ' // label_2 // ' is present ', is_present, stat
       setup_succeeded = setup_succeeded .and. (stat == SUCCESS)
 !      call ESMF_ConfigGetAttribute(config, value=val, label=trim(label), rc=stat)
 !      if(stat /= SUCCESS) error stop "Cannot get attribute 2: " // trim(label)
 !      write(*, fmt='(A, " ", A)') trim(label), trim(val)
 
-      label = LABEL3
-      call ESMF_ConfigFindLabel(config, label=trim(label), isPresent=is_present, rc=stat)
-      write(*, *) 'Label 3 ' // trim(label) // ' is present: ', is_present, ': ', stat
+      call ESMF_ConfigFindLabel(config, label=label_3, isPresent=is_present, rc=stat)
+      write(*, fmt=ALIFMT) 'Label 3 ' // label_3 // ' is present ', is_present, stat
       setup_succeeded = setup_succeeded .and. (stat == SUCCESS)
 !      call ESMF_ConfigGetAttribute(config, value=val, label=trim(label), rc=stat)
 !      if(stat /= SUCCESS) error stop "Cannot get attribute 3: " // trim(label)
@@ -167,7 +170,7 @@ contains
          rc = FAILURE
       end if
 
-      write(*, *) 'END SETUP ====================================================================='
+      write(*, fmt=AFMT) 'END SETUP ====================================================================='
 
    end subroutine setup
 
@@ -211,23 +214,23 @@ contains
 !
 !   end subroutine write_attributes
 
-   subroutine test_overwrite(config, label1, value_set, label2, rc)
+   subroutine test_overwrite(config, label1, newval, label2, rc)
       type(ESMF_Config), intent(inout) :: config
-      character(len=*), intent(in) :: label1, value_set, label2
+      character(len=*), intent(in) :: label1, newval, label2
       integer, intent(out) :: rc
       logical :: is_present, test_succeeded
       integer :: stat
 
-      write(*, *) 'TEST =========================================================================='
-      call ESMF_ConfigSetAttribute(config, value=trim(value_set), label=trim(label1), rc=stat)
-      write(*, *) 'Set label1 ' // trim(label1) // ' = ', trim(value_set) // ': ', stat
+      write(*, fmt=AFMT) 'TEST =========================================================================='
+      call ESMF_ConfigSetAttribute(config, value=newval, label=label1, rc=stat)
+      write(*, fmt=AIFMT) 'Set label1 ' // label1 // ' = ' // newval, stat
       test_succeeded = (stat == SUCCESS)
 !      test_failed = failed(stat, SUCCESS, rc)
 !      if(test_failed) then
 !         message = 'Failed to set ' // label1
 !      else
-         call ESMF_ConfigFindLabel(config, label=trim(label2), isPresent=is_present, rc = stat)
-         write(*, *) 'Label2 ' // trim(label2) // ': ', is_present, ': ', stat
+         call ESMF_ConfigFindLabel(config, label=label2, isPresent=is_present, rc = stat)
+         write(*, fmt=ALIFMT) 'Label2 ' // label2 // ' is present ', is_present, stat
          test_succeeded = test_succeeded .and. (stat == SUCCESS)
 !         test_failed = failed(stat, SUCCESS, rc)
 !      end if
@@ -241,7 +244,7 @@ contains
 
 !      passed = .not. test_failed
 
-      write(*, *) 'END TEST ===================================================================='
+      write(*, fmt=AFMT) 'END TEST ======================================================================'
 
       if(test_succeeded) then
          rc = SUCCESS
@@ -289,29 +292,42 @@ contains
 !
 !   end function append_character
 
-   logical function succeeded(stat, current)
-      integer, intent(in) :: stat
-      logical, optional, intent(in) :: current
-      logical :: current_
+!   logical function succeeded(stat, current)
+!      integer, intent(in) :: stat
+!      logical, optional, intent(in) :: current
+!      logical :: current_
+!
+!      if(present(current)) then
+!         current_ = current
+!      else
+!         current_ = .TRUE.
+!      endif
+!
+!      succeeded = current_ .and. (stat == SUCCESS)
+!
+!   end function succeeded
+!   
+!   integer function make_stat(lval)
+!      logical, intent(in) :: lval
+!
+!      if(lval) then
+!         make_stat = SUCCESS
+!      else
+!         make_stat = FAILURE
+!      end if
+!   end function make_stat
 
-      if(present(current)) then
-         current_ = current
+   function quote(s, use_double) result(quoted)
+      character(len=*), intent(in) :: s
+      logical, optional, intent(in) :: use_double
+      character(len=:), allocatable :: quoted
+
+      if(present(use_double) .and. use_double) then
+         quoted = '"' // s // '"'
       else
-         current_ = .TRUE.
-      endif
-
-      succeeded = current_ .and. (stat == SUCCESS)
-
-   end function succeeded
-   
-   integer function make_stat(lval)
-      logical, intent(in) :: lval
-
-      if(lval) then
-         make_stat = SUCCESS
-      else
-         make_stat = FAILURE
+         quoted = "'" // s // "'"
       end if
-   end function make_stat
+
+   end function quote
 
 end program overwrite_config_fulltest
