abbc23fa4d3f08b5c8608b7a92f2638159ffc225
Darian Boggs <william.d.boggs@nasa.gov>
2023-11-20 11:40:35 -0500

    Remove unused procedures

--- field_utils/udunits2.F90
+++ field_utils/udunits2.F90
@@ -9,22 +9,4 @@ module udunits2mod
 #include "udunits2types.h"
 #include "udunits2interfaces.h"
 
-contains
-
-   logical true(n, success)
-      integer(c_int), intent(in) :: n
-      integer, optional, intent(in) :: success
-      
-      true = merge(n == success, n /= 0, present(success))
-
-   end function true
-
-   character(kind=c_char, len=MAXLEN) &
-   function cstring(fstring)
-       character(len=*) :: fstring
-       
-       cstring = fstring // c_null_char
-
-   end function cstring
-
 end module udunits2mod
