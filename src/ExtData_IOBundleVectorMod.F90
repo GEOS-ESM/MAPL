64fdf4fd15f09624cac0e7099ef31f6943165763
mathomp4 <fortran@gmail.com>
2023-12-06 16:03:30 +0000

    Deploying to gh-pages from @ GEOS-ESM/MAPL@9e0f2d18be728e626deff8cb83359d663c8aa6c7 🚀

--- src/ExtData_IOBundleVectorMod.F90
+++ src/ExtData_IOBundleVectorMod.F90
@@ -1,10 +1,10 @@
-module MAPL_ExtData_IOBundleVectorMod
-  use MAPL_ExtData_IOBundleMod
+module MAPL_ExtDataNG_IOBundleVectorMod
+  use MAPL_ExtDataNG_IOBundleMod
   
-#define _type type(ExtData_IoBundle)
-#define _vector IoBundleVector
-#define _iterator IoBundleVectorIterator
+#define _type type(ExtDataNG_IoBundle)
+#define _vector IoBundleNGVector
+#define _iterator IoBundleNGVectorIterator
 
 #include "templates/vector.inc"
 
-end module MAPL_ExtData_IOBundleVectorMod
+end module MAPL_ExtDataNG_IOBundleVectorMod
