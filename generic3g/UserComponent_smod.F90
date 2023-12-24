f1d9f5c195ab6edd007b5ece5af7764343b2873f
Tom Clune <thomas.l.clune@nasa.gov>
2023-12-23 22:00:34 -0500

    Baby steps.

---
+++ generic3g/UserComponent_smod.F90
@@ -0,0 +1,91 @@
+#include "MAPL_ErrLog.h"
+
+submodule(mapl3g_UserComponent) UserComponent_run_smod
+   use :: mapl_ErrorHandling
+   use :: mapl3g_OuterMetaComponent
+   use :: mapl3g_MethodPhasesMapUtils
+   use :: mapl_KeywordEnforcer
+   implicit none
+
+contains
+
+   module subroutine run_self(this, clock, unusable, phase_idx, rc)
+      class(UserComponent), intent(inout) :: this
+      type(ESMF_Clock), intent(inout) :: clock
+      class(KeywordEnforcer), optional, intent(in) :: unusable
+      integer, optional, intent(in) :: phase_idx
+      integer, optional, intent(out) :: rc
+
+      integer :: status, userRC
+
+      associate ( &
+           importState => this%states%importState, &
+           exportState => this%states%exportState)
+
+        call ESMF_GridCompRun(this%gridcomp, &
+             importState=importState, &
+             exportState=exportState, &
+             clock=clock, &
+             phase=phase_idx, userRC=userRC, _RC)
+        _VERIFY(userRC)
+      end associate
+
+      _RETURN(_SUCCESS)
+      _UNUSED_DUMMY(unusable)
+   end subroutine run_self
+
+   recursive module subroutine initialize_self(this, clock, unusable, phase_idx, rc)
+      class(UserComponent), intent(inout) :: this
+      type(ESMF_Clock), intent(inout) :: clock
+      class(KeywordEnforcer), optional, intent(in) :: unusable
+      integer, optional, intent(in) :: phase_idx
+      integer, optional, intent(out) :: rc
+
+      integer :: status, userRC
+
+      associate ( &
+           importState => this%states%importState, &
+           exportState => this%states%exportState)
+
+        call ESMF_GridCompInitialize(this%gridcomp, &
+             importState=importState, exportState=exportState, clock=clock, &
+             phase=phase_idx, userRC=userRC, _RC)
+        _VERIFY(userRC)
+
+      end associate
+
+      _RETURN(_SUCCESS)
+      _UNUSED_DUMMY(unusable)
+   end subroutine initialize_self
+
+   module subroutine finalize_self(this, clock, unusable, phase_idx, rc)
+      class(UserComponent), intent(inout) :: this
+      type(ESMF_Clock), intent(inout) :: clock
+      class(KeywordEnforcer), optional, intent(in) :: unusable
+      integer, optional, intent(in) :: phase_idx
+      integer, optional, intent(out) :: rc
+
+      integer :: status, userRC
+
+      associate ( &
+           importState => this%states%importState, &
+           exportState => this%states%exportState)
+
+        call ESMF_GridCompFinalize(this%gridcomp, &
+             importState=importState, exportState=exportState, clock=clock, &
+             phase=phase_idx, userRC=userRC, _RC)
+        _VERIFY(userRC)
+      end associate
+
+      _RETURN(_SUCCESS)
+      _UNUSED_DUMMY(unusable)
+   end subroutine finalize_self
+
+   module function get_states(this) result(states)
+      type(MultiState) :: states
+      class(UserComponent), intent(in) :: this
+
+      states = this%states
+   end function get_states
+
+end submodule UserComponent_run_smod
