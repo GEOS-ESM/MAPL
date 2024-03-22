7e24292ad0b626943735e1d9f835463c0297c415
Benjamin Auer <benjamin.m.auer@nasa.gov>
2024-03-22 12:01:38 -0400

    one fix

--- gridcomps/History/MAPL_HistoryGridComp.F90
+++ gridcomps/History/MAPL_HistoryGridComp.F90
@@ -5188,30 +5188,57 @@ ENDDO PARSER
     _RETURN(ESMF_SUCCESS)
   end subroutine RecordRestart
 
-  subroutine  checkIfStateHasField(state, fieldName, hasField, rc)
+  subroutine  checkIfStateHasField(state, input_fieldName, hasField, rc)
     type(ESMF_State), intent(in) :: state ! export state
-    character(len=*), intent(in) :: fieldName
+    character(len=*), intent(in) :: input_fieldName
     logical, intent(out)         :: hasField
     integer, intent(out), optional :: rc ! Error code:
 
-    integer :: n, i, status
+    integer :: n, i, status, p_index
     character (len=ESMF_MAXSTR), allocatable  :: itemNameList(:)
     type(ESMF_StateItem_Flag),   allocatable  :: itemTypeList(:)
+    character(len=:),allocatable :: field_name,bundle_name
+    logical :: is_bundle,isPresent
+    type(ESMF_FieldBundle) :: bundle
 
     call ESMF_StateGet(state, itemcount=n,  _RC)
 
     allocate(itemNameList(n), _STAT)
     allocate(itemTypeList(n), _STAT)
     call ESMF_StateGet(state,itemnamelist=itemNamelist,itemtypelist=itemTypeList,_RC)
+    p_index = index(input_fieldName,"%")
+    if (p_index/=0) then
+       is_bundle = .true.
+       bundle_name = input_fieldName(1:p_index-1)
+       field_name = input_fieldName(p_index+1:)
+    else
+       is_bundle = .false.
+       field_name = input_fieldName
+    end if
 
     hasField = .false.
-    do I=1,N
-       if(itemTypeList(I)/=ESMF_STATEITEM_FIELD) cycle
-       if(itemNameList(I)==fieldName) then
-          hasField = .true.
-          exit
-       end if
-    end do
+    if (is_bundle) then
+      do I=1,N
+         if(itemTypeList(I)/=ESMF_STATEITEM_FIELDBUNDLE) cycle
+         if(itemNameList(I)==bundle_name) then
+            call ESMF_StateGet(state,bundle_name,bundle,_RC)
+            call ESMF_FieldBundleGet(bundle,field_name,isPresent=isPresent,_RC)
+            if (isPresent) then
+               hasField = .true.
+               exit
+            end if
+         end if
+      end do
+
+    else
+      do I=1,N
+         if(itemTypeList(I)/=ESMF_STATEITEM_FIELD) cycle
+         if(itemNameList(I)==field_name) then
+            hasField = .true.
+            exit
+         end if
+      end do
+    end if
     deallocate(itemNameList, _STAT)
     deallocate(itemTypeList, _STAT)
 
