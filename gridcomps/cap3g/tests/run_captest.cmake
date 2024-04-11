271552d86872a93b63bbb9ec7f965b03f70908eb
Benjamin Auer <benjamin.m.auer@nasa.gov>
2024-04-11 14:41:45 -0400

    add test

---
+++ gridcomps/cap3g/tests/run_captest.cmake
@@ -0,0 +1,20 @@
+macro(run_case CASE)
+    string(RANDOM LENGTH 24 tempdir)
+    execute_process(
+      COMMAND ${CMAKE_COMMAND} -E make_directory ${tempdir}
+      COMMAND ${CMAKE_COMMAND} -E copy_directory ${CMAKE_CURRENT_LIST_DIR}/${CASE} ${tempdir}
+      )
+    set(num_procs "1")
+    execute_process(
+      COMMAND ${MPIEXEC_EXECUTABLE} ${MPIEXEC_NUMPROC_FLAG} ${num_procs} ${MPIEXEC_PREFLAGS} ${MY_BINARY_DIR}/GEOS.x cap.yaml
+      RESULT_VARIABLE CMD_RESULT
+      WORKING_DIRECTORY ${tempdir}
+      )
+    execute_process(
+      COMMAND ${CMAKE_COMMAND} -E rm -rf ${tempdir}
+      )
+    if(CMD_RESULT)
+       message(FATAL_ERROR "Error running ${CASE}")
+    endif()
+endmacro()
+run_case(${TEST_CASE})
