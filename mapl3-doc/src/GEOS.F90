aa009a34f204daa2967eeaffbe062a0a1a584ad3
tclune <thomas.l.clune@nasa.gov>
2024-04-18 20:31:09 +0000

    Deploying to gh-pages from @ GEOS-ESM/MAPL@ae36076a1ce560e417c857ec75e90440ca39b2e2 🚀

--- mapl3-doc/src/GEOS.F90
+++ mapl3-doc/src/GEOS.F90
@@ -2,37 +2,65 @@
 #include "MAPL_Generic.h"
 
 program geos
-   use mapl3g
-   use mapl_ErrorHandling
+   use mapl3
    use esmf
-   use pflogger, only: pflogger_initialize => initialize
    implicit none
 
    integer :: status
    type(ESMF_Config) :: config
-   type(ESMF_HConfig) :: hconfig
 
-   call ESMF_Initialize(configFileNameFromArgNum=1, configKey=['esmf'], config=config, _RC)
-   call pflogger_initialize()
-   call ESMF_ConfigGet(config, hconfig=hconfig, _RC)
-   call run_geos(hconfig, _RC)
-   call ESMF_Finalize(_RC)
+   call initialize(config=config, _RC)
+   call run_geos(config, _RC)
+   call finalize(config=config, _RC)
 
 contains
 
 #undef I_AM_MAIN
 #include "MAPL_Generic.h"
 
-   subroutine run_geos(hconfig, rc)
-      type(ESMF_HConfig), intent(inout) :: hconfig
+   subroutine initialize(config, rc)
+      type(ESMF_Config), intent(out) :: config
       integer, optional, intent(out) :: rc
+      
+      integer :: status
+      type(ESMF_HConfig) :: hconfig, mapl_hconfig
+      logical :: has_mapl_section
+
+      call ESMF_Initialize(configFilenameFromArgNum=1, configKey=['esmf'], config=config, _RC)
+      call ESMF_ConfigGet(config, hconfig=hconfig, _RC)
+      has_mapl_section = ESMF_HConfigIsDefined(hconfig, keystring='mapl', _RC)
+      if (has_mapl_section) then
+         mapl_hconfig = ESMF_HConfigCreateAt(hconfig, keystring='mapl', _RC)
+      end if
+      call MAPL_Initialize(mapl_hconfig=mapl_hconfig, _RC)
+
+      _RETURN(_SUCCESS)
+   end subroutine initialize
+
+   subroutine run_geos(config, rc)
+      type(ESMF_Config), intent(inout) :: config
+      integer, optional, intent(out) :: rc
+
+      type(ESMF_HConfig) :: cap_hconfig
       integer :: status
 
-      !call MAPL_initialize(hconfig, _RC)
-      call MAPL_run_driver(hconfig, _RC)
-      !call MAPL_finalize(_RC)
+      call ESMF_ConfigGet(config, hconfig=cap_hconfig, _RC)
+      call MAPL_run_driver(cap_hconfig, _RC)
 
       _RETURN(_SUCCESS)
    end subroutine run_geos
+
+   subroutine finalize(config, rc)
+      type(ESMF_Config), intent(inout) :: config
+      integer, optional, intent(out) :: rc
+
+      integer :: status
+      
+      call MAPL_Finalize(_RC)
+      call ESMF_ConfigDestroy(config, _RC)
+      call ESMF_Finalize(_RC)
+
+      _RETURN(_SUCCESS)
+   end subroutine finalize
 
 end program geos
--- mapl3-doc/src/mapl3g.F90
+++ mapl3-doc/src/mapl3g.F90
@@ -1,4 +1,13 @@
-module mapl3g
+! Public interface (package) to MAPL3
+module mapl3
+   use mapl3g_MaplFramework
    use generic3g
    use mapl3g_cap
-end module mapl3g
+   use mapl_ErrorHandling
+   
+
+   ! We use default PUBLIC to avoid explicitly listing exports from
+   ! the other layers.  When the dust settles and such micro
+   ! management become feasible, this can be reconsidered.
+   
+end module mapl3
