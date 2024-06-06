2e44403990d371d445aba5e5b6ec0fbd178e3dc4
Ricardo Todling <ricardo.todling@nasa.gov>
2024-06-05 21:17:30 -0400

    clean version

--- base/MAPL_DefGridName.F90
+++ base/MAPL_DefGridName.F90
@@ -5,26 +5,17 @@ logical,intent(in)::iamroot
 character(len=*),intent(out)::gridname
 character(len=2) poletype
 character(len=3) llcb
-character(len=30) myfmt
-character(len=2) ii,jj
+character(len=30) imstr,jmstr
 poletype='PC'
 if(mod(jm,2)==0) poletype='PE'
 
 llcb='-DC' ! lat-lon
 if(6*im==jm) llcb='-CF' ! cubed
 
-! better than it was before, but still dummy
-myfmt='null'
-if(im>    10.and.im<100)   ii='i2'
-if(im>   100.and.im<1000)  ii='i3'
-if(im>  1000.and.im<10000) ii='i4'
-if(im> 10000.and.im<100000)ii='i5'
-!
-if(jm>    10.and.jm<100)   jj='i2'
-if(jm>   100.and.jm<1000)  jj='i3'
-if(jm>  1000.and.jm<10000) jj='i4'
-if(jm> 10000.and.jm<100000)jj='i5'
+write(imstr,*) im
+write(jmstr,*) jm
 
-myfmt = '(a,'//ii//',a,'//jj//',a)'
-write(gridname,fmt=trim(myfmt)) trim(poletype),im,'x',jm,trim(llcb)
+gridname=trim(poletype)//trim(adjustl(imstr))//'x'//&
+                         trim(adjustl(jmstr))//trim(llcb)
+
 end subroutine MAPL_DefGridName
