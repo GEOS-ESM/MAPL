bf16e2506dd596e6aad3c29d4c86b5c0d4064591
Jules Kouatchou <Jules.Kouatchou-1@nasa.gov>
2024-06-11 16:19:50 -0400

    First attempt to break OuterMetaComponent.F90 into submodule files

---
+++ generic3g/OuterMetaComponent/get_internal_state.F90
@@ -0,0 +1,20 @@
+#include "MAPL_Generic.h"
+
+submodule (mapl3g_OuterMetaComponent) get_internal_state_smod
+   implicit none
+
+contains
+
+   !TODO: put "user" in procedure name
+   module function get_internal_state(this) result(internal_state)
+      type(ESMF_State) :: internal_state
+      class(OuterMetaComponent), intent(in) :: this
+
+      type(MultiState) :: user_states
+
+      user_states = this%user_gc_driver%get_states()
+      internal_state = user_states%internalState
+
+   end function get_internal_state
+
+end submodule get_internal_state_smod
---
+++ generic3g/OuterMetaComponent/get_registry.F90
@@ -0,0 +1,15 @@
+#include "MAPL_Generic.h"
+
+submodule (mapl3g_OuterMetaComponent) get_registry_smod
+   implicit none
+
+contains
+
+   module function get_registry(this) result(registry)
+      type(HierarchicalRegistry), pointer :: registry
+      class(OuterMetaComponent), target, intent(in) :: this
+
+      registry => this%registry
+   end function get_registry
+
+end submodule get_registry_smod
