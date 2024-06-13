95e61a94a27ac111b51d24694946127152b3cb78
tclune <thomas.l.clune@nasa.gov>
2024-06-13 13:13:22 +0000

    Deploying to gh-pages from @ GEOS-ESM/MAPL@d47dd3ac1c2937b602d751b3a1b3d161252dd762 🚀

---
+++ mapl3-doc/src/free_outer_meta.F90
@@ -0,0 +1,27 @@
+#include "MAPL_Generic.h"
+
+submodule (mapl3g_OuterMetaComponent) free_outer_meta_smod
+   implicit none
+
+contains
+
+   module subroutine free_outer_meta(gridcomp, rc)
+      type(ESMF_GridComp), intent(inout) :: gridcomp
+      integer, optional, intent(out) :: rc
+
+      integer :: status
+      type(OuterMetaWrapper) :: wrapper
+      type(ESMF_GridComp) :: user_gridcomp
+
+      call MAPL_UserCompGetInternalState(gridcomp, OUTER_META_PRIVATE_STATE, wrapper, status)
+      _ASSERT(status==ESMF_SUCCESS, "OuterMetaComponent not created for this gridcomp")
+
+      user_gridcomp = wrapper%outer_meta%user_gc_driver%get_gridcomp()
+      call free_inner_meta(user_gridcomp, _RC)
+
+      deallocate(wrapper%outer_meta)
+
+      _RETURN(_SUCCESS)
+   end subroutine free_outer_meta
+
+end submodule free_outer_meta_smod
