2bb254daf64531b619b7516b0cc08ed7008f95ff
Darian Boggs <william.d.boggs@nasa.gov>
2024-07-01 14:08:27 -0400

    Create Cap for tests

--- Tests/GridCompCreationDriver.F90
+++ Tests/GridCompCreationDriver.F90
@@ -1,12 +1,29 @@
-program GridCompCreationDriver.F90
+#define I_AM_MAIN
+#include "MAPL_Generic.h"
+program grid_comp_creation_driver
 
 !_   use, intrinsic :: iso_fortran_env, only: int32, int64, real32, real64
+   use MPI
+   use ESMF
+   use gridcomp_creation_cap
+   use MAPL
+
    implicit none
-   private
-!_   public ::
 !_ INTERFACES
 !_ TYPES
 !_ VARIABLES
-!_contains
+   integer :: status
+   character(len=:), parameter :: Iam="GridCompCreationDriver"
+   character(len=:), parameter :: CAPNAME = 'GridCompCreationCap'
+   type(MAPL_CapOptions) :: cap_options
+   type(MAPL_FargparseCLI) :: cli
 
-end program GridCompCreationDriver.F90
+   cli = MAPL_FargparseCLI()
+   cap_options = MAPL_CapOptions(CLI)
+
+   call initialize_cap(CAPNAME, Root_SetServices, options=cap_options, _RC) 
+   call run_cap(_RC)
+   stop
+
+!_contains
+end program grid_comp_creation_driver
