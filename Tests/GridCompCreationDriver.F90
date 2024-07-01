228ad7b8c3edae4f728b206d3ff7e6b2be6632ea
Darian Boggs <william.d.boggs@nasa.gov>
2024-07-01 12:30:18 -0400

    Create driver for GridCompCreation testing

---
+++ Tests/GridCompCreationDriver.F90
@@ -0,0 +1,12 @@
+program GridCompCreationDriver.F90
+
+!_   use, intrinsic :: iso_fortran_env, only: int32, int64, real32, real64
+   implicit none
+   private
+!_   public ::
+!_ INTERFACES
+!_ TYPES
+!_ VARIABLES
+!_contains
+
+end program GridCompCreationDriver.F90
