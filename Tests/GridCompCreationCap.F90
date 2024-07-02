fa5a70e18348d068a7a9be0433c8bc43aebb2a5a
Darian Boggs <william.d.boggs@nasa.gov>
2024-07-02 11:10:36 -0400

    Additional functionality

--- Tests/GridCompCreationCap.F90
+++ Tests/GridCompCreationCap.F90
@@ -1,17 +1,23 @@
 #include "MAPL_Generic.h"
 module grid_comp_creation_cap
 
-!_   use, intrinsic :: iso_fortran_env, only: int32, int64, real32, real64
+   use, intrinsic :: iso_fortran_env, only: int32, int64, real32, real64
    implicit none
    private
-!_   public ::
-!_ INTERFACES
-!_ TYPES
-!_ VARIABLES
-   type(ESMF_LogKindFlag), parameter :: ESMF_LOGGING_MODE = ESMF_LOGKIND_NONE
+
+   type :: TestParameters
+      type(ESMF_LogKind_Flag), parameter :: esmf_logging_mode = ESMF_LOGKIND_NONE
+      character(len=*), allocatable :: name
+      integer :: npes_model = -1
+      type(TestParameters), allocatable :: subtest_parameters => null()
+      character(:), allocatable :: logging_config
+   end type TestParameters
+
+   interface TestParameters
+      module procedure :: construct_test_parameters
+   end interface TestParameters
 
    procedure, pointer :: cap_set_services => null()
-   type(MAPL_CapOptions) :: cap_options = MAPL_CapOptions()
    type(ServerManager) :: cap_server
    type(SplitCommunicator) :: split_comm
    integer :: rank
@@ -21,29 +27,37 @@ module grid_comp_creation_cap
 
 contains
 
-   subroutine initialize_cap(name, set_services, unusable, options, rc)
+   function construct_test_parameters(name, npes_model) result(parameters)
+      Type(TestParameters) :: parameters
       character(len=*), intent(in) :: name
+      integer, intent(in) :: npes_model
+
+      parameters%name = name
+      parameters%npes_model = npes_model
+
+   end function construct_test_parameters
+
+   subroutine initialize_cap(set_services, parameters, unusable, rc)
       procedure, intent(in) :: set_services
+      class(TestParameters), intent(inout) :: parameters
       class(KeywordEnforcer), optional, intent(in) :: unusable
-      type(MAPL_CapOptions), optional, intent(in) :: options
       integer, optional, intent(out) :: rc
       integer :: status
 
       _UNUSED_DUMMY(unusable)
-      cap_name = name
+      cap_name = parameters%name
       cap_set_services => set_services
-      if(present(options)) cap_options = options
       call initialize_mpi(_RC)
-      call MAPL_Initialize(comm=MPI_COMM_WORLD, logging_config=cap_options%logging_config, _RC)
+      call MAPL_Initialize(comm=MPI_COMM_WORLD, logging_config=parameters%logging_config, _RC)
       _RETURN(_SUCCESS)
 
    end subroutine initialize_cap
 
-   subroutine initialize_mpi(rc) 
+   subroutine initialize_mpi(parameters, rc) 
+      type(TestParameters), intent(in) :: parameters
       integer, optional, intent(out) :: rc
       integer :: ierror
       integer :: npes_world
-
 
       call MPI_Init(ierror)
 
@@ -51,11 +65,11 @@ contains
       call MPI_Comm_rank(comm_world, rank, ierror); _VERIFY(ierror)
       call MPI_Comm_size(comm_world, npes_world, ierror); _VERIFY(ierror)
 
-      if (cap_options%npes_model == -1) then
-         ! just a feed back to cap_options to maintain integrity
-          cap_options%npes_model = npes_world
+      if (parameters%npes_model == -1) then
+         ! just a feed back to parameters to maintain integrity
+          parameters%npes_model = npes_world
       endif
-      _ASSERT(npes_world >= cap_options%npes_model, "npes_world is smaller than npes_model")
+      _ASSERT(npes_world >= parameters%npes_model, "npes_world is smaller than npes_model")
 
       _RETURN(_SUCCESS)
 
@@ -83,28 +97,35 @@ contains
 
    end subroutine finalize_cap
 
-   subroutine run_cap(rc)
+   subroutine run_cap(parametes, rc)
+      type(TestParameters), intent(in) :: parameters
       integer, optional, intent(out) :: rc
       integer :: status
       character(len=:), parameter :: SPLITCOMM_NAME = 'model'
-      character(len=:), parameter :: CAP_FILE = 'GridCompCreationCap.rc'
+      character(len=:), parameter :: CAP_FILE = 'GridCompCreationCap.yaml'
       integer :: comm_cap
       type(SplitCommunicator) :: split_comm
-      type (ESMF_VM) :: vm
+      type(ESMF_VM) :: vm
 
       comm_cap = MPI_COMM_WORLD
       call cap_server%get_splitcomm(split_comm)
       if(split%get_name() == SPLITCOMM_NAME) then
-         call ESMF_Initialize (vm=vm, logKindFlag=cap_options%esmf_logging_mode, &
+         call ESMF_Initialize(vm=vm, logKindFlag=parameters%esmf_logging_mode,&
             & mpiCommunicator=split_comm%get_subcommunicator(), _RC)
-         config = ESMF_ConfigCreate(_RC)
-         call ESMF_ConfigLoadFile(config, CAP_FILE, _RC)
-
+         call read_test_parameters(CAP_FILE, _RC)
       end if
-
-
+      ! Read HConfig
       call finalize_cap(comm_cap, _RC)
 
    end subroutine run_cap
 
+   subroutine read_test_parameters(filename, rc)
+      character(len=*), intent(in) :: filename
+      integer, optional, intent(out) :: rc
+      integer :: status
+
+      hconfig = ESMF_HConfigCreate(filename, _RC)
+      call ESMF_HConfigDestroy(hconfig, _RC)
+   end subroutine read_test_parameters
+      
 end module grid_comp_creation_cap
--- Tests/GridCompCreationDriver.F90
+++ Tests/GridCompCreationDriver.F90
@@ -2,26 +2,22 @@
 #include "MAPL_Generic.h"
 program grid_comp_creation_driver
 
-!_   use, intrinsic :: iso_fortran_env, only: int32, int64, real32, real64
+   use, intrinsic :: iso_fortran_env, only: int32, int64, real32, real64
    use MPI
    use ESMF
    use gridcomp_creation_cap
    use MAPL
-
    implicit none
-!_ INTERFACES
-!_ TYPES
-!_ VARIABLES
+
    integer :: status
    character(len=:), parameter :: Iam="GridCompCreationDriver"
    character(len=:), parameter :: CAPNAME = 'GridCompCreationCap'
-   type(MAPL_CapOptions) :: cap_options
-   type(MAPL_FargparseCLI) :: cli
+   character(len=:), parameter :: TESTNAME = 'GridCompCreationTest'
+   integer :: npes_model = 20
+   type(TestParameters) :: parameters
 
-   cli = MAPL_FargparseCLI()
-   cap_options = MAPL_CapOptions(CLI)
-
-   call initialize_cap(CAPNAME, Root_SetServices, options=cap_options, _RC) 
+   parameters = TestParameters(TESTNAME, npes_model)
+   call initialize_cap(Root_SetServices, parameters, _RC) 
    call run_cap(_RC)
    stop
 
