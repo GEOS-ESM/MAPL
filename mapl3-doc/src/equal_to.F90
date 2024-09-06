680e7c3ba3fed1f443ffa6f5d877543ddec2acd5
mathomp4 <fortran@gmail.com>
2024-09-06 15:44:31 +0000

    Deploying to gh-pages from @ GEOS-ESM/MAPL@7d3348aa8b12f5714d62058a640774014e8db621 🚀

--- mapl3-doc/src/equal_to.F90
+++ mapl3-doc/src/equal_to.F90
@@ -1,30 +1,29 @@
 #include "MAPL_ErrLog.h"
 
-submodule (mapl3g_LatLonGeomSpec) equal_to_smod
-   use mapl3g_CoordinateAxis
-   use mapl3g_GeomSpec
-   use pfio
-   use MAPL_RangeMod
-   use MAPLBase_Mod
-   use mapl_ErrorHandling
-   use esmf
+submodule (mapl3g_LatLonDecomposition) equal_to_smod
+   use mapl_ErrorHandlingMod
+   use MAPL_Base
    implicit none (type, external)
-   
+
 contains
 
-   pure logical module function equal_to(a, b)
-      class(LatLonGeomSpec), intent(in) :: a
-      class(GeomSpec), intent(in) :: b
+   elemental module function equal_to(decomp1, decomp2)
+      logical :: equal_to
+      type(LatLonDecomposition), intent(in) :: decomp1
+      type(LatLonDecomposition), intent(in) :: decomp2
 
-      select type (b)
-      type is (LatLonGeomSpec)
-         equal_to = (a%lon_axis == b%lon_axis) .and. (a%lat_axis == b%lat_axis)
-         if (.not. equal_to) return
-         equal_to = (a%decomposition == b%decomposition)
-      class default
-         equal_to = .false.
-      end select
+      equal_to = size(decomp1%lon_distribution) == size(decomp2%lon_distribution)
+      if (.not. equal_to) return
+
+      equal_to = size(decomp1%lat_distribution) == size(decomp2%lat_distribution)
+      if (.not. equal_to) return
+
+      equal_to = all(decomp1%lon_distribution == decomp2%lon_distribution)
+      if (.not. equal_to) return
+
+      equal_to = all(decomp1%lat_distribution == decomp2%lat_distribution)
 
    end function equal_to
 
 end submodule equal_to_smod
+
