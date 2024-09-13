cdd4dd8ff7691991bc9ee277a9343303e1567dc0
Darian Boggs <william.d.boggs@nasa.gov>
2024-09-13 17:09:06 -0400

    Testing

--- field_utils/FieldCondensedArrayDims.F90
+++ field_utils/FieldCondensedArrayDims.F90
@@ -5,24 +5,24 @@ module mapl3g_FieldCondensedArrayDims
    public :: FieldCondensedArrayDims
 
    type :: FieldCondensedArrayDims
-      private
       integer :: horz_(2)
       integer :: vert_
       integer, allocatable :: ungridded_(:)
       integer :: dims_(3)
+      integer :: horizontal
+      integer :: vertical
+      integer :: ungridded
    contains
-      procedure :: horizontal
-      procedure :: vertical
-      procedure :: ungridded
-      procedure :: dims
       procedure :: arguments
+      procedure :: initialize
+      procedure :: reset
    end type FieldCondensedArrayDims
 
    interface FieldCondensedArrayDims
       module procedure :: construct
       module procedure :: construct_dimcount0
       module procedure :: construct_vert
-      module procedure :: construct_1h
+      module procedure :: construct_surface
    end interface FieldCondensedArrayDims
 
 contains
@@ -38,87 +38,49 @@ contains
    function construct_vert(k, w) result(cadims)
       type(FieldCondensedArrayDims) :: cadims
       integer, intent(in) :: k
-      integer, optional, intent(in) w(:)
+      integer, optional, intent(in) :: w(:)
 
       cadims = FieldCondensedArrayDims(0, 0, k, w)
 
    end function construct_vert
 
-   function construct_1h(u, z, nox, w)
+   function construct_surface(x, y, w) result(cadims)
       type(FieldCondensedArrayDims) :: cadims
-      integer, intent(in) :: u, z
-      logical, intent(in) :: nox
+      integer, intent(in) :: x, y
       integer, optional, intent(in) :: w(:)
-      integer :: x, y
 
-      x = 1
-      y = 0
-      if(nox) then
-         x = 0
-         y = 1
-      end if
+      cadims = FieldCondensedArrayDims(x, y, 0, w)
 
-      cadims = FieldCondensedArrayDims(x, y, z, w)
-
-   end function construct_1h
+   end function construct_surface
 
    function construct(x, y, z, w) result(cadims)
       type(FieldCondensedArrayDims) :: cadims
       integer, intent(in) :: x, y
-      integer, optional, intent(in) :: z
+      integer, intent(in) :: z
       integer, optional, intent(in) :: w(:)
-      integer, allocatable :: w_(:)
+      integer :: dims_(3)
       integer :: i, j, k, n
       
-      w_ = [integer :: ]
-      if(present(w)) w_ = w
-      k = 0
-      if(present(z)) k = z
       cadims%horz_ = [x, y]
-      cadims%vert_ = k
-      cadims%ungridded_ = w_
-
+      cadims%vert_ = z
+      cadims%ungridded_ = [integer::]
       i = max(x, 1)
       j = max(y, 1)
-      k = max(k, 1)
-      n = 1
-      if(size(w_) > 0) n = product(max(w, 1))
+      k = max(z, 1)
 
-      cadims%dims_ = [i*j, k, n]
+      n = 1
+      if(present(w)) then
+         cadims%ungridded_ = w
+         n = product(max(w, 1))
+      end if
+
+      dims_ = [i*j, k, n]
+      cadims%dims_ = dims_
+      cadims%horizontal = dims_(1)
+      cadims%horizontal = dims_(2)
+      cadims%ungridded = dims_(3)
 
    end function construct
-
-   function horizontal(this) result(val)
-      integer :: val
-      class(FieldCondensedArrayDims), intent(in) :: this
-      
-      val = this%dims_[1]
-
-   end function horizontal
-   
-   function vertical(this) result(val)
-      integer :: val
-      class(FieldCondensedArrayDims), intent(in) :: this
-      
-      val = this%dims_[2]
-
-   end function vertical
-
-   function ungridded(this) result(val)
-      integer :: val
-      class(FieldCondensedArrayDims), intent(in) :: this
-      
-      val = this%dims_[3]
-
-   end function ungridded
-
-   function dims(this) result(val)
-      integer :: val(3)
-      class(FieldCondensedArrayDims), intent(in) :: this
-      
-      val = this%dims_
-
-   end function dims
 
    function arguments(this) result(val)
       integer, allocatable :: val(:)
@@ -127,7 +89,7 @@ contains
       
       size_ungridded = size(this%ungridded_)
       allocate(val(3+size_ungridded))
-      val(1:3) = [this%horz_(1), this%horz_(2), this%vert]
+      val(1:3) = [this%horz_(1), this%horz_(2), this%vert_]
       if(size_ungridded > 0) val(4:size(val)) =  this%ungridded_
 
    end function arguments
