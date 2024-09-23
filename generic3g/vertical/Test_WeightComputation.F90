8bed1762b03d647c15fedba2cafc66d4361b6e67
Purnendu Chakraborty <purnendu.chakraborty@nasa.gov>
2024-09-23 07:56:15 -0400

    Fixed routine name

--- generic3g/vertical/Test_WeightComputation.F90
+++ generic3g/vertical/Test_WeightComputation.F90
@@ -5,7 +5,7 @@ program Test_WeightComputation
 
    use mapl_ErrorHandling
    use mapl3g_CSR_SparseMatrix
-   use mapl3g_WeightComputation, only: get_weights_fixedlevels_to_fixedlevels_linear
+   use mapl3g_WeightComputation, only: compute_linear_map_fixedlevels_to_fixedlevels
    use, intrinsic :: iso_fortran_env, only: REAL32
 
    implicit none
@@ -17,7 +17,7 @@ program Test_WeightComputation
 
    src = [40., 30., 20., 10.]
    dst = [40., 32., 38., 25., 21., 13., 10.]
-   call get_weights_fixedlevels_to_fixedlevels_linear(src, dst, weights, _RC)
+   call compute_linear_map_fixedlevels_to_fixedlevels(src, dst, weights, _RC)
    print *, "dst:    ", dst
    print *, "result: ", matmul(weights, src)
 
