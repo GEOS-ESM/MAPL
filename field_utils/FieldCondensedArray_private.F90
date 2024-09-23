3c0a4a7b2414f6a712a524a799b811184b53c243
Darian Boggs <william.d.boggs@nasa.gov>
2024-09-23 11:48:44 -0400

    Rm comments and _HERE lines. Move assign_fptr.

--- field_utils/FieldCondensedArray_private.F90
+++ field_utils/FieldCondensedArray_private.F90
@@ -5,7 +5,7 @@ module mapl3g_FieldCondensedArray_private
    implicit none
 
    private
-   public :: get_fptr_shape
+   public :: get_fptr_shape, only: FieldGetLocalElementCount
 
 contains
 
@@ -24,31 +24,17 @@ contains
       
       vert_dim = 0
       vert_size = 1
-      _HERE, 'gridToFieldMap: ', gridToFieldMap
-      _HERE, 'localElementCount: ', localElementCount
-      _HERE, 'has_vertical: ', has_vertical
+      
       rank = size(localElementCount)
-      _HERE, 'rank: ', rank
       grid_dims = pack(gridToFieldMap, gridToFieldMap /= 0)
-      _HERE, 'grid_dims: ', grid_dims
-      _HERE, 'size(grid_dims): ', size(grid_dims)
-      _HERE, 'grid_dims <= size(grid_dims): ', (grid_dims <= size(grid_dims))
-      _HERE, 'all(grid_dims <= size(grid_dims)): ', all(grid_dims <= size(grid_dims))
       _ASSERT(all(grid_dims <= size(grid_dims)), 'MAPL expects geom dims before ungridded.')
-      _HERE
       if(has_vertical) vert_dim = 1 
       if(size(grid_dims) > 0) vert_dim = maxval(grid_dims) + vert_dim
       ungridded_dims = pack([(i,i=1,rank)], [(all([vert_dim, grid_dims] /= i), i=1, rank)])
-      _HERE, 'ungridded_dims: ', ungridded_dims
       horz_size = product([(localElementCount(grid_dims(i)), i=1, size(grid_dims))])
-      _HERE, 'horz_size: ', horz_size
       if(has_vertical) vert_size = localElementCount(vert_dim)
-!      vert_size = product([(localElementCount(vert_dims(i)), i=1, size(vert_dims))])
-      _HERE, 'vert_size: ', vert_size
       ungridded_size = product([(localElementCount(ungridded_dims(i)), i=1, size(ungridded_dims))])
-      _HERE, 'ungridded_size: ', ungridded_size
       fptr_shape = [horz_size, vert_size, ungridded_size]
-      _HERE, 'fptr_shape: ', fptr_shape
       _RETURN(_SUCCESS)
 
    end function get_fptr_shape
--- field_utils/FieldPointerUtilities.F90
+++ field_utils/FieldPointerUtilities.F90
@@ -1,8 +1,7 @@
 #include "MAPL_Generic.h"
 
 module MAPL_FieldPointerUtilities
-   use mapl3g_output_info, only: get_vertical_dim_spec_name
-   use mapl3g_FieldCondensedArray_private, only: get_fptr_shape_private => get_fptr_shape
+!   use mapl3g_output_info, only: get_vertical_dim_spec_name
    use ESMF
    use MAPL_ExceptionHandling
    use, intrinsic :: iso_c_binding, only: c_ptr, c_f_pointer, c_loc
@@ -36,10 +35,10 @@ module MAPL_FieldPointerUtilities
       module procedure assign_fptr_r8_rank2
    end interface assign_fptr
 
-   interface assign_fptr_rank3
-      module procedure :: assign_fptr_r4_rank3
-      module procedure :: assign_fptr_r8_rank3
-   end interface assign_fptr_rank3
+!   interface assign_fptr_rank3
+!      module procedure :: assign_fptr_r4_rank3
+!      module procedure :: assign_fptr_r8_rank3
+!   end interface assign_fptr_rank3
 
    interface FieldGetCptr
       procedure get_cptr
@@ -158,39 +157,39 @@ contains
       _RETURN(_SUCCESS)
    end subroutine assign_fptr_r8_rank2
 
-   subroutine assign_fptr_r4_rank3(x, fptr, rc)
-      type(ESMF_Field), intent(inout) :: x
-      real(kind=ESMF_KIND_R4), pointer, intent(out) :: fptr(:)
-      integer, optional, intent(out) :: rc
-
-      ! local declarations
-      type(c_ptr) :: cptr
-      integer(ESMF_KIND_I8), allocatable :: fp_shape(:)
-      integer :: status
-
-      fp_shape = get_fptr_shape(x, _RC)
-      call FieldGetCptr(x, cptr, _RC)
-      call c_f_pointer(cptr, fptr, fp_shape)
-
-      _RETURN(_SUCCESS)
-   end subroutine assign_fptr_r4_rank3
-
-   subroutine assign_fptr_r8_rank3(x, fptr, rc)
-      type(ESMF_Field), intent(inout) :: x
-      real(kind=ESMF_KIND_R8), pointer, intent(out) :: fptr(:)
-      integer, optional, intent(out) :: rc
-
-      ! local declarations
-      type(c_ptr) :: cptr
-      integer(ESMF_KIND_I8), allocatable :: fp_shape(:)
-      integer :: status
-
-      fp_shape = get_fptr_shape(x, _RC)
-      call FieldGetCptr(x, cptr, _RC)
-      call c_f_pointer(cptr, fptr, fp_shape)
-
-      _RETURN(_SUCCESS)
-   end subroutine assign_fptr_r8_rank3
+!   subroutine assign_fptr_r4_rank3(x, fptr, rc)
+!      type(ESMF_Field), intent(inout) :: x
+!      real(kind=ESMF_KIND_R4), pointer, intent(out) :: fptr(:)
+!      integer, optional, intent(out) :: rc
+!
+!      ! local declarations
+!      type(c_ptr) :: cptr
+!      integer(ESMF_KIND_I8), allocatable :: fp_shape(:)
+!      integer :: status
+!
+!      fp_shape = get_fptr_shape(x, _RC)
+!      call FieldGetCptr(x, cptr, _RC)
+!      call c_f_pointer(cptr, fptr, fp_shape)
+!
+!      _RETURN(_SUCCESS)
+!   end subroutine assign_fptr_r4_rank3
+!
+!   subroutine assign_fptr_r8_rank3(x, fptr, rc)
+!      type(ESMF_Field), intent(inout) :: x
+!      real(kind=ESMF_KIND_R8), pointer, intent(out) :: fptr(:)
+!      integer, optional, intent(out) :: rc
+!
+!      ! local declarations
+!      type(c_ptr) :: cptr
+!      integer(ESMF_KIND_I8), allocatable :: fp_shape(:)
+!      integer :: status
+!
+!      fp_shape = get_fptr_shape(x, _RC)
+!      call FieldGetCptr(x, cptr, _RC)
+!      call c_f_pointer(cptr, fptr, fp_shape)
+!
+!      _RETURN(_SUCCESS)
+!   end subroutine assign_fptr_r8_rank3
 
    subroutine get_cptr(x, cptr, rc)
       type(ESMF_Field), intent(inout) :: x
@@ -993,32 +992,32 @@ contains
 
    end subroutine Destroy
 
-   function get_fptr_shape(f, rc) result(fptr_shape)
-      integer :: fptr_shape(3)
-      type(ESMF_Field), intent(inout) :: f
-      integer, optional, intent(out) :: rc
-      integer :: status
-      integer :: rank
-      integer, allocatable :: gridToFieldMap(:)
-      integer, allocatable :: localElementCount(:)
-      logical :: has_vertical
-      character(len=:), allocatable :: spec_name
-      character(len=*), parameter :: VERTICAL_DIM_NONE_NAME = 'VERTICAL_DIM_NONE'
-      !wdb fixme deleteme This seems fragile. We should probably make a utility function
-      !that selects the type(VerticalDimSpec) parameter based on a string. Perhaps a
-      !logical function in VerticalDimSpec.F90 that recogizes a VerticalDimSpec based on
-      !the string from the ESMF_Info.
-
-      call ESMF_FieldGet(f, gridToFieldMap=gridToFieldMap, _RC) 
-      call ESMF_FieldGet(f, rank=rank, _RC)
-      allocate(localElementCount(rank))
-      !     Due to an ESMF bug, getting the localElementCount must use the module function.
-      !     See FieldGetLocalElementCount (specific function) comments.
-      localElementCount = FieldGetLocalElementCount(f, _RC)
-      spec_name = get_vertical_dim_spec_name(f, _RC)
-      has_vertical = spec_name /= VERTICAL_DIM_NONE_NAME
-      fptr_shape = get_fptr_shape_private(gridToFieldMap, localElementCount, has_vertical, _RC)
-
-   end function get_fptr_shape
+!   function get_fptr_shape(f, rc) result(fptr_shape)
+!      integer :: fptr_shape(3)
+!      type(ESMF_Field), intent(inout) :: f
+!      integer, optional, intent(out) :: rc
+!      integer :: status
+!      integer :: rank
+!      integer, allocatable :: gridToFieldMap(:)
+!      integer, allocatable :: localElementCount(:)
+!      logical :: has_vertical
+!      character(len=:), allocatable :: spec_name
+!      character(len=*), parameter :: VERTICAL_DIM_NONE_NAME = 'VERTICAL_DIM_NONE'
+!      !wdb fixme deleteme This seems fragile. We should probably make a utility function
+!      !that selects the type(VerticalDimSpec) parameter based on a string. Perhaps a
+!      !logical function in VerticalDimSpec.F90 that recogizes a VerticalDimSpec based on
+!      !the string from the ESMF_Info.
+!
+!      call ESMF_FieldGet(f, gridToFieldMap=gridToFieldMap, _RC) 
+!      call ESMF_FieldGet(f, rank=rank, _RC)
+!      allocate(localElementCount(rank))
+!      !  Due to an ESMF bug, getting the localElementCount must use the module function.
+!      !  See FieldGetLocalElementCount (specific function) comments.
+!      localElementCount = FieldGetLocalElementCount(f, _RC)
+!      spec_name = get_vertical_dim_spec_name(f, _RC)
+!      has_vertical = spec_name /= VERTICAL_DIM_NONE_NAME
+!      fptr_shape = get_fptr_shape_private(gridToFieldMap, localElementCount, has_vertical, _RC)
+!
+!   end function get_fptr_shape
 
 end module MAPL_FieldPointerUtilities
