f9194b5a1a5cb04b5a808c9ac06e3c3a81120ac0
mathomp4 <fortran@gmail.com>
2024-10-04 14:31:36 +0000

    Deploying to gh-pages from @ GEOS-ESM/MAPL@230d319fddee47dc5e397a1f9f4bbcf1fda923ae 🚀

---
+++ dev-doc/src/GathervSpec.F90
@@ -0,0 +1,108 @@
+#include "MAPL_ErrLog.h"
+module mapl_GathervSpec
+   use mapl_GathervKernel
+   use mapl_ErrorHandlingMod
+   use fArgParse
+   use mpi
+   use, intrinsic :: iso_fortran_env, only: INT64
+   implicit none
+   private
+
+   public :: GathervSpec
+   public :: make_GathervSpec
+   public :: make_GathervKernel
+
+   type :: GathervSpec
+      integer :: nx
+      integer :: n_levs
+      integer :: n_writers
+      integer :: n_tries
+   end type GathervSpec
+
+contains
+
+   function make_GathervSpec(rc) result(spec)
+      type(GathervSpec) :: spec
+      integer, optional, intent(out) :: rc
+
+      integer :: status
+      type(ArgParser) :: parser
+      type(StringUnlimitedMap) :: options
+      class(*), pointer :: option
+
+      parser = ArgParser()
+      call add_cli_options(parser)
+      options = parser%parse_args()
+      
+      option => options%at('nx')
+      _ASSERT(associated(option), 'nx not found')
+      call cast(option, spec%nx, _RC)
+
+      option => options%at('n_levs')
+      _ASSERT(associated(option), 'n_levs not found')
+      call cast(option, spec%n_levs, _RC)
+
+
+      option => options%at('n_writers')
+      _ASSERT(associated(option), 'n_writers not found')
+      call cast(option, spec%n_writers, _RC)
+
+
+      option => options%at('n_tries')
+      _ASSERT(associated(option), 'n_tries not found')
+      call cast(option, spec%n_tries, _RC)
+      
+      _RETURN(_SUCCESS)
+   end function make_GathervSpec
+
+
+   subroutine add_cli_options(parser)
+      type(ArgParser), intent(inout) :: parser
+
+      call parser%add_argument('--nx', &
+           help='dimension of cube face', &
+           type='integer',  &
+           action='store')
+
+      call parser%add_argument('--n_levs', &
+           help='number of vertical levels', &
+           type='integer',  &
+           action='store')
+
+      call parser%add_argument('--n_writers', &
+           help='number of simultaneous, independent writes to disk', &
+           type='integer',  &
+           action='store')
+
+      call parser%add_argument('--n_tries', &
+           help='number of times to call kernel to get statistics', &
+           type='integer',  &
+           default=1, &
+           action='store')
+
+   end subroutine add_cli_options
+
+
+   ! (1) Allocate and initialize buffer
+   ! (2) Open per-process file, and save i/o unit
+   function make_GathervKernel(spec, comm, rc) result(kernel)
+      type(GathervKernel) :: kernel
+      type(GathervSpec), intent(in) :: spec
+      integer, intent(in) :: comm
+      integer, optional, intent(out) :: rc
+
+      integer :: status
+      integer :: npes
+      integer :: n
+
+      call MPI_Comm_size(MPI_COMM_WORLD, npes, status)
+      _VERIFY(status)
+      n = int(spec%nx,kind=INT64)**2 * 6 * spec%n_levs / npes
+
+      kernel = GathervKernel(n, comm)
+      call kernel%init(_RC)
+
+      _RETURN(_SUCCESS)
+   end function make_GathervKernel
+
+end module mapl_GathervSpec
---
+++ dev-doc/src/MAPL_NetCDF.F90
@@ -0,0 +1,126 @@
+#include "MAPL_Exceptions.h"
+#include "MAPL_ErrLog.h"
+! Procedures to convert from NetCDF datetime to ESMF_Time and ESMF_TimeInterval
+! NetCDF datetime is: {integer, character(len=*)}
+! {1800, 'seconds since 2010-01-23 18:30:37'}
+! {TIME_SPAN, 'TIME_UNIT since YYYY-MM-DD hh:mm:ss'}
+module MAPL_NetCDF
+
+   use MAPL_ExceptionHandling
+   use MAPL_KeywordEnforcerMod
+   use MAPL_DateTime_Parsing, only: datetime_duration
+   use MAPL_DateTime_Parsing_ESMF
+   use MAPL_CF_Time
+
+   implicit none
+
+   public :: convert_NetCDF_DateTime_to_ESMF
+   public :: get_ESMF_Time_from_NetCDF_DateTime
+
+   private
+
+   interface convert_NetCDF_DateTime_to_ESMF
+      module procedure :: get_ESMF_Time_from_NetCDF_DateTime_integer
+      module procedure :: get_ESMF_Time_from_NetCDF_DateTime_real
+   end interface convert_NetCDF_DateTime_to_ESMF
+
+   interface get_ESMF_Time_from_NetCDF_DateTime
+      module procedure :: get_ESMF_Time_from_NetCDF_DateTime_integer
+      module procedure :: get_ESMF_Time_from_NetCDF_DateTime_real
+   end interface get_ESMF_Time_from_NetCDF_DateTime
+
+!   integer, parameter :: MAX_CHARACTER_LENGTH = 64
+
+contains
+
+!===============================================================================
+!========================= HIGH-LEVEL PROCEDURES ===========================
+
+   ! Convert NetCDF_DateTime {int_time, units_string} to
+   ! ESMF time variables {interval, basetime, time} and time unit {time_unit}
+   ! basetime is the start time, and time is basetime + interval
+   subroutine get_ESMF_Time_from_NetCDF_DateTime_integer(duration, units_string, &
+      interval, basetime, unusable, time, time_unit, rc)
+      integer, intent(in) :: duration
+      character(len=*), intent(in) :: units_string
+      type(ESMF_TimeInterval), intent(inout) :: interval
+      type(ESMF_Time), intent(inout) :: basetime
+      class (KeywordEnforcer), optional, intent(in) :: unusable
+      type(ESMF_Time), optional, intent(inout) :: time
+      character(len=:), allocatable, optional, intent(out) :: time_unit
+      integer, optional, intent(out) :: rc
+
+      type(CF_Time_Integer) :: cft
+      type(datetime_duration) :: dt_duration
+      character(len=MAX_CHARACTER_LENGTH) :: isostring
+      character(len=MAX_CHARACTER_LENGTH) :: tunit_
+      integer :: status
+      
+      _UNUSED_DUMMY(unusable)
+
+      _ASSERT((len_trim(adjustl(units_string)) > 0), 'units empty')
+
+      cft = CF_Time_Integer(duration, units_string)
+      call convert_CF_Time_to_datetime_duration(cft, dt_duration, _RC)
+      call set_ESMF_TimeInterval(interval, dt_duration, _RC)
+
+      call extract_ISO8601_from_CF_Time(cft, isostring, _RC)
+      call set_ESMF_Time_from_ISO8601(basetime, isostring, _RC)
+
+      if(present(time)) time = basetime + interval
+
+      if(present(time_unit)) then
+         call extract_CF_Time_unit(cft, tunit_, _RC)
+         time_unit = trim(tunit_)
+      end if
+
+      _RETURN(_SUCCESS)
+
+   end subroutine get_ESMF_Time_from_NetCDF_DateTime_integer
+
+   ! Convert NetCDF_DateTime {real_time, units_string} to
+   ! ESMF time variables {interval, basetime, time} and time unit {time_unit}
+   ! basetime is the start time, and time is basetime + interval
+   subroutine get_ESMF_Time_from_NetCDF_DateTime_real(duration, units_string, &
+      interval, basetime, unusable, time, time_unit, rc)
+      real(kind=ESMF_KIND_R8), intent(in) :: duration
+      character(len=*), intent(in) :: units_string
+      type(ESMF_TimeInterval), intent(inout) :: interval
+      type(ESMF_Time), intent(inout) :: basetime
+      class (KeywordEnforcer), optional, intent(in) :: unusable
+      type(ESMF_Time), optional, intent(inout) :: time
+      character(len=:), allocatable, optional, intent(out) :: time_unit
+      integer, optional, intent(out) :: rc
+
+      type(CF_Time_Real) :: cft
+      type(datetime_duration) :: dt_duration
+      character(len=MAX_CHARACTER_LENGTH) :: isostring
+      character(len=MAX_CHARACTER_LENGTH) :: tunit_
+      integer :: status
+      
+      _UNUSED_DUMMY(unusable)
+
+      _ASSERT((len_trim(adjustl(units_string)) > 0), 'units empty')
+
+      cft = CF_Time_Real(duration, units_string)
+      call convert_CF_Time_to_datetime_duration(cft, dt_duration, _RC)
+      call set_ESMF_TimeInterval(interval, dt_duration, _RC)
+
+      call extract_ISO8601_from_CF_Time(cft, isostring, _RC)
+      call set_ESMF_Time_from_ISO8601(basetime, isostring, _RC)
+
+      if(present(time)) time = basetime + interval
+
+      if(present(time_unit)) then
+         call extract_CF_Time_unit(cft, tunit_, _RC)
+         time_unit = trim(tunit_)
+      end if
+
+      _RETURN(_SUCCESS)
+
+   end subroutine get_ESMF_Time_from_NetCDF_DateTime_real
+
+!======================= END HIGH-LEVEL PROCEDURES =========================
+!===============================================================================
+
+end module MAPL_NetCDF
