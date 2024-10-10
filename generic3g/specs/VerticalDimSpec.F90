f0dbcbce7e909f8df79decf2994dfd3664c9c680
Purnendu Chakraborty <purnendu.chakraborty@nasa.gov>
2024-10-10 09:51:44 -0400

    Added write(formatted) to VerticalDimSpec

--- generic3g/specs/VerticalDimSpec.F90
+++ generic3g/specs/VerticalDimSpec.F90
@@ -1,11 +1,14 @@
 #include "MAPL_Generic.h"
 
 module mapl3g_VerticalDimSpec
+
    !use mapl3g_UngriddedDimSpec
    use esmf, only: ESMF_Info
    use esmf, only: ESMF_InfoCreate
    use esmf, only: ESMF_InfoSet
+   use esmf, only: ESMF_MAXSTR
    use mapl_ErrorHandling
+
    implicit none
    private
   
@@ -26,6 +29,8 @@ module mapl3g_VerticalDimSpec
    contains
       procedure :: get_id
       procedure :: make_info
+      procedure :: write_formatted
+      generic :: write(formatted) => write_formatted
    end type VerticalDimSpec
 
    type(VerticalDimSpec), parameter :: VERTICAL_DIM_UNKNOWN = VerticalDimSpec(-1)
@@ -54,6 +59,38 @@ contains
       type(VerticalDimSpec), intent(in) :: a, b
       equal_to = a%id == b%id
    end function equal_to
+
+   subroutine write_formatted(this, unit, iotype, v_list, iostat, iomsg)
+      class(VerticalDimSpec), intent(in) :: this
+      integer, intent(in) :: unit
+      character(*), intent(in) :: iotype
+      integer, intent(in) :: v_list(:)
+      integer, intent(out) :: iostat
+      character(*), intent(inout) :: iomsg
+
+      integer :: id
+      character(len=ESMF_MAXSTR) :: dim_spec_str
+
+      id = this%id
+      select case(id)
+      case(-1)
+         dim_spec_str = "VERTICAL_DIM_UNKNOWN"
+      case(1)
+         dim_spec_str = "VERTICAL_DIM_NONE"
+      case(2)
+         dim_spec_str = "VERTICAL_DIM_CENTER"
+      case(3)
+         dim_spec_str = "VERTICAL_DIM_EDGE"
+      case(4)
+         dim_spec_str = "VERTICAL_DIM_MIRROR"
+      ! case default
+      !    _FAIL("Invalid vertical dim spec")
+      end select
+      write(unit, '("VerticalDimSpec{",a,">}")', iostat=iostat, iomsg=iomsg) dim_spec_str
+
+      _UNUSED_DUMMY(iotype)
+      _UNUSED_DUMMY(v_list)
+   end subroutine write_formatted
 
    elemental logical function not_equal_to(a, b)
       type(VerticalDimSpec), intent(in) :: a, b
