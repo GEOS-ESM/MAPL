53afd91534b7735214f0e0809020ecc35fa382b6
Darian Boggs <william.d.boggs@nasa.gov>
2024-10-22 16:07:19 -0400

    Macros

--- generic3g/actions/AccumulatorFunctions.F90
+++ generic3g/actions/AccumulatorFunctions.F90
@@ -9,6 +9,71 @@ module mapl3g_AccumulatorFunctions.F90
    implicit none
    private
 
+   type :: AccumulatorFunctions
+      procedure(FunctionR4R4), pointer :: accumulateR4point => null()
+      procedure(FunctionR8R8), pointer :: accumulateR8point => null()
+      procedure(FunctionR4), pointer :: clearR4point => null()
+      procedure(FunctionR8), pointer :: clearR8point => null()
+      logical :: keep_left
+      logical :: check_divide
+   contains
+      procedure :: accumulate
+      procedure :: increment
+      procedure :: couple
+      procedure :: clear
+   end type AccumulatorFunctions
+
+   abstract interface
+      function FunctionR4R4(left, right) result(val)
+         real(kind=ESMF_KIND_R4) :: val
+         real(kind=ESMF_KIND_R4), intent(in) :: left, right
+      end function FunctionR4R4
+      function FunctionR4R8(left, right) result(val)
+         real(kind=ESMF_KIND_R4) :: val
+         real(kind=ESMF_KIND_R4), intent(in) :: left
+         real(kind=ESMF_KIND_R8), intent(in) :: right
+      end function FunctionR4R8
+      function FunctionR8R8(left, right) result(val)
+         real(kind=ESMF_KIND_R8) :: val
+         real(kind=ESMF_KIND_R8), intent(in) :: left, right
+      end function FunctionR8R8
+      function FunctionR4(point) result(val)
+         real(kind=ESMF_KIND_R4) :: val
+         real(kind=ESMF_KIND_R4), intent(in) :: point
+      end function FunctionR4
+      function FunctionR8(point) result(val)
+         real(kind=ESMF_KIND_R8) :: val
+         real(kind=ESMF_KIND_R8), intent(in) :: point
+      end function FunctionR8
+   end interface
+
+contains
+
+   function AddAccumulateR4(left, right) result(val)
+      real(kind=ESMF_KIND_R4) :: val
+      real(kind=ESMF_KIND_R4), intent(in) :: left, right
+
+      val = left + right
+
+   end function AddAccumulateR4
+
+   function AddAccumulateR8(left, right) result(val)
+      real(kind=ESMF_KIND_R8) :: val
+      real(kind=ESMF_KIND_R8), intent(in) :: left, right
+
+      val = left + right
+
+   end function AddAccumulateR8
+
+   function AddMinR4((left, right) result(val)
+      real(kind=ESMF_KIND_R4) :: val
+      real(kind=ESMF_KIND_R4), intent(in) :: left, right
+
+      val = min(left + right)
+
+
+end module AccumulatorFunctions
+
    public :: fieldMin
    public :: fieldMax
    public :: fieldAdd
