d58081975df74d6e44296d2f7d2d049fec52d6a1
Darian Boggs <william.d.boggs@nasa.gov>
2024-10-29 10:46:23 -0400

    Implementing fptr procedures

--- generic3g/actions/AccumulatorAction.F90
+++ generic3g/actions/AccumulatorAction.F90
@@ -8,6 +8,7 @@ module mapl3g_AccumulatorAction
    public :: AccumulatorAction
 
    type, extends(ExtensionAction), abstract :: AccumulatorAction
+      private
       type(ESMF_Field), allocatable :: accumulation_field
       type(ESMF_Field), allocatable :: result_field
    contains
@@ -16,46 +17,46 @@ module mapl3g_AccumulatorAction
       procedure :: invalidate_default_action
       procedure :: initialize
       procedure :: update
-      procedure(AccumulateField), deferred :: accumulate
       ! Helpers
+      procedure, private :: accumulate
       procedure, private :: initialized => action_initialized
       procedure, private :: clear_accumulator => clear_accumulator_default
       procedure, private :: clear_accumulator_default
       procedure, private :: get_undef_R4
       procedure, private :: get_clear_value_R4
       procedure, private :: calculate_result
+      ! Deferred
+      procedure(AccumulateR4),  deferred :: accumulate_R4
    end type AccumulatorAction
 
    abstract interface
-      subroutine AccumulateField(this, field_update, rc)
-         class(AccumulatorAction), intent(inout) :: this
-         type(ESMF_Field), intent(inout) :: field_update
+      subroutine AccumulateR4(this, update_field, rc)
+         import :: AccumulatorAction
+         class(AccumulatorAction), intent(in) :: this
+         type(ESMF_Field), intent(inout) :: update_field
          integer, optional, intent(out) :: rc
-      end subroutine AccumulateField
+      end subroutine AccumulateR4
    end interface
 
    type, extends(AccumulatorAction) :: MeanAccumulator
-      integer(ESMF_KIND_R8) :: counter_scalar = 0_ESMF_FIND_I8
+      private
+      integer(ESMF_KIND_R8) :: counter_scalar = 0_ESMF_KIND_I8
+      real(kind=ESMF_KIND_R4) :: clear_value_R4 = 0.0_ESMF_KIND_I4
    contains
       procedure :: invalidate => invalidate_mean_accumulator
-      procedure :: clear_accumulator => clear_accumulator_mean
+      procedure, private :: clear_accumulator => clear_accumulator_mean
       procedure, private :: increment_counter
       procedure, private :: clear_counter
       procedure, private :: calculate_result => calculate_mean
+      procedure, private :: accumulate_R4 => add_accumulate_R4
    end type MeanAccumulator
 
-   interface MeanAccumulator
-      module procedure :: construct_MeanAccumulator
-   end interface MeanAccumulator
-
    type, extends(AccumulatorAction) :: MaxAccumulator
+      private
    contains
       procedure, private :: get_clear_value_R4_undef
+      procedure, private :: accumulate_R4 => max_accumulate_R4
    end type MaxAccumulator
-
-   interface MaxAccumulator
-      module procedure :: construct_MaxAccumulator
-   end interface MaxAccumulator
 
 contains
 
@@ -78,7 +79,7 @@ contains
       real(kind=ESMF_KIND_R4) :: clear_value
       class(AccumulatorAction), intent(in) :: this
 
-      clear_value = 0_ESMF_KIND_R4
+      clear_value = this%clear_value_R4
 
    end function get_clear_value_R4
 
@@ -139,11 +140,9 @@ contains
       integer, optional, intent(out) :: rc
 
       integer :: status
-      type(ESMF_Field) :: import_field, field
-      type(ESMF_TypeKind_Flag) :: tk
+      type(ESMF_Field) :: import_field
 
       call get_field(importState, import_field, _RC)
-      call ESMF_FieldGet(import_field, typekind=tk, _RC)
       call FieldCopy(import_field, this%accumulation_field, _RC)
       call FieldSet(this%accumulation_field, this%get_clear_value(), _RC)
       call this%clear_accumulator(_RC)
@@ -287,40 +286,66 @@ contains
 
    end subroutine calculate_mean_R4
 
-   function construct_MeanAccumulator() result(acc)
-      type(MeanAccumulator) :: acc
-
-
-   end function construct_MeanAccumulator
-
-    subroutine add_accumulate_R4(this, current, latest)
+   subroutine accumulate(this, update_field, rc)
       class(AccumulatorAction), intent(inout) :: this
+      type(ESMF_Field), intent(inout) :: update_field
+      integer, optional, intent(out) :: rc
+      
+      integer :: status
+      type(ESMF_TypeKind_Flag) :: tk, tk_field
+
+      call ESMF_FieldGet(this%accumulation_field, typekind=tk, _RC)
+      call ESMF_FieldGet(update_field, typekind=tk_field, _RC)
+      _ASSERT(tk == tk_field, 'Update field must be the same typekind as the accumulation field.')
+      if(tk == ESMF_TYPEKIND_R4) then
+         call this%accumulate_R4(this, update_field, _RC)
+      else
+         _FAIL('Unsupported typekind value')
+      end if
+
+      _RETURN(_SUCCESS)
+
+   end subroutine accumulate
+
+   subroutine add_accumulate_R4(this, update_field, rc)
+      class(AccumulatorAction), intent(inout) :: this
+      type(ESMF_Field), intent(inout) :: update_field
+      integer, optional, intent(out) :: rc
+
+      integer :: status
+      real(kind=ESMF_KIND_R4), pointer, intent(inout) :: current(:)
+      real(kind=ESMF_KIND_R4), pointer, intent(in) :: latest(:)
+      real(kind=ESMF_KIND_R4) :: undef
+
+      undef = this%get_undef_R4()
+      call assign_fptr(this%accumulation_field, current, _RC)
+      call assign_fptr(update_field, latest, _RC)
+      where(current /= undef .and. latest /= undef)
+        current = current + latest
+      elsewhere(latest == undef)
+        current = undef
+      end where
+      _RETURN(_SUCCESS)
+
+   end subroutine add_accumulate_R4
+
+    subroutine max_accumulate_R4(this, update_field, rc)
+      class(AccumulatorAction), intent(inout) :: this
+      type(ESMF_Field), intent(inout) :: update_field
+      integer, optional, intent(out) :: rc
+
+      integer :: status
       real(kind=ESMF_KIND_R4), pointer, intent(inout) :: current(:)
       real(kind=ESMF_KIND_R4), pointer, intent(in) :: latest(:)
       real(kind=ESMF_KIND_R4) :: undef
       
-      call this%set_undef(undef)
-      where(current == undef .or. latest == undef)
-         current = undef
-      elsewhere
-         current = current + latest
+      undef = this%get_undef_R4()
+      call assign_fptr(this%accumulation_field, current, _RC)
+      call assign_fptr(update_field, latest, _RC)
+      where(current /= undef .and. latest /= undef)
+         current = max(current, latest)
       end where
-
-   end subroutine add_accumulate_R4
-
-   function construct_MaxAccumulator() result(acc)
-      type(MaxAccumulator) :: acc
-
-      acc%skip_on_undef = .FALSE.
-      acc%update_elements = .FALSE.
-
-   end function construct_MaxAccumulator
-
-    function max_accumulate_R4(current_value, latest) :: result(updated_value)
-      real(kind=ESMF_KIND_R4) :: updated_value
-      real(kind=ESMF_KIND_R4), intent(in) :: current_value, latest
-      
-      updated_value = max(current_value, latest)
+      _RETURN(_SUCCESS)
 
    end function max_accumulate_R4
 
