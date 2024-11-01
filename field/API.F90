ac1efc52d01abecb07dcc908af787e124d74f17b
Tom Clune <thomas.l.clune@nasa.gov>
2024-11-01 14:36:48 -0400

    Fixes #3140 - create field factory
    This PR is the first of what will be a sequence of changes to wrap
    ESMF_Field functionality.  The change introduces a new subdirectory
    `field` and the following extensions:
    
    - MAPL_FieldCreate
    - MAPL_FieldReset
    - MAPL_FieldInfo  (only internal info items for now)
    
    The code compiles and passes rudimentary unit tests, but is not used
    elsewher in MAPL.
    
    Subsequent PRs will refactor other layers to use this as well as
    relocate some all-but-redundant bits.

---
+++ field/API.F90
@@ -0,0 +1,12 @@
+module mapl3g_Field_API
+   use mapl3g_FieldCreate
+   use mapl3g_FieldInfo
+   use mapl3g_VerticalStaggerLoc
+
+   ! Internal info should not be exposed to users
+!#   use mapl3g_FieldInfo, only: MAPL_FieldInfoGetPrivate
+!#   use mapl3g_FieldInfo, only: MAPL_FieldInfoSetPrivate
+!#   use mapl3g_FieldInfo, only: MAPL_FieldInfoSetShared
+!#   use mapl3g_FieldInfo, only: MAPL_FieldInfoGetShared
+
+end module mapl3g_Field_API
