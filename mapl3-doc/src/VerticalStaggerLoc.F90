5734a638ff764880fc07a36bfc7848ba35e08583
mathomp4 <fortran@gmail.com>
2024-11-06 14:47:52 +0000

    Deploying to gh-pages from @ GEOS-ESM/MAPL@2232d0bab4258d04f0039c81e2b6af4cbcc26eb2 🚀

--- mapl3-doc/src/VerticalStaggerLoc.F90
+++ mapl3-doc/src/VerticalStaggerLoc.F90
@@ -1,83 +1,43 @@
-module mapl3g_VerticalStaggerLoc
+module oomph_VerticalStaggerLoc
    implicit none
    private
 
    public :: VerticalStaggerLoc
-   public :: VERTICAL_STAGGER_NONE
-   public :: VERTICAL_STAGGER_EDGE
-   public :: VERTICAL_STAGGER_CENTER
-   public :: VERTICAL_STAGGER_INVALID
+   public :: V_STAGGER_LOC_NONE
+   public :: V_STAGGER_LOC_EDGE
+   public :: V_STAGGER_LOC_CENTER
+   
+   integer, parameter :: INVALID = -1
 
-   public :: operator(==)
-   public :: operator(/=)
-
-   ! The type below has an "extraneous" component ID.  The purpose of
-   ! this is to allow the default structure constructor to be usable
-   ! in constant expressions (parameter statements), while still allowing
-   ! private components which require a non-default constructor for external
-   ! modules. Subtle.
    type :: VerticalStaggerLoc
       private
-      integer :: id = -1
-      character(24) :: name = "VERTICAL_STAGGER_INVALID"
+      integer :: i = INVALID
    contains
-      procedure :: to_string
+      procedure :: equal_to
+      procedure :: not_equal_to
+      generic :: operator(==) => equal_to
+      generic :: operator(/=) => not_equal_to
    end type VerticalStaggerLoc
 
-   interface VerticalStaggerLoc
-      procedure :: new_VerticalStaggerLoc
-   end interface VerticalStaggerLoc
+   type(VerticalStaggerLoc) :: V_STAGGER_LOC_NONE = VerticalStaggerLoc(0)
+   type(VerticalStaggerLoc) :: V_STAGGER_LOC_EDGE = VerticalStaggerLoc(1)
+   type(VerticalStaggerLoc) :: V_STAGGER_LOC_CENTER = VerticalStaggerLoc(2)
 
-   interface operator(==)
-      procedure are_equal
-   end interface operator(==)
-
-   interface operator(/=)
-      procedure are_not_equal
-   end interface operator(/=)
-
-   type(VerticalStaggerLoc), parameter :: VERTICAL_STAGGER_NONE = VerticalStaggerLoc(0, "VERTICAL_STAGGER_NONE")
-   type(VerticalStaggerLoc), parameter :: VERTICAL_STAGGER_EDGE = VerticalStaggerLoc(1, "VERTICAL_STAGGER_EDGE")
-   type(VerticalStaggerLoc), parameter :: VERTICAL_STAGGER_CENTER = VerticalStaggerLoc(2, "VERTICAL_STAGGER_CENTER")
-   type(VerticalStaggerLoc), parameter :: VERTICAL_STAGGER_INVALID = VerticalStaggerLoc(3, "VERTICAL_STAGGER_INVALID")
-
+   
 contains
 
-   ! Restrict values to just the 4 defined options.
-   function new_VerticalStaggerLoc(name) result(staggerloc)
-      type(VerticalStaggerLoc) :: staggerloc
-      character(*), intent(in) :: name
 
-      select case (name)
-      case (VERTICAL_STAGGER_NONE%name)
-         staggerloc = VERTICAL_STAGGER_NONE
-      case (VERTICAL_STAGGER_EDGE%name)
-         staggerloc = VERTICAL_STAGGER_EDGE
-      case (VERTICAL_STAGGER_CENTER%name)
-         staggerloc = VERTICAL_STAGGER_CENTER
-      case default
-         staggerloc = VERTICAL_STAGGER_INVALID
-      end select
-   end function new_VerticalStaggerLoc
-
-   function to_string(this) result(s)
-      character(:), allocatable :: s
+   pure logical function equal_to(this, other)
       class(VerticalStaggerLoc), intent(in) :: this
-
-      s = trim(this%name)
-
-   end function to_string
-
-   elemental logical function are_equal(this, that)
-      type(VerticalStaggerLoc), intent(in) :: this
-      type(VerticalStaggerLoc), intent(in) :: that
-      are_equal = this%name == that%name
-   end function are_equal
-
-   elemental logical function are_not_equal(this, that)
-      type(VerticalStaggerLoc), intent(in) :: this
-      type(VerticalStaggerLoc), intent(in) :: that
-      are_not_equal = .not. (this == that)
-   end function are_not_equal
-
-end module mapl3g_VerticalStaggerLoc
+      type(VerticalStaggerLoc), intent(in) :: other
+      equal_to = this%i == other%i
+   end function equal_to
+   
+   pure logical function not_equal_to(this, other)
+      class(VerticalStaggerLoc), intent(in) :: this
+      type(VerticalStaggerLoc), intent(in) :: other
+      not_equal_to = .not. (this == other)
+   end function not_equal_to
+   
+   
+end module oomph_VerticalStaggerLoc
