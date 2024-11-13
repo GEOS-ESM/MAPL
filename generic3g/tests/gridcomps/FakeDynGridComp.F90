f8e9791ff2416867c6eec6dc2b48db7a30442306
Purnendu Chakraborty <purnendu.chakraborty@nasa.gov>
2024-11-13 14:05:58 -0500

    FakeDynGridComp - looks more like a user gridcomp

--- generic3g/tests/gridcomps/FakeDynGridComp.F90
+++ generic3g/tests/gridcomps/FakeDynGridComp.F90
@@ -4,10 +4,8 @@ module mapl3g_FakeDynGridComp
 
    use mapl_ErrorHandling
    use generic3g
-   use mapl3g_FieldCondensedArray, only: assign_fptr_condensed_array
-   use mapl3g_Field_API, only: MAPL_FieldEmptyComplete, VerticalStaggerLoc, VERTICAL_STAGGER_CENTER
-   use mapl3g_UngriddedDims
    use esmf
+   use mapl, only: MAPL_GetPointer
 
    implicit none
    private
@@ -23,7 +21,7 @@ contains
       integer :: status
 
       ! Set entry points
-      call MAPL_GridCompSetEntryPoint(gridcomp, ESMF_METHOD_INITIALIZE, init, phase_name="GENERIC::INIT_REALIZE", _RC)
+      call MAPL_GridCompSetEntryPoint(gridcomp, ESMF_METHOD_INITIALIZE, init, phase_name="GENERIC::INIT_USER", _RC)
       call MAPL_GridCompSetEntryPoint(gridcomp, ESMF_METHOD_RUN, run, phase_name="run", _RC)
 
       _RETURN(_SUCCESS)
@@ -36,23 +34,17 @@ contains
       type(ESMF_Clock) :: clock
       integer, intent(out)  :: rc
 
-      type(ESMF_Geom) :: geom
-      type(ESMF_Field) :: field1, field2
-      class(VerticalGrid), allocatable :: vertical_grid
-      integer :: num_levels, status
+      integer :: status
+      real(kind=ESMF_KIND_R4), pointer :: pl(:, :, :), t_dyn(:, :, :)
 
-      call MAPL_GridCompGet(gridcomp, geom=geom, vertical_grid=vertical_grid, _RC)
-      num_levels = vertical_grid%get_num_levels()
+      call MAPL_GetPointer(exportState, pl,  "PL", _RC)
+      call set_pressure_(pl)
 
-      call ESMF_StateGet(exportState, "PL", field1, _RC)
-      call field_complete_(field1, geom, num_levels, VERTICAL_STAGGER_CENTER, "hPa", "air_pressure", rc)
-      call set_pressure_(field1, _RC)
-
-      call ESMF_StateGet(exportState, "T_DYN", field2, _RC)
-      call field_complete_(field2, geom, num_levels, VERTICAL_STAGGER_CENTER, "K", "temeperature", rc)
-      call set_temperature_(field2, _RC)
+      call MAPL_GetPointer(exportState, t_dyn, "T_DYN", _RC)
+      call set_temperature_(t_dyn)
 
       _RETURN(_SUCCESS)
+      _UNUSED_DUMMY(gridcomp)
       _UNUSED_DUMMY(importState)
       _UNUSED_DUMMY(clock)
    end subroutine init
@@ -74,82 +66,47 @@ contains
       _UNUSED_DUMMY(clock)
    end subroutine run
 
-   subroutine field_complete_(field, geom, num_levels, vertical_stagger, units, standard_name, rc)
-      type(ESMF_Field), intent(inout) :: field
-      type(ESMF_Geom), intent(in) :: geom
-      integer, intent(in) :: num_levels
-      type(VerticalStaggerLoc), intent(in) :: vertical_stagger
-      character(*), intent(in) :: units
-      character(*), intent(in) :: standard_name
-      integer, optional, intent(out) :: rc
+   subroutine set_pressure_(pressure)
+      real(kind=ESMF_KIND_R4), pointer, intent(inout) :: pressure(:, :, :)
 
-      type(ESMF_FieldStatus_Flag) :: field_status
-      integer :: status
+      integer :: shape_(3), i, j, k, num_levels
 
-      call ESMF_FieldGet(field, status=field_status, _RC)
-      _ASSERT(field_status == ESMF_FIELDSTATUS_EMPTY, "field is not empty")
-      call ESMF_FieldEmptySet(field, geom, _RC)
-      call MAPL_FieldEmptyComplete( &
-           field, &
-           typekind=ESMF_TYPEKIND_R4, &
-           ungridded_dims=UngriddedDims(), &
-           num_levels=num_levels, &
-           vert_staggerLoc=vertical_stagger, &
-           units=units, &
-           standard_name=standard_name, &
-           _RC)
-
-      _RETURN(_SUCCESS)
-   end subroutine field_complete_
-
-   subroutine set_pressure_(field, rc)
-      type(ESMF_Field), intent(inout) :: field
-      integer, optional, intent(out) :: rc
-
-      real(kind=ESMF_KIND_R4), pointer :: farr(:, :, :)
-      integer :: shape_(3), num_levels, horz, vert, ungrd, status
-
-      call assign_fptr_condensed_array(field, farr, _RC)
-      shape_ = shape(farr); num_levels = shape_(2)
-      do concurrent(horz = 1:shape_(1), ungrd = 1:shape_(3))
-         do vert = 1, num_levels
-            farr(horz, vert, ungrd) = real((num_levels - vert + 1) * 10)
+      shape_ = shape(pressure)
+      num_levels = shape_(3)
+      do concurrent(i = 1:shape_(1), j = 1:shape_(2))
+         do k = 1, num_levels
+            pressure(i, j, k) = real((num_levels - k + 1) * 10)
          end do
       end do
-
-      _RETURN(_SUCCESS)
    end subroutine set_pressure_
 
-   subroutine set_temperature_(field, rc)
-      type(ESMF_Field), intent(inout) :: field
-      integer, optional, intent(out) :: rc
+   subroutine set_temperature_(temperature)
+      real(kind=ESMF_KIND_R4), pointer, intent(inout) :: temperature(:, :, :)
 
-      real(kind=ESMF_KIND_R4), pointer :: farr(:, :, :)
-      integer :: shape_(3), num_levels, horz, vert, ungrd, status
+      integer :: shape_(3), i, j, k, num_levels
 
-      call assign_fptr_condensed_array(field, farr, _RC)
-      shape_ = shape(farr); num_levels = shape_(2)
-      do concurrent(horz = 1:shape_(1), ungrd = 1:shape_(3))
-         do vert = 1, num_levels
-            farr(horz, vert, ungrd) = real(5 * (2 ** (num_levels - vert)))
+      shape_ = shape(temperature)
+      num_levels = shape_(3)
+      do concurrent(i = 1:shape_(1), j=1:shape_(2))
+         do k = 1, num_levels
+            temperature(i, j, k) = real(5 * (2 ** (num_levels - k)))
          end do
       end do
-
-      _RETURN(_SUCCESS)
    end subroutine set_temperature_
 
 end module mapl3g_FakeDynGridComp
 
-subroutine SetServices(gridcomp,rc)
+subroutine SetServices(gridcomp, rc)
    use MAPL_ErrorHandlingMod
    use mapl3g_FakeDynGridComp, only: FakeDyn_SetServices => SetServices
    use esmf
 
-   type(ESMF_GridComp)  :: gridcomp
+   type(ESMF_GridComp), intent(inout)  :: gridcomp
    integer, intent(out) :: rc
 
    integer :: status
 
-   call FakeDyn_SetServices(gridcomp,_RC)
+   call FakeDyn_SetServices(gridcomp, _RC)
+
    _RETURN(_SUCCESS)
 end subroutine SetServices
