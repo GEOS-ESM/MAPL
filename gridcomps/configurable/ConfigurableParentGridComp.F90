0d5636ec9fae985dc2b894eb6436e31d8267afcc
Purnendu Chakraborty <purnendu.chakraborty@nasa.gov>
2024-12-05 09:23:03 -0500

    Clean up of configurable gridcomps

--- gridcomps/configurable/ConfigurableParentGridComp.F90
+++ gridcomps/configurable/ConfigurableParentGridComp.F90
@@ -2,8 +2,9 @@
 
 module ConfigurableParentGridComp
 
-   use generic3g
    use mapl_ErrorHandling
+   use mapl3g_Generic, only: MAPL_GridCompSetEntryPoint, MAPL_RunChildren, get_outer_meta_from_inner_gc
+   use mapl3g_OuterMetaComponent, only: OuterMetaComponent
    use pFlogger, only: logger
    use esmf
 
@@ -19,20 +20,15 @@ contains
       type(ESMF_GridComp) :: gridcomp
       integer, intent(out) :: rc
 
-      type(ESMF_HConfig) :: hconfig, collections_config, child_hconfig
-      character(len=:), allocatable :: child_name, collection_name
-      type(ESMF_HConfigIter) :: iter, iter_begin, iter_end
-      logical :: has_active_collections
-      class(logger), pointer :: lgr
-      integer :: num_collections, status
-      type(BasicVerticalGrid) :: vertical_grid
-      type(ESMF_GridComp) outer_gridcomp
       type(OuterMetaComponent), pointer :: outer_meta
+      type(BasicVerticalGrid) :: vertical_grid
+      integer :: status
 
       call MAPL_GridCompSetEntryPoint(gridcomp, ESMF_METHOD_INITIALIZE, init, _RC)
-      call MAPL_GridCompSetEntryPoint(gridcomp, ESMF_METHOD_RUN, run, phase_name='run', _RC)
+      call MAPL_GridCompSetEntryPoint(gridcomp, ESMF_METHOD_RUN, run, phase_name="run", _RC)
 
-      outer_meta => get_outer_meta_from_inner_gc(gridcomp,_RC)
+      ! TODO: DO WE NEED THIS? -pchakrab
+      outer_meta => get_outer_meta_from_inner_gc(gridcomp, _RC)
       vertical_grid = BasicVerticalGrid(4)
       call outer_meta%set_vertical_grid(vertical_grid)
 
@@ -40,11 +36,11 @@ contains
    end subroutine setServices
 
    subroutine init(gridcomp, importState, exportState, clock, rc)
-      type(ESMF_GridComp)   :: gridcomp
-      type(ESMF_State)      :: importState
-      type(ESMF_State)      :: exportState
-      type(ESMF_Clock)      :: clock
-      integer, intent(out)  :: rc
+      type(ESMF_GridComp) :: gridcomp
+      type(ESMF_State) :: importState
+      type(ESMF_State) :: exportState
+      type(ESMF_Clock) :: clock
+      integer, intent(out) :: rc
 
       integer :: status
 
@@ -52,19 +48,18 @@ contains
    end subroutine init
 
    subroutine run(gridcomp, importState, exportState, clock, rc)
-      !use mapl3g_MultiState
-      type(ESMF_GridComp)   :: gridcomp
-      type(ESMF_State)      :: importState
-      type(ESMF_State)      :: exportState
-      type(ESMF_Clock)      :: clock
-      integer, intent(out)  :: rc
+      type(ESMF_GridComp) :: gridcomp
+      type(ESMF_State) :: importState
+      type(ESMF_State) :: exportState
+      type(ESMF_Clock) :: clock
+      integer, intent(out) :: rc
 
       integer :: status
       character(len=ESMF_MAXSTR) :: gc_name
 
       call ESMF_GridCompGet(gridcomp, name=gc_name, _RC)
-      print*,'running ',trim(gc_name)
-      call MAPL_RunChildren(gridcomp, phase_name='run', _RC)
+      print *, "running ", trim(gc_name)
+      call MAPL_RunChildren(gridcomp, phase_name="run", _RC)
 
       _RETURN(_SUCCESS)
    end subroutine run
@@ -83,4 +78,3 @@ subroutine setServices(gridcomp,rc)
    call ConfigurableParent_setServices(gridcomp,_RC)
    _RETURN(_SUCCESS)
 end subroutine setServices
-
