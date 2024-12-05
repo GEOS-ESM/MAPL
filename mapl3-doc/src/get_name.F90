5dfbc17457e72d1f50c2e07e89b53d960dde7e34
mathomp4 <fortran@gmail.com>
2024-12-05 18:45:55 +0000

    Deploying to gh-pages from @ GEOS-ESM/MAPL@64e63aee0bb7c3ad19bff1f6577a9fd18ca31929 🚀

--- mapl3-doc/src/get_name.F90
+++ mapl3-doc/src/get_name.F90
@@ -1,20 +1,21 @@
 #include "MAPL_Generic.h"
 
-submodule (mapl3g_GriddedComponentDriver) get_name_smod
+submodule (mapl3g_OuterMetaComponent) get_name_smod
+   use mapl_ErrorHandling
    implicit none
 
 contains
 
    module function get_name(this, rc) result(name)
       character(:), allocatable :: name
-      class(GriddedComponentDriver), intent(in) :: this
+      class(OuterMetaComponent), intent(in) :: this
       integer, optional, intent(out) :: rc
 
       integer :: status
       character(len=ESMF_MAXSTR) :: buffer
 
-      call ESMF_GridCompGet(this%gridcomp, name=buffer, _RC)
-      name = trim(buffer)
+      call ESMF_GridCompGet(this%self_gridcomp, name=buffer, _RC)
+      name=trim(buffer)
 
       _RETURN(ESMF_SUCCESS)
    end function get_name
