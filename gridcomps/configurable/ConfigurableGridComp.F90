24a534bc388d6904548647cc5faf0c91228e93ca
Purnendu Chakraborty <purnendu.chakraborty@nasa.gov>
2024-12-06 12:22:20 -0500

    Replaced Configurable Leaf and Parent with a single Configurable gridcomp

--- gridcomps/configurable/ConfigurableGridComp.F90
+++ gridcomps/configurable/ConfigurableGridComp.F90
@@ -1,9 +1,9 @@
 #include "MAPL_Generic.h"
 
-module ConfigurableLeafGridComp
+module ConfigurableGridComp
 
    use mapl_ErrorHandling
-   use mapl3g_Generic, only: MAPL_GridCompSetEntryPoint, get_outer_meta_from_inner_gc
+   use mapl3g_Generic, only: MAPL_GridCompSetEntryPoint, MAPL_RunChildren
    use mapl3g_OuterMetaComponent, only: OuterMetaComponent
    use pFlogger, only: logger
    use esmf
@@ -16,21 +16,13 @@ module ConfigurableLeafGridComp
 contains
 
    subroutine setServices(gridcomp, rc)
-      ! use mapl3g_BasicVerticalGrid
       type(ESMF_GridComp) :: gridcomp
       integer, intent(out) :: rc
 
-      ! type(OuterMetaComponent), pointer :: outer_meta
-      ! type(BasicVerticalGrid) :: vertical_grid
       integer :: status
 
       call MAPL_GridCompSetEntryPoint(gridcomp, ESMF_METHOD_INITIALIZE, init, _RC)
       call MAPL_GridCompSetEntryPoint(gridcomp, ESMF_METHOD_RUN, run, phase_name="run", _RC)
-
-      ! ! TODO: DO WE NEED THIS? -pchakrab
-      ! outer_meta => get_outer_meta_from_inner_gc(gridcomp,_RC)
-      ! vertical_grid = BasicVerticalGrid(4)
-      ! call outer_meta%set_vertical_grid(vertical_grid)
 
       _RETURN(_SUCCESS)
    end subroutine setServices
@@ -47,7 +39,7 @@ contains
       _RETURN(_SUCCESS)
    end subroutine init
 
-   subroutine run(gridcomp, importState, exportState, clock, rc)
+   recursive subroutine run(gridcomp, importState, exportState, clock, rc)
       type(ESMF_GridComp) :: gridcomp
       type(ESMF_State) :: importState
       type(ESMF_State) :: exportState
@@ -56,24 +48,24 @@ contains
 
       integer :: status
       character(len=ESMF_MAXSTR) :: gc_name
+
       call ESMF_GridCompGet(gridcomp, name=gc_name, _RC)
-      print *, "running ", trim(gc_name)
+      call MAPL_RunChildren(gridcomp, phase_name="run", _RC)
 
       _RETURN(_SUCCESS)
    end subroutine run
 
-end module ConfigurableLeafGridComp
+end module ConfigurableGridComp
 
-subroutine setServices(gridcomp,rc)
+subroutine setServices(gridcomp, rc)
    use ESMF
    use MAPL_ErrorHandlingMod
-   use ConfigurableLeafGridComp, only: ConfigurableLeaf_setServices => SetServices
+   use ConfigurableGridComp, only: Configurable_setServices => SetServices
    type(ESMF_GridComp)  :: gridcomp
    integer, intent(out) :: rc
 
    integer :: status
 
-   call ConfigurableLeaf_setServices(gridcomp,_RC)
-
+   call Configurable_setServices(gridcomp,_RC)
    _RETURN(_SUCCESS)
 end subroutine setServices
