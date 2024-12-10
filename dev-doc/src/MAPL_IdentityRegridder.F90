fa07274b6271ae246d49ce4e53d34eaf1ef7fe8e
mathomp4 <fortran@gmail.com>
2024-12-10 14:58:48 +0000

    Deploying to gh-pages from @ GEOS-ESM/MAPL@c3d2046ad1f92e2004c748f9edd5f44383e36c87 🚀

---
+++ dev-doc/src/MAPL_IdentityRegridder.F90
@@ -0,0 +1,179 @@
+#include "MAPL_Generic.h"
+
+module MAPL_IdentityRegridderMod
+   use MAPL_AbstractRegridderMod
+   use MAPL_GridSpecMod
+   use MAPL_RegridderSpec
+   use MAPL_KeywordEnforcerMod
+   use mapl_ErrorHandlingMod
+   use mapl_RegridMethods
+   use ESMF
+
+   use, intrinsic :: iso_fortran_env, only: REAL32
+   implicit none
+   private
+
+   !----------------------------
+   ! Note this module implements the singleton pattern.  The type is
+   ! PRIVATE, which prevents other modules from creating objects of
+   ! that type.  The identity_regridder() procedure is PUBLIC and
+   ! returns a pointer to the singleton object.
+   !----------------------------
+   public :: identity_regridder
+
+   integer, parameter :: NUM_DIMS = 2
+
+   type, extends(AbstractRegridder) :: IdentityRegridder
+      private
+      logical :: initialized
+   contains
+      procedure :: initialize_subclass
+      procedure :: regrid_scalar_2d_real32
+      procedure :: regrid_scalar_3d_real32
+      procedure :: regrid_vector_2d_real32
+      procedure :: regrid_vector_3d_real32
+   end type IdentityRegridder
+
+   character(len=*), parameter :: MOD_NAME = 'MAPL_IdentityRegridder::'
+
+   type (IdentityRegridder), save, target :: singleton
+
+contains
+
+
+   function identity_regridder() result(regridder)
+      use ESMF
+      type (IdentityRegridder), pointer :: regridder
+      type (RegridderSpec) :: spec
+
+      regridder => singleton
+
+      ! Due to how MAPL is set up, the default regrid_method is
+      ! bilinear. But if an identity regridder is requested, we
+      ! want to reflect that in the metadata by updating the spec.
+      spec = regridder%get_spec()
+      spec%regrid_method = REGRID_METHOD_IDENTITY
+      call regridder%set_spec(spec)
+    end function identity_regridder
+
+
+   subroutine regrid_scalar_2d_real32(this, q_in, q_out, rc)
+      class (IdentityRegridder), intent(in) :: this
+      real (kind=REAL32), intent(in) :: q_in(:,:)
+      real (kind=REAL32), intent(out) :: q_out(:,:)
+      integer, optional, intent(out) :: rc
+
+      character(len=*), parameter :: Iam = MOD_NAME//'regrid_scalar_2d_real32'
+
+      _UNUSED_DUMMY(this)
+
+      q_out = q_in
+
+      _RETURN(_SUCCESS)
+
+   end subroutine regrid_scalar_2d_real32
+
+
+   subroutine regrid_scalar_3d_real32(this, q_in, q_out, rc)
+      use MAPL_CommsMod
+      use MAPL_BaseMod
+
+      class (IdentityRegridder), intent(in) :: this
+      real (kind=REAL32), intent(in) :: q_in(:,:,:)
+      real (kind=REAL32), intent(out) :: q_out(:,:,:)
+      integer, optional, intent(out) :: rc
+
+      character(len=*), parameter :: Iam = MOD_NAME//'regrid_scalar_2d_real32'
+
+      _UNUSED_DUMMY(this)
+
+      _ASSERT(size(q_in,3) == size(q_out,3), 'inconsistent array shape')
+
+      q_out = q_in
+
+      _RETURN(_SUCCESS)
+
+   end subroutine regrid_scalar_3d_real32
+
+   subroutine regrid_vector_2d_real32(this, u_in, v_in, u_out, v_out, rotate, rc)
+      use MAPL_CommsMod
+      use MAPL_BaseMod
+      use, intrinsic :: iso_fortran_env, only: REAL32
+      class (IdentityRegridder), intent(in) :: this
+      real(kind=REAL32), intent(in) :: u_in(:,:)
+      real(kind=REAL32), intent(in) :: v_in(:,:)
+      real(kind=REAL32), intent(out) :: u_out(:,:)
+      real(kind=REAL32), intent(out) :: v_out(:,:)
+      logical, optional, intent(in) :: rotate
+      integer, optional, intent(out) :: rc
+
+      character(len=*), parameter :: Iam = MOD_NAME//'regrid_vector_3d_real32'
+
+      _UNUSED_DUMMY(this)
+      _UNUSED_DUMMY(rotate)
+
+      u_out = u_in
+      v_out = v_in
+
+     _RETURN(_SUCCESS)
+
+   end subroutine regrid_vector_2d_real32
+
+
+   subroutine regrid_vector_3d_real32(this, u_in, v_in, u_out, v_out, rotate, rc)
+      use MAPL_CommsMod
+      use MAPL_BaseMod
+      use, intrinsic :: iso_fortran_env, only: REAL32
+      class (IdentityRegridder), intent(in) :: this
+      real(kind=REAL32), intent(in) :: u_in(:,:,:)
+      real(kind=REAL32), intent(in) :: v_in(:,:,:)
+      real(kind=REAL32), intent(out) :: u_out(:,:,:)
+      real(kind=REAL32), intent(out) :: v_out(:,:,:)
+      logical, optional, intent(in) :: rotate
+      integer, optional, intent(out) :: rc
+
+      character(len=*), parameter :: Iam = MOD_NAME//'regrid_vector_3d_real32'
+
+      _UNUSED_DUMMY(this)
+      _UNUSED_DUMMY(rotate)
+
+      _ASSERT(size(u_in,3) == size(u_out,3), 'inconsistent array shape')
+      _ASSERT(size(v_in,3) == size(v_out,3), 'inconsistent array shape')
+      _ASSERT(size(u_in,3) == size(v_in,3), 'inconsistent array shape')
+
+      u_out = u_in
+      v_out = v_in
+
+     _RETURN(_SUCCESS)
+
+   end subroutine regrid_vector_3d_real32
+
+!$$
+!$$   function clone(this)
+!$$      class (AbstractRegridder), allocatable :: clone
+!$$      class (IdentityRegridder), intent(in) :: this
+!$$
+!$$      ! We just need the type - not the details, so we copy an empty object.
+!$$      type (IdentityRegridder) :: foo
+!$$
+!$$      allocate(clone, source=foo)
+!$$
+!$$   end function clone
+!$$
+   ! do nothing
+   subroutine initialize_subclass(this, unusable, rc)
+      use MAPL_KeywordEnforcerMod
+      use MAPL_RegridderSpec
+      class (IdentityRegridder), intent(inout) :: this
+      class (KeywordEnforcer), optional, intent(in) :: unusable
+      integer, optional, intent(out) :: rc
+
+      character(len=*), parameter :: Iam = 'initialize_subclass'
+
+      _UNUSED_DUMMY(this)
+      _UNUSED_DUMMY(unusable)
+      _UNUSED_DUMMY(rc)
+
+   end subroutine initialize_subclass
+
+end module MAPL_IdentityRegridderMod
---
+++ dev-doc/src/OpenMPServer.F90
@@ -0,0 +1,80 @@
+module pFIO_OpenMPServerMod
+   use pFIO_AbstractDirectoryServiceMod
+   use pFIO_ServerThreadMod
+   use pFIO_AbstractDataReferenceMod
+   use pFIO_AbstractSocketMod
+   use pFIO_AbstractSocketVectorMod
+   use pFIO_AbstractServerMod
+   use pFIO_BaseServerMod
+   use pFIO_IntegerIntegerMapMod
+
+   implicit none
+   private
+
+   public :: OpenMPServer
+
+   type,extends(BaseServer) :: OpenMPServer
+   contains
+      procedure :: start
+   end type OpenMPServer
+
+   interface OpenMPServer
+      module procedure new_OpenMPServer
+   end interface OpenMPServer
+
+
+contains
+
+   function new_OpenMPServer(comm,directory_service) result(s)
+      type (OpenMPServer) :: s
+      integer, intent(in) :: comm
+      class(AbstractDirectoryService), target, intent(inout) :: directory_service
+
+      call s%init(comm)
+     
+      s%directory_service => directory_service
+      call s%directory_service%publish(PortInfo('i_server'), s%comm)
+
+   end function new_OpenMPServer
+
+
+   subroutine start(this)
+      class (OpenMPServer),target, intent(inout) :: this
+      class (AbstractSocket), pointer :: sckt
+      type (AbstractSocketVector) :: sockets
+      class (ServerThread),pointer :: threadPtr
+      integer :: i,thread_size
+
+      this%terminate = .false.
+      sockets = AbstractSocketVector()
+
+      !$omp parallel shared(this,sockets) 
+      !$omp single
+
+      call this%directory_service%connect_to_client(PortInfo('i_server'),this%comm,sockets,this%terminate)
+      this%num_clients=this%num_clients+sockets%size()
+          
+      thread_size = sockets%size()
+
+      do i =1,thread_size
+         sckt => sockets%at(i)
+         !$omp task private(threadPtr),firstprivate(sckt),shared(this)
+         allocate(threadPtr,source = ServerThread(sckt,this))
+         do while (.true.)
+            call threadPtr%run()
+            if(threadPtr%do_terminate()) then
+               deallocate(threadPtr)
+               exit
+            endif
+         enddo
+         !$omp end task
+       enddo
+
+      !$omp taskwait
+      !$omp end single
+      !$omp end parallel
+      print*, "terminated successfully"
+
+   end subroutine start
+
+end module pFIO_OpenMPServerMod
