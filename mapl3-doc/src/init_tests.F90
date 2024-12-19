ffcd3154ddd5b8537fd3826bb040d499b872f22b
mathomp4 <fortran@gmail.com>
2024-12-19 14:06:40 +0000

    Deploying to gh-pages from @ GEOS-ESM/MAPL@1c55973c244834ed0d78c39964d2738c3d8f8a8b 🚀

--- mapl3-doc/src/init_tests.F90
+++
@@ -1,8 +0,0 @@
-subroutine init_tests()
-   use MAPL_pFUnit_ThrowMod, only: pFUnit_throw
-   use MAPL_ThrowMod
-   implicit none
-
-   call MAPL_set_throw_method(pfunit_throw)
-
-end subroutine init_tests
