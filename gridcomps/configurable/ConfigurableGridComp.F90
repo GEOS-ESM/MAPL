908126c5d7961ec0b95995a0f54bc75649a789c7
Purnendu Chakraborty <purnendu.chakraborty@nasa.gov>
2025-01-09 14:02:53 -0500

    Added ability for Congfigurable GridComp to read k_values and update field

--- gridcomps/configurable/ConfigurableGridComp.F90
+++ gridcomps/configurable/ConfigurableGridComp.F90
@@ -1,15 +1,22 @@
 #include "MAPL_Generic.h"
 
-module ConfigurableGridComp
+module mapl3g_ConfigurableGridComp
 
    use mapl_ErrorHandling
    use mapl3g_Generic, only: MAPL_GridCompSetEntryPoint, MAPL_RunChildren
+   use mapl3g_Generic, only: MAPL_GridCompGet
+   use mapl, only: MAPL_GetPointer
    use esmf
 
    implicit none
    private
 
    public :: setServices
+
+   character(*), parameter :: MAPL_SECTION = "mapl"
+   character(*), parameter :: COMPONENT_STATES_SECTION = "states"
+   character(*), parameter :: COMPONENT_EXPORT_STATE_SECTION = "export"
+   character(*), parameter :: KEY_K_VALUES = "k_values"
 
 contains
 
@@ -32,12 +39,42 @@ contains
       type(ESMF_Clock) :: clock
       integer, intent(out) :: rc
 
-      integer :: status
+      type(ESMF_FieldBundle) :: export_bundle
+      type(ESMF_Field), allocatable :: field_list(:)
+      character(len=ESMF_MAXSTR) :: field_name
+      type(ESMF_HConfig) :: hconfig, mapl_cfg, states_cfg, export_cfg, var_cfg
+      logical :: has_k_values
+      real(kind=ESMF_KIND_R4), allocatable :: k_values(:)
+      real(kind=ESMF_KIND_R4), pointer :: ptr3d(:, :, :)
+      integer :: field_count, idx, i, j, shape_(3), status
+
+      call MAPL_GridCompGet(gridcomp, hconfig=hconfig, _RC)
+      mapl_cfg = ESMF_HConfigCreateAt(hconfig, keyString=MAPL_SECTION, _RC)
+      states_cfg = ESMF_HConfigCreateAt(mapl_cfg, keyString=COMPONENT_STATES_SECTION, _RC)
+      export_cfg = ESMF_HConfigCreateAt(states_cfg, keyString=COMPONENT_EXPORT_STATE_SECTION, _RC)
+
+      ! For each field getting 'export'ed, check hconfig and use k_values if specified
+      export_bundle = get_bundle_from_state_(exportState, _RC)
+      call ESMF_FieldBundleGet(export_bundle, fieldCount=field_count, _RC)
+      allocate(field_list(field_count), _STAT)
+      call ESMF_FieldBundleGet(export_bundle, fieldList=field_list, _RC)
+      do idx = 1, field_count
+         call ESMF_FieldGet(field_list(idx), name=field_name, _RC)
+         var_cfg = ESMF_HConfigCreateAt(export_cfg, keyString=trim(field_name), _RC)
+         has_k_values = ESMF_HConfigIsDefined(var_cfg, keyString=KEY_K_VALUES, _RC)
+         if (has_k_values) then
+            k_values = ESMF_HConfigAsR4Seq(var_cfg, keyString=KEY_K_VALUES, _RC)
+            call MAPL_GetPointer(exportState, ptr3d, trim(field_name), _RC)
+            shape_ = shape(ptr3d)
+            _ASSERT(shape_(3) == size(k_values), "incorrect number of k_values")
+            do concurrent(i = 1:shape_(1), j=1:shape_(2))
+               ptr3d(i, j, :) = k_values
+            end do
+         end if
+      end do
 
       _RETURN(_SUCCESS)
-      _UNUSED_DUMMY(gridcomp)
       _UNUSED_DUMMY(importState)
-      _UNUSED_DUMMY(exportState)
       _UNUSED_DUMMY(clock)
    end subroutine init
 
@@ -58,12 +95,45 @@ contains
       _UNUSED_DUMMY(clock)
    end subroutine run
 
-end module ConfigurableGridComp
+   type(ESMF_FieldBundle) function get_bundle_from_state_(state, rc) result(bundle)
+      ! Arguments
+      type(ESMF_State), intent(in) :: state
+      integer, optional, intent(out) :: rc
+
+      ! Locals
+      character(len=ESMF_MAXSTR), allocatable :: item_name(:)
+      type (ESMF_StateItem_Flag), allocatable  :: item_type(:)
+      type(ESMF_Field) :: field
+      type(ESMF_FieldStatus_Flag) :: field_status
+      integer :: item_count, idx, status
+
+      ! bundle to pack fields in
+      bundle = ESMF_FieldBundleCreate(_RC)
+      call ESMF_StateGet(state, itemCount=item_count, _RC)
+      allocate(item_name(item_count), _STAT)
+      allocate(item_type(item_count), _STAT)
+      call ESMF_StateGet(state, itemNameList=item_name, itemTypeList=item_type, _RC)
+      do idx = 1, item_count
+         if (item_type(idx) /= ESMF_STATEITEM_FIELD) then
+            _FAIL("FieldBundle has not been implemented yet")
+         end if
+         call ESMF_StateGet(state, item_name(idx), field, _RC)
+         call ESMF_FieldGet(field, status=field_status, _RC)
+         if (field_status == ESMF_FIELDSTATUS_COMPLETE) then
+            call ESMF_FieldBundleAdd(bundle, [field], _RC)
+         end if
+      end do
+      deallocate(item_name, item_type, _STAT)
+
+      _RETURN(_SUCCESS)
+   end function get_bundle_from_state_
+
+end module Mapl3g_ConfigurableGridComp
 
 subroutine setServices(gridcomp, rc)
    use ESMF
    use MAPL_ErrorHandlingMod
-   use ConfigurableGridComp, only: Configurable_setServices => SetServices
+   use mapl3g_ConfigurableGridComp, only: Configurable_setServices => SetServices
    type(ESMF_GridComp)  :: gridcomp
    integer, intent(out) :: rc
 
