bca4af6d7731b73526b6e065255fd43102635fe1
Purnendu Chakraborty <purnendu.chakraborty@nasa.gov>
2025-01-10 11:12:24 -0500

    Renamed k_values -> vertical_profile

--- gridcomps/configurable/ConfigurableGridComp.F90
+++ gridcomps/configurable/ConfigurableGridComp.F90
@@ -16,7 +16,7 @@ module mapl3g_ConfigurableGridComp
    character(*), parameter :: MAPL_SECTION = "mapl"
    character(*), parameter :: COMPONENT_STATES_SECTION = "states"
    character(*), parameter :: COMPONENT_EXPORT_STATE_SECTION = "export"
-   character(*), parameter :: KEY_K_VALUES = "k_values"
+   character(*), parameter :: KEY_DEFAULT_VERT_PROFILE = "default_vertical_profile"
 
 contains
 
@@ -41,8 +41,8 @@ contains
 
       character(:), allocatable :: field_name
       type(ESMF_HConfig) :: hconfig, mapl_cfg, states_cfg, export_cfg, field_cfg
-      logical :: has_export_section, has_k_values
-      real(kind=ESMF_KIND_R4), allocatable :: k_values(:)
+      logical :: has_export_section, has_default_vert_profile
+      real(kind=ESMF_KIND_R4), allocatable :: default_vert_profile(:)
       real(kind=ESMF_KIND_R4), pointer :: ptr3d(:, :, :)
       integer :: ii, jj, shape_(3), status
 
@@ -55,7 +55,7 @@ contains
       has_export_section = ESMF_HConfigIsDefined(states_cfg, keyString=COMPONENT_EXPORT_STATE_SECTION, _RC)
       _RETURN_UNLESS(has_export_section)
 
-      ! For each field getting 'export'ed, check hconfig and use k_values if specified
+      ! For each field getting 'export'ed, check hconfig and use default_vert_profile if specified
       export_cfg = ESMF_HConfigCreateAt(states_cfg, keyString=COMPONENT_EXPORT_STATE_SECTION, _RC)
       b = ESMF_HConfigIterBegin(export_cfg, _RC)
       e = ESMF_HConfigIterEnd(export_cfg, _RC)
@@ -64,16 +64,15 @@ contains
          field_name = ESMF_HConfigAsStringMapKey(iter, _RC)
          ! print *, "FIELD: ", field_name
          field_cfg = ESMF_HConfigCreateAtMapVal(iter, _RC)
-         has_k_values = ESMF_HConfigIsDefined(field_cfg, keyString=KEY_K_VALUES, _RC)
-         if (has_k_values) then
-            k_values = ESMF_HConfigAsR4Seq(field_cfg, keyString=KEY_K_VALUES, _RC)
-            ! print *, "K VALUES: ", k_values
+         has_default_vert_profile = ESMF_HConfigIsDefined(field_cfg, keyString=KEY_DEFAULT_VERT_PROFILE, _RC)
+         if (has_default_vert_profile) then
+            default_vert_profile = ESMF_HConfigAsR4Seq(field_cfg, keyString=KEY_DEFAULT_VERT_PROFILE, _RC)
             call MAPL_GetPointer(exportState, ptr3d, trim(field_name), _RC)
             shape_ = shape(ptr3d)
-            _ASSERT(shape_(3) == size(k_values), "incorrect number of k_values")
+            _ASSERT(shape_(3) == size(default_vert_profile), "incorrect size of vertical profile")
             print *, ptr3d(1, 4, 3)
             do concurrent(ii = 1:shape_(1), jj=1:shape_(2))
-               ptr3d(ii, jj, :) = k_values
+               ptr3d(ii, jj, :) = default_vert_profile
             end do
             print *, ptr3d(1, 4, 3)
          end if
