ab6159ea37ffc10c821c3f59bc171f22928bddbd
Darian Boggs <william.d.boggs@nasa.gov>
2025-01-17 17:18:08 -0500

    Switch to ESMF iso parsing.

--- generic3g/ComponentSpecParser/parse_timestep.F90
+++ generic3g/ComponentSpecParser/parse_timestep.F90
@@ -1,7 +1,6 @@
 #include "MAPL_ErrLog.h"
 
 submodule (mapl3g_ComponentSpecParser) parse_timestep_smod
-   use MAPL_TimeStringConversion, only: string_to_esmf_timeinterval, string_to_esmf_time
 contains
 
       module subroutine parse_timestep(hconfig, timestep, start, rc)
@@ -14,16 +13,22 @@ contains
          logical :: has_timestep, has_start
          character(len=32) :: iso_datetime
          character(len=128) :: iso_duration
+         type(ESMF_Time) :: time
+         type(ESMF_TimeInterval) :: interval
 
          has_timestep = ESMF_HConfigIsDefined(hconfig, keyString=KEY_TIMESTEP, _RC)
          _RETURN_UNLESS(has_timestep)
          iso_duration = ESMF_HConfigAsString(hconfig, keyString=KEY_TIMESTEP, _RC)
-         timestep = string_to_esmf_timeinterval(trim(iso_duration), _RC)
+!         timestep = string_to_esmf_timeinterval(trim(iso_duration), _RC)
+         call ESMF_TimeIntervalSet(interval, timeIntervalString=iso_duration, _RC)
+         allocate(timestep, source=interval)
 
          has_start = ESMF_HConfigIsDefined(hconfig, keyString=KEY_TIMESTEP_START, _RC)
          _RETURN_UNLESS(has_start)
          iso_datetime = ESMF_HConfigAsString(hconfig, keyString=KEY_TIMESTEP_START, _RC)
-         start = string_to_esmf_time(iso_datetime, _RC)
+!         start = string_to_esmf_time(iso_datetime, _RC)
+         call ESMF_TimeSet(time, timeString=iso_datetime, _RC)
+         allocate(start, source=time)
 
          _RETURN(_SUCCESS)
 
