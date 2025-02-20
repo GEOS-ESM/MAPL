81c2723ad480f5a79709b0bc8927b2eb635e1dfe
Benjamin Auer <benjamin.m.auer@nasa.gov>
2025-02-20 14:44:03 -0500

    add some loggin prints

--- gridcomps/ExtData2G/ExtDataGridCompNG.F90
+++ gridcomps/ExtData2G/ExtDataGridCompNG.F90
@@ -692,9 +692,6 @@ CONTAINS
 
       if (do_pointer_update(i)) then
 
-         call extdata_lgr%debug('ExtData Run_: INTERP_LOOP: interpolating between bracket times, variable: %a, file: %a', &
-              & trim(current_base_name), trim(item%file_template))
-
          call MAPL_ExtDataVerticalInterpolate(self,item,import,_RC)
 
       endif
@@ -951,6 +948,7 @@ CONTAINS
 
      if (item%allow_vertical_regrid .and. (item%vcoord%vertical_type == model_pressure)) then
 
+        call extdata_lgr%info('ExtData vertical conservative regridding of '//trim(item%name))
         call ESMF_StateGet(import, "PLE", dst_ple, _RC)
         call ESMF_FieldGet(dst_ple,farrayPtr=dst_ple_ptr,_RC)
         src_ps_name = item%vcoord%surf_name//"_"//trim(item%vcomp1)
@@ -980,6 +978,7 @@ CONTAINS
            _FAIL(units_in//" not supported for vertical regridding")
         end select
      else if (item%vcoord%vertical_type == simple_coord .and. item%do_fill) then
+        call extdata_lgr%info('ExtData filling destination with available layers of source for '//trim(item%name))
         call ESMF_FieldBundleGet(item%t_interp_bundle, trim(item%vcomp1), field=src_field, _RC)
         call ESMF_StateGet(MAPLExtState%ExtDataState,trim(item%vcomp1),dst_field,_RC)
         call MAPL_ExtDataFillField(item, dst_field, src_field, _RC) 
