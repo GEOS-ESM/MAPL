77418892bbab9a362ed5d5e8599d36386c5f3fda
Tom Clune <thomas.l.clune@nasa.gov>
2025-04-16 09:26:31 -0400

    Renaming for consistency.
    Arg names for StateGetPointer are now spelled same as corresponding
    ESMF args.

--- state/StateGetPointer.F90
+++ state/StateGetPointer.F90
@@ -11,14 +11,14 @@ module mapl3g_StateGetPointer
    public :: StateGetPointer
 
    interface StateGetPointer
-      procedure state_get_fArrayPtr_r4_1d
-      procedure state_get_fArrayPtr_r4_2d
-      procedure state_get_fArrayPtr_r4_3d
-      procedure state_get_fArrayPtr_r4_4d
-      procedure state_get_fArrayPtr_r8_1d
-      procedure state_get_fArrayPtr_r8_2d
-      procedure state_get_fArrayPtr_r8_3d
-      procedure state_get_fArrayPtr_r8_4d
+      module procedure state_get_array_ptr_r4_1d
+      module procedure state_get_array_ptr_r4_2d
+      module procedure state_get_array_ptr_r4_3d
+      module procedure state_get_array_ptr_r4_4d
+      module procedure state_get_array_ptr_r8_1d
+      module procedure state_get_array_ptr_r8_2d
+      module procedure state_get_array_ptr_r8_3d
+      module procedure state_get_array_ptr_r8_4d
    end interface StateGetPointer
 
 contains
@@ -27,7 +27,7 @@ contains
 #  undef NAME_
 #endif 
 
-#define NAME_ state_get_fArrayPtr
+#define NAME_ state_get_array_ptr
 
 #ifdef TYPEKIND_
 #  undef TYPEKIND_
@@ -38,22 +38,22 @@ contains
    
 ! StateGetPointerToDataR4_1
 #define RANK_ 1
-#include "get_fArrayPtr_template.H"
+#include "get_array_ptr_template.H"
 #undef RANK_
 
 ! StateGetPointerToDataR4_2
 #define RANK_ 2
-#include "get_fArrayPtr_template.H"
+#include "get_array_ptr_template.H"
 #undef RANK_
 
 ! StateGetPointerToDataR4_3
 #define RANK_ 3
-#include "get_fArrayPtr_template.H"
+#include "get_array_ptr_template.H"
 #undef RANK_
 
 ! StateGetPointerToDataR4_4
 #define RANK_ 4
-#include "get_fArrayPtr_template.H"
+#include "get_array_ptr_template.H"
 #undef RANK_
 
 #undef TYPEKIND_
@@ -62,22 +62,22 @@ contains
 
 ! StateGetPointerToDataR8_1
 #define RANK_ 1
-#include "get_fArrayPtr_template.H"
+#include "get_array_ptr_template.H"
 #undef RANK_
 
 ! StateGetPointerToDataR8_2
 #define RANK_ 2
-#include "get_fArrayPtr_template.H"
+#include "get_array_ptr_template.H"
 #undef RANK_
 
 ! StateGetPointerToDataR8_3
 #define RANK_ 3
-#include "get_fArrayPtr_template.H"
+#include "get_array_ptr_template.H"
 #undef RANK_
 
 ! StateGetPointerToDataR8_4
 #define RANK_ 4
-#include "get_fArrayPtr_template.H"
+#include "get_array_ptr_template.H"
 #undef RANK_
 
 #undef TYPEKIND_
