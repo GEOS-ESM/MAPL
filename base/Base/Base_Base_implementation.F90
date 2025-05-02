7c0d0488557df16f858b275be9766a85bcf961e4
Matthew Thompson <matthew.thompson@nasa.gov>
2025-05-02 09:44:41 -0400

    v2.53: Add ability to select ESMF_PIN

--- base/Base/Base_Base_implementation.F90
+++ base/Base/Base_Base_implementation.F90
@@ -137,6 +137,7 @@ contains
     type(ESMF_Pin_Flag) :: pinflag
     type(ESMF_VM)       :: vm
     logical             :: ssiSharedMemoryEnabled
+    character(len=:), allocatable :: pinflag_str
 ! SSI
 
     call ESMF_FieldGet(field, grid=GRID, _RC)
@@ -169,8 +170,21 @@ contains
 
     call ESMF_VMGet(vm, ssiSharedMemoryEnabledFlag=ssiSharedMemoryEnabled, _RC)
 
-    _ASSERT(ssiSharedMemoryEnabled, 'SSI shared memory is NOT supported')
-    pinflag=ESMF_PIN_DE_TO_SSI_CONTIG  ! requires support for SSI shared memory
+    select case (pinflag_str)
+    case ('PET')
+       pinflag = ESMF_PIN_DE_TO_PET
+    case ('VAS')
+       pinflag = ESMF_PIN_DE_TO_VAS
+    case ('SSI')
+       _ASSERT(ssiSharedMemoryEnabled, 'SSI shared memory is NOT supported')
+       pinflag = ESMF_PIN_DE_TO_SSI
+    case ('SSI_CONTIG')
+       _ASSERT(ssiSharedMemoryEnabled, 'SSI shared memory is NOT supported')
+       pinflag = ESMF_PIN_DE_TO_SSI_CONTIG
+    case default
+       _ASSERT(ssiSharedMemoryEnabled, 'SSI shared memory is NOT supported')
+       pinflag = ESMF_PIN_DE_TO_SSI_CONTIG
+    end select
 ! SSI
 
     Dimensionality: select case(DIMS)
@@ -2797,8 +2811,8 @@ contains
     _ASSERT( IM_WORLD*6 == JM_WORLD, "It only works for cubed-sphere grid")
 
     allocate(lons(npts),lats(npts))
-    
-    call MAPL_Reverse_Schmidt(Grid, stretched, npts, lon=lon, lat=lat, lonR8=lonR8, latR8=latR8, lonRe=lons, latRe=lats, _RC) 
+
+    call MAPL_Reverse_Schmidt(Grid, stretched, npts, lon=lon, lat=lat, lonR8=lonR8, latR8=latR8, lonRe=lons, latRe=lats, _RC)
 
     dalpha = 2.0d0*alpha/IM_WORLD
 
@@ -2918,7 +2932,7 @@ contains
        if ( I1 == 1 .and. J1 == 1 ) then
           allocate(lonRe(local_dims(2)),      latRe(local_dims(2)))
           call MAPL_Reverse_Schmidt(grid, stretched, local_dims(2), lonR8=corner_lons(1,1:local_dims(2)), &
-                                   latR8=corner_lats(1,1:local_dims(2)), lonRe=lonRe, latRe=latRe, _RC)  
+                                   latR8=corner_lats(1,1:local_dims(2)), lonRe=lonRe, latRe=latRe, _RC)
 
           allocate(accurate_lon(local_dims(2)), accurate_lat(local_dims(2)))
 
@@ -3413,32 +3427,32 @@ contains
   end function MAPL_GetCorrectedPhase
 
   module subroutine MAPL_Reverse_Schmidt(Grid, stretched, npts, lon, lat, lonR8, latR8, lonRe, latRe, rc)
-     type(ESMF_Grid), intent(inout) :: Grid 
+     type(ESMF_Grid), intent(inout) :: Grid
      logical, intent(out)           :: stretched
      integer,                      intent(in   ) :: npts        ! number of points in lat and lon arrays
      real, optional,               intent(in   ) :: lon(npts)   ! array of longitudes in radians
      real, optional,               intent(in   ) :: lat(npts)   ! array of latitudes in radians
      real(ESMF_KIND_R8), optional, intent(in   ) :: lonR8(npts) ! array of longitudes in radians
-     real(ESMF_KIND_R8), optional, intent(in   ) :: latR8(npts) !     
-     real(ESMF_KIND_R8), optional, intent(out  ) :: lonRe(npts) ! 
-     real(ESMF_KIND_R8), optional, intent(out  ) :: latRe(npts) ! 
+     real(ESMF_KIND_R8), optional, intent(in   ) :: latR8(npts) !
+     real(ESMF_KIND_R8), optional, intent(out  ) :: lonRe(npts) !
+     real(ESMF_KIND_R8), optional, intent(out  ) :: latRe(npts) !
      integer, optional, intent(out) :: rc
 
      logical :: factorPresent, lonPresent, latPresent
      integer :: status
      real(ESMF_KIND_R8) :: c2p1, c2m1, half_pi, two_pi, stretch_factor, target_lon, target_lat
-     real(ESMF_KIND_R8), dimension(npts) :: x,y,z, Xx, Yy, Zz  
+     real(ESMF_KIND_R8), dimension(npts) :: x,y,z, Xx, Yy, Zz
      logical, dimension(npts) :: n_s
 
      _RETURN_IF( npts == 0 )
- 
+
      call ESMF_AttributeGet(grid, name='STRETCH_FACTOR', isPresent= factorPresent, _RC)
      call ESMF_AttributeGet(grid, name='TARGET_LON',     isPresent= lonPresent,    _RC)
      call ESMF_AttributeGet(grid, name='TARGET_LAT',     isPresent= latPresent,    _RC)
 
      if ( factorPresent .and. lonPresent .and. latPresent) then
         stretched = .true.
-     else 
+     else
         stretched = .false.
      endif
 
@@ -3464,7 +3478,7 @@ contains
      call ESMF_AttributeGet(grid, name='TARGET_LON',     value=target_lon,     _RC)
      call ESMF_AttributeGet(grid, name='TARGET_LAT',     value=target_lat,     _RC)
 
-     c2p1 = 1 + stretch_factor*stretch_factor 
+     c2p1 = 1 + stretch_factor*stretch_factor
      c2m1 = 1 - stretch_factor*stretch_factor
 
      half_pi = MAPL_PI_R8/2
@@ -3472,7 +3486,7 @@ contains
 
      target_lon = target_lon*MAPL_DEGREES_TO_RADIANS_R8
      target_lat = target_lat*MAPL_DEGREES_TO_RADIANS_R8
-  
+
      x = cos(latRe)*cos(lonRe - target_lon)
      y = cos(latRe)*sin(lonRe - target_lon)
      z = sin(latRe)
