86df603ca9fe31fc415c856b84bae4f5ce08d884
Purnendu Chakraborty <purnendu.chakraborty@nasa.gov>
2025-05-02 11:06:32 -0400

    Peeled off vert-only code from new_pFIOServerBounds

--- GeomIO/pFIOServerBounds.F90
+++ GeomIO/pFIOServerBounds.F90
@@ -13,7 +13,7 @@ module mapl3g_pFIOServerBounds
 
    public :: pFIOServerBounds
 
-   integer, parameter :: DEFAULT_GRID_DIMS = 2
+   integer, parameter :: grid_dims = 2
 
    type :: pFIOServerBounds
       private
@@ -29,7 +29,7 @@ module mapl3g_pFIOServerBounds
    end type pFIOServerBounds
 
    interface pFIOServerBounds
-      procedure new_pFIOServerBounds
+      procedure new_pFIOServerBounds_grid
    end interface pFIOServerBounds
 
 contains
@@ -37,46 +37,81 @@ contains
    function get_local_start(this) result(local_start)
       integer, allocatable :: local_start(:)
       class(pFIOServerBounds), intent(in) :: this
-      local_start =this%local_start
+      local_start = this%local_start
    end function get_local_start
 
    function get_global_start(this) result(global_start)
       integer, allocatable :: global_start(:)
       class(pFIOServerBounds), intent(in) :: this
-      global_start =this%global_start
+      global_start = this%global_start
    end function get_global_start
 
    function get_global_count(this) result(global_count)
       integer, allocatable :: global_count(:)
       class(pFIOServerBounds), intent(in) :: this
-      global_count =this%global_count
+      global_count = this%global_count
    end function get_global_count
 
    function get_file_shape(this) result(file_shape)
       integer, allocatable :: file_shape(:)
       class(pFIOServerBounds), intent(in) :: this
-      file_shape =this%file_shape
+      file_shape = this%file_shape
    end function get_file_shape
 
-   function new_pFIOServerBounds(grid, field_shape, time_index, rc) result(server_bounds)
+   function new_pFIOServerBounds_grid(grid, field_shape, time_index, rc) result(server_bounds)
       type(ESMF_Grid), intent(in) :: grid
       integer, intent(in) :: field_shape(:)
       integer, intent(in), optional :: time_index
       integer, intent(out), optional :: rc
-
       type(pFIOServerBounds) :: server_bounds
+
+      integer :: status
       logical :: vert_only
-      integer :: status, tile_count, n_dims, tm, global_dim(3)
-      integer :: i1, in, j1, jn, tile, extra_file_dim, file_dims, grid_dims, new_grid_dims
 
       vert_only = .false.
-      if (size(field_shape)==1) vert_only = .true.
+      if (size(field_shape) == 1) vert_only = .true.
 
-      grid_dims = DEFAULT_GRID_DIMS
-      if (vert_only) grid_dims = 0
+      if (vert_only) then
+         server_bounds = pFIOServerBounds_vert_only_field(field_shape(1), time_index, _RC)
+      else
+         server_bounds = pFIOServerBounds_gridded_field(grid, field_shape, time_index, _RC)
+      end if
+
+      _RETURN(_SUCCESS)
+   end function new_pFIOServerBounds_grid
+
+   function pFIOServerBounds_vert_only_field(num_field_levels, time_index, rc) result(server_bounds)
+      integer, intent(in) :: num_field_levels
+      integer, intent(in), optional :: time_index
+      integer, intent(out), optional :: rc
+      type(pFIOServerBounds) :: server_bounds ! result
+
+      integer, parameter :: file_dims = 1
+      integer :: tm
+
+      tm = 0
+      if (present(time_index)) tm = 1
+      
+      allocate(server_bounds%file_shape(1), source=num_field_levels)
+      allocate(server_bounds%local_start(1+tm), source=1)
+      allocate(server_bounds%global_start(1+tm), source=1)
+      allocate(server_bounds%global_count(1+tm), source=1)
+      server_bounds%global_count(1) = num_field_levels
+
+      _RETURN(_SUCCESS)
+   end function pFIOServerBounds_vert_only_field
+
+   function pFIOServerBounds_gridded_field(grid, field_shape, time_index, rc) result(server_bounds)
+      type(ESMF_Grid), intent(in) :: grid
+      integer, intent(in) :: field_shape(:)
+      integer, intent(in), optional :: time_index
+      integer, intent(out), optional :: rc
+      type(pFIOServerBounds) :: server_bounds ! field
+
+      integer :: status, tile_count, n_dims, tm, global_dim(3)
+      integer :: i1, in, j1, jn, tile, extra_file_dim, file_dims, new_grid_dims
 
       call ESMF_GridGet(grid, tileCount=tile_count, _RC)
-      if (vert_only) tile_count = 1
       call MAPL2_GridGetInterior(grid, i1, in, j1, jn)
       call MAPL2_GridGet(grid, globalCellCountPerDim=global_dim, _RC)
       n_dims = size(field_shape)
@@ -115,21 +150,17 @@ contains
 
       case (1)
 
-         if (vert_only) then
-            server_bounds%file_shape(1:new_grid_dims) = [integer ::]
-            server_bounds%global_count(1:new_grid_dims) = [integer ::]
-            server_bounds%local_start(1:new_grid_dims) = [integer ::]
-         else
-            server_bounds%file_shape(1:new_grid_dims) = [field_shape(1), field_shape(2)]
-            server_bounds%global_count(1:new_grid_dims) = [global_dim(1), global_dim(2)]
-            server_bounds%local_start(1:new_grid_dims) = [i1,j1]
-         end if
+         server_bounds%file_shape(1:new_grid_dims) = [field_shape(1), field_shape(2)]
+         server_bounds%global_count(1:new_grid_dims) = [global_dim(1), global_dim(2)]
+         server_bounds%local_start(1:new_grid_dims) = [i1,j1]
 
       case default
+
          _FAIL("unsupported grid")
+
       end select
 
       _RETURN(_SUCCESS)
-   end function new_pFIOServerBounds
+   end function pFIOServerBounds_gridded_field
 
 end module mapl3g_pFIOServerBounds
