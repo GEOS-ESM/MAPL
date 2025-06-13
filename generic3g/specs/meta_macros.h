c145909ae56c3e5ad2d3fbf70a4b0002aef4ef12
Darian Boggs <william.d.boggs@nasa.gov>
2025-06-13 09:20:15 -0400

    changes to macros for validate VariableSpec

--- generic3g/specs/meta_macros.h
+++ generic3g/specs/meta_macros.h
@@ -1,4 +1,4 @@
 #include "undef_macros.h"
-#define _INVALID(V) "Invalid " // #V
-#define _ASSERT_SPEC_VALUE(V, F) if(present(spec%V)) then;_ASSERT(F(spec%V), _INVALID(V));end if
 #define _ISIN(V) findloc(_SET, spec%V) >= lbound(_SET)
+#define _ISIN_STRINGVECTOR(V) is_stringvector_subset(V, _SET)
+#define _ISIN_RANGES(V) is_in(V, _SET)
