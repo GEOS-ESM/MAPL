3836e7a4747f9252d0a54df3bc532ff3923a1c23
Darian Boggs <william.d.boggs@nasa.gov>
2025-06-17 00:20:06 -0400

    Update CMakeLists; rm unused include files

--- generic3g/specs/meta_macros.h
+++
@@ -1,8 +0,0 @@
-#include "undef_macros.h"
-#define _ISIN(V) findloc(_SET, spec%V) >= lbound(_SET)
-#define _ISIN_STRINGVECTOR(V) is_stringvector_subset(V, _SET)
-#define _ISIN_RANGES(V) is_in(V, _SET)
-
-#define _ISIN_(V, S) findloc(S, spec%V) >= lbound(S)
-#define _VECTOR_ISIN_VECTOR_(V1, V2) is_stringvector_subset(V1, V2)
-#define _ISIN_RANGES_(V, R) is_in(V, R)
