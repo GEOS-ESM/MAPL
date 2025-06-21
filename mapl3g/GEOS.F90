4301423c8740ae1c0e2213abf12c6907391ee42b
Tom Clune <thomas.l.clune@nasa.gov>
2025-06-21 14:32:07 -0400

    Weird merge artifact.
    Somehow an obsolete interface was saved in GEOS.F90.

--- mapl3g/GEOS.F90
+++ mapl3g/GEOS.F90
@@ -11,7 +11,7 @@ program geos
    logical :: is_model_pet
    type(ESMF_GridComp), allocatable :: servers(:)
 
-   call MAPL_Initialize(hconfig=hconfig, is_model_pet=is_model_pet, servers=servers, configFileNameFromArgNum=1, _RC)
+   call MAPL_Initialize(hconfig=hconfig, is_model_pet=is_model_pet, servers=servers, _RC)
    call run_geos(hconfig, is_model_pet=is_model_pet, servers=servers, _RC)
    call MAPL_Finalize(_RC)
 
