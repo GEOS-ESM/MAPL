6c59167534954b8793db00e19104e2100e708f3f
Tom Clune <thomas.l.clune@nasa.gov>
2025-06-21 15:54:52 -0400

    Fixes #3805 (logging) (#3815)
    * Fixes #3805
    
    Needs further work, but at least this commit clarfies how to proceed.
    
    * Workarounds for gfortran
    
    Gfortran 14 still does not like passing strings to CLASS(*) dummy args
    when the actual argument is a function return value.
    
    * Weird merge artifact.
    
    Somehow an obsolete interface was saved in GEOS.F90.
    
    * fixing previous mistake
    
    * Fix for NOAA build without pFlogger
    
    ---------
    
    Co-authored-by: Matt Thompson <matthew.thompson@nasa.gov>

--- generic3g/OuterMetaComponent/add_child_by_spec.F90
+++ generic3g/OuterMetaComponent/add_child_by_spec.F90
@@ -29,6 +29,8 @@ contains
       type(ESMF_GridComp) :: child_outer_gc
       type(OuterMetaComponent), pointer :: child_meta
       type(ESMF_HConfig) :: total_hconfig
+      class(Logger), pointer :: lgr
+      character(:), allocatable :: this_name
 
       _ASSERT(is_valid_name(child_name), 'Child name <' // child_name //'> does not conform to GEOS standards.')
       _ASSERT(this%children%count(child_name) == 0, 'duplicate child name: <'//child_name//'>.')
@@ -46,6 +48,10 @@ contains
 
       child_driver = GriddedComponentDriver(child_outer_gc)
       call this%children%insert(child_name, child_driver)
+
+      lgr => this%get_logger()
+      this_name = this%get_name() ! workaround for gfortran
+      call lgr%debug('%a added child <%a~>', this_name, child_name, _RC)
 
       _RETURN(_SUCCESS)
    end subroutine add_child_by_spec
