255429d0341130f363d3f622d4fb59574fdb2c36
Tom Clune <thomas.l.clune@nasa.gov>
2025-06-21 10:21:00 -0400

    Restricting Gridcomps to use public API

--- generic3g/Generic3g.F90
+++ generic3g/Generic3g.F90
@@ -1,12 +1,8 @@
 module Generic3g
-<<<<<<< HEAD
    use mapl3g_ESMF_Subset
    use mapl3g_Generic
    use mapl3g_Field_API
    use mapl3g_VariableSpec
-=======
-   use mapl3g_Generic
->>>>>>> e5b0046f859 (Finally a compromise that passes all compilers.)
    use mapl3g_GenericPhases
    use mapl3g_OuterMetaComponent
    use mapl3g_GenericGridComp, only: MAPL_GridCompCreate
@@ -21,10 +17,6 @@ module Generic3g
    use mapl3g_ESMF_HConfigUtilities, only: MAPL_HConfigMatch
    use mapl3g_RestartHandler, only: MAPL_RESTART, MAPL_RESTART_OPTIONAL, MAPL_RESTART_SKIP
    use mapl3g_RestartHandler, only: MAPL_RESTART_REQUIRED, MAPL_RESTART_BOOT, MAPL_RESTART_SKIP_INITIAL
-<<<<<<< HEAD
    use mapl3g_VerticalStaggerLoc
    use mapl3g_geomio
-=======
->>>>>>> e5b0046f859 (Finally a compromise that passes all compilers.)
-
 end module Generic3g
