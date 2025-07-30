779f3442dad08d712be7fdde900270941d7ed1a0
Darian Boggs <61847056+darianboggs@users.noreply.github.com>
2025-07-30 17:07:54 -0400

    Add simple MAPL_HConfigGet (specific) procedures (#3929)
    * Implement new subroutine; incomplete for now.
    
    * Push down macros and templates
    
    * Clean up
    
    * Fixes to macros
    
    * Tests pass for hconfig_utils & generic3g
    
    * All tests pass.
    
    * _ASSERT message; commented out logger%info call.
    
    * Fix for pflogger error
    
    * Disable test for exception
    
    * Tests pass
    
    * Clean up macros
    
    * Move _RELATION to correct location.
    
    * Fix macro for allocatable String

--- hconfig_utils/hconfig_get.F90
+++ hconfig_utils/hconfig_get.F90
@@ -1,10 +1,8 @@
 module mapl3g_hconfig_get
 
-   use mapl3g_hconfig_get_private, only: MAPL_HConfigGet => get_value
+   use mapl3g_hconfig_get_private, only: MAPL_HConfigGet
    use mapl3g_hconfig_params, only: HConfigParams
-
    implicit none
-
    public :: MAPL_HConfigGet
 
 end module mapl3g_hconfig_get
---
+++ hconfig_utils/mapl_hconfig_get_value_macros_undef.h
@@ -0,0 +1,27 @@
+#ifdef _COMPARE
+#undef _COMPARE
+#endif
+
+#ifdef _RELATION
+#undef _RELATION
+#endif
+
+#ifdef _DIMS_
+#undef _DIMS_
+#endif
+
+#ifdef _DECL_NUM_ITEMS_
+#undef _DECL_NUM_ITEMS_
+#endif
+
+#ifdef _SET_NUM_ITEMS
+#undef _SET_NUM_ITEMS
+#endif
+
+#ifdef _WRITE_DIMS_
+#undef _WRITE_DIMS_
+#endif
+
+#ifdef _ADJUST_VALUESTRING
+#undef _ADJUST_VALUESTRING
+#endif
