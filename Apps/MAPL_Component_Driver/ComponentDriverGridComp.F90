c4d6a63d51264fb26a7abd0a0fb9182c88f47a30
Benjamin Auer <benjamin.m.auer@nasa.gov>
2025-08-07 15:47:40 -0400

    add a compare mode

--- Apps/MAPL_Component_Driver/ComponentDriverGridComp.F90
+++ Apps/MAPL_Component_Driver/ComponentDriverGridComp.F90
@@ -21,8 +21,6 @@ module mapl3g_ComponentDriverGridComp
       character(len=:), allocatable :: runMode
       type(timeVar) :: tFunc
       real :: delay ! in seconds
-      !type(StringVector) :: reserved_internal_fields
-      !type(ESMF_State) :: non_reserved_internal_state
    end type Comp_Driver_Support 
 
    character(*), parameter :: PRIVATE_STATE = "Comp_Driver_Support"
@@ -115,8 +113,6 @@ contains
       call MAPL_GridCompGet(gridcomp, hconfig=hconfig, _RC)
 
       call MAPL_GridCompGetInternalState(gridcomp, internal_state, _RC) 
-      !support%reserved_internal_fields = fill_reserved_names()
-      !call create_nonreserverd_internal_state(support%non_reserved_internal_state, support%reserved_internal_fields, internal_state, _RC)
 
       support%runMode = ESMF_HConfigAsString(hconfig, keyString='RUN_MODE', _RC)
       support%delay = -1.0
@@ -169,6 +165,7 @@ contains
          call fill_state_from_internal(exportState, internal_state, support, _RC)
          ! fill internal or export state
          ! compare import state to reference state
+         call compare_states(importState, exportState, 0.001, _RC)
       else
          _FAIL("no run mode selected")
       end if
@@ -309,8 +306,8 @@ contains
          call ESMF_StateGet(reference_state, trim(itemNameList(i)), source_type, _RC)
          _ASSERT(source_type == ESMF_StateItem_Field, 'source and destination are not both fields')
          call ESMF_StateGet(reference_state, trim(itemNameList(i)), reference_field, _RC)
-         call ESMF_FieldGet(field, 0, farrayPtr=ptr, _RC)
-         call ESMF_FieldGet(reference_field, 0, farrayPtr=reference_ptr, _RC)
+         call assign_fptr(field, ptr, _RC)
+         call assign_fptr(reference_field, reference_ptr, _RC)
          if (any(abs(ptr-reference_ptr) > threshold)) then
             _FAIL("state differs from reference state greater than allowed threshold")
          end if
@@ -318,48 +315,6 @@ contains
 
       _RETURN(_SUCCESS)
    end subroutine compare_states
-
-   !function fill_reserved_names() result(names)
-      !type(StringVector) :: names
-
-      !call names%push_back("time_interval")
-      !call names%push_back("rand")
-      !call names%push_back("grid_lons")
-      !call names%push_back("grid_lats")
-
-   !end function fill_reserved_names
-
-   !subroutine create_nonreserverd_internal_state(state, reserved_fields, original_state, rc)
-      !type(ESMF_State), intent(inout) :: state
-      !type(StringVector), intent(in) :: reserved_fields
-      !type(ESMF_State), intent(inout) :: original_state
-      !integer, optional, intent(out) :: rc
-
-      !integer :: itemCount, i, status
-      !type(ESMF_StateItem_Flag), allocatable :: itemTypeList(:)
-      !type(ESMF_StateItem_Flag) :: source_type
-      !character(len=ESMF_MAXSTR), allocatable :: itemNameList(:)
-      !type(ESMF_Field) :: field
-
-      !call ESMF_StateGet(original_state, itemCount=itemCount, _RC)
-      !allocate(itemNameList(itemCount), _STAT)
-      !allocate(itemTypeList(itemCount), _STAT)
-      !call ESMF_StateGet(original_state, itemTypeList=itemTypeList, itemNameList=itemNameList, _RC)
-      !do i=1,itemCount
-         !! check if item is in reserved_fields
-         !if (name_in_vector(reserved_fields, itemNameList(i))) cycle
-         !! if not add to state
-      !enddo
-
-      !_RETURN(_SUCCESS)
-   !end subroutine create_nonreserverd_internal_state
-
-   !function name_in_vector(vector, name) result(in_vector)
-      !logical :: in_vector
-      !type(StringVector), intent(in) :: vector
-      !character(len=*), intent(in) :: name
-
-   !end function name_in_vector
 
 end module mapl3g_ComponentDriverGridComp
 
