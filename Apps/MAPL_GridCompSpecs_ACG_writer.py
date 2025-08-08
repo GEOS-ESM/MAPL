b4c3d46adefb1b4fa3c39cb104fae88f888b367b
Darian Boggs <william.d.boggs@nasa.gov>
2025-08-08 15:52:28 -0400

    Bugfixes: 1) extra space ignored, 2) IGNORECASE

--- Apps/MAPL_GridCompSpecs_ACG_writer.py
+++ Apps/MAPL_GridCompSpecs_ACG_writer.py
@@ -9,7 +9,7 @@ COMMENT = r'#'
 FORTCOMM = r'!'
 DASH = r'-'
 RS = r' | '
-SUBROUTINE_RE = re.compile(r'call\s+MAPL_Add(?P<state>\w+)Spec\((?P<arguments>.*?)\)')
+SUBROUTINE_RE = re.compile(r'call\s+MAPL_Add(?P<state>\w+)Spec\s*\((?P<arguments>.*?)\)', re.IGNORECASE)
 
 is_quoted = lambda s: (s[0] == "'" or s[0] == '"') and s[-1] == s[0]
 is_array = lambda s: (s[0], s[-1]) == ('[', ']')
@@ -48,11 +48,14 @@ def join_line(line, lines=['']):
         lines.append('')
     return lines
 
+#==============================================================================#
 
-def parse_line(line):
+def parse_line(line, skipped=None):
     m=SUBROUTINE_RE.match(line)
     if m:
         return (m.group('state'), m.group('arguments'))
+    elif skipped is not None:
+        skipped.append(line)
     return (None, None)
 
 #==============================================================================#
@@ -63,11 +66,11 @@ remove_delimiters = lambda s: s[1:-1] if is_quoted(s) or is_array(s) else s
 
 #==============================================================================#
 
-def parse_file(args):
+def parse_file(args, skipped=None):
     with open(args.input, 'r') as f:
         lines = f.readlines()
     joined = reduce(lambda a, c: join_line(c, a), lines, [])
-    calls = (parse_line(line) for line in joined)
+    calls = (parse_line(line, skipped) for line in joined)
     calls = [(state.upper(), args_str) for state, args_str in calls if state]
     return [(state, parse_call(args_str)) for state, args_str in calls]
 
@@ -81,13 +84,30 @@ def parse_call(args):
 #==============================================================================#
 
 def make_output(state, records):
-    cols = list(unique(chain.from_iterable(map(lambda d: d.keys(), records))))
+    key_filter = lambda k: k != 'RC'
+    cols = list(filter(key_filter, unique(chain.from_iterable(map(lambda d: d.keys(), records)))))
+    rows = list(map(lambda r: map(lambda c: r.get(c, ''), cols), records))
+#    cwidths = column_widths([cols] + rows)
+    llen = max_row_len([cols]+rows, RS)
 #    fields_width = max((sum(len(f) for f in r) for r in (records+cols)))
 #    divider = make_divider(DASH, (len(cols)-1)*len(RS)+fields_width)
-    divider = make_divider(DASH, 79)
+    divider = make_divider(DASH, llen-1)
     header = [f'category: {state}', divider, RS.join(list(cols)), divider]
     rows = [RS.join(list(v)) for v in ((r.get(c, '') for c in cols) for r in records)]
     return header + rows + ['']
+
+#==============================================================================#
+
+def column_widths(table):
+    return tuple(max(len(r) for r in col) for col in zip(table))
+
+#    return tuple(map(lambda c: max(tuple(map(lambda f: len(f), c))), zip(table)))
+
+#==============================================================================#
+
+def max_row_len(table, delim=None):
+    delim_len = len(delim)*(len(table[0])-1) if delim else 0
+    return max(tuple(map(lambda r: sum(tuple(map(len, r))), table))) + delim_len
 
 #==============================================================================#
 
@@ -97,18 +117,25 @@ def make_parser():
     parser.add_argument("input", action='store', help="source Gridded Component filename")
     parser.add_argument("-o", "--output", action='store', help="destination specs filename")
     parser.add_argument("-c", "--component", action='store', default=None, help="component name")
+    parser.add_argument("-d", "--debug", action='store', default=None, help="debug log")
     return parser
+
+#==============================================================================#
 
 def main(args):
     """ args is dict of arguments: {input: ..., component: ..., output: ...} """
+    skipped = [] if args.debug else None
     component = args.component if args.component else Path(args.input).stem if args.input else None
-    records = parse_file(args)
+    records = parse_file(args, skipped)
     lines = ['schema_version: 2.0.0', f'component: {component}', '']
     for state in set(s for s, _ in records):
         state_records = [r for s, r in records if s == state]
         lines.extend(make_output(state, state_records))
     with open(args.output, 'w') as f:
         f.writelines(f'{line}\n' for line in lines)
+    if skipped:
+        with open(args.debug, 'w') as f:
+            f.writelines(f'{line}\n' for line in skipped)
 
 # Main
 if __name__ == '__main__':
