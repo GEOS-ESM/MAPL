a2bfd4a882fa6276237d6e458d10639787f0ea3d
Darian Boggs <william.d.boggs@nasa.gov>
2025-09-10 00:35:58 -0400

    Updated ACG writer substantially

--- Apps/MAPL_GridCompSpecs_ACG_writer.py
+++ Apps/MAPL_GridCompSpecs_ACG_writer.py
@@ -5,7 +5,10 @@ from functools import reduce, partial
 from pathlib import Path
 from itertools import filterfalse, chain
 from collections import defaultdict
+from os import linesep
 
+SUCCESS = 0
+FAILURE = SUCCESS-1
 SPACE = r' '
 BLANK = r''
 BLANK_LINE = [BLANK]
@@ -15,14 +18,15 @@ DASH = r'-'
 RS = r' | '
 SUBROUTINE_RE = re.compile(r'call\s+MAPL_Add(?P<state>\w+)Spec\s*\((?P<arguments>.*)\)\s*$', re.IGNORECASE)
 
-is_quoted = lambda s: (s[0] == "'" or s[0] == '"') and s[-1] == s[0]
+#==============================================================================#
+
+is_quoted = lambda s: s[0] in '"\''"'" and s[-1] == s[0]
 is_array = lambda s: (s[0], s[-1]) == ('[', ']')
 remove_delimiters = lambda s: s[1:-1] if is_quoted(s) or is_array(s) else s
 make_comment = lambda s: f"{COMMENT}{s}"
 make_divider = lambda c, n: make_comment(f"{c * max(0, n-1)}")
-
-def newline(n=1):
-    return  max(n, 0) *'\n'
+newline = linesep
+newlines = lambda n: max(n, 0)*newline
 
 #==============================================================================#
 
@@ -67,12 +71,6 @@ def parse_line(line, skipped=None):
 
 #==============================================================================#
 
-is_quoted = lambda s: (s[0] == "'" or s[0] == '"') and s[-1] == s[0]
-is_array = lambda s: (s[0], s[-1]) == ('[', ']')
-remove_delimiters = lambda s: s[1:-1] if is_quoted(s) or is_array(s) else s
-
-#==============================================================================#
-
 def parse_file(args, skipped=None):
     with open(args.input, 'r') as f:
         lines = f.readlines()
@@ -93,17 +91,13 @@ def parse_call(args):
 def make_output(state, records):
     key_filter = lambda k: k != 'RC'
     cols = list(filter(key_filter, unique(chain.from_iterable(map(lambda d: d.keys(), records)))))
-    rows = [cols] + [[r.get(c, BLANK) for c in cols] for r in records]
-    row_lines = [RS.join(row) for row in pad_all(rows)]
-    llen = max([len(line) for line in row_lines])
+    rows = [[r.get(c, BLANK) for c in cols] for r in records]
+    rows.insert(0, cols)
+    lines = [RS.join(row) for row in pad_all(rows)]
+    llen = max([len(line) for line in lines])
     divider = make_divider(DASH, llen-1)
-    cols_line, *row_lines = row_lines
-    return [f'category: {state}', divider, cols_line, divider, *row_lines]
-
-#==============================================================================#
-
-def check_none(table):
-    return any(map(lambda r: True if r is None else any(tuple(map(lambda c: c is None, r))), table))
+    cols_line, *lines = lines
+    return [f'category: {state}', divider, cols_line, divider, *lines]
 
 #==============================================================================#
 def pad(s, w):
@@ -129,25 +123,29 @@ def update(d, t):
     d[t[0]].append(t[1])
     return d
 
-def prepend(s, e):
-    s.insert(0, e)
-    return s
+def prepend(value, iterable):
+    "Prepend a single value in front of an iterable."
+    return chain([value], iterable)
 
 #==============================================================================#
 
 def main(args):
     """ args is dict of arguments: {input: ..., component: ..., output: ...} """
+    rc = FAILURE
     skipped = [] if args.debug else None
     component = args.component if args.component else Path(args.input).stem if args.input else None
     records = parse_file(args, skipped)
     state_records = reduce(update, records, defaultdict(list))
-    sections = [newline().join(l) for l in (make_output(*sr) for sr in sorted(state_records.items(), key=lambda t: t[0]))]
+    sections = [newline.join(l) for l in (make_output(*sr) for sr in sorted(state_records.items(), key=lambda t: t[0]))]
     with open(args.output, 'w') as f:
-        f.write(f'schema_version: 2.0.0{newline()}component: {component}{newline(2)}')
-        f.write(newline(2).join(sections))
-    if skipped:
-        with open(args.debug, 'w') as f:
-            f.writelines(f'{line}{newline()}' for line in skipped)
+        f.write(f'schema_version: 2.0.0{newline}component: {component}{newlines(2)}')
+        f.write(newlines(2).join(sections))
+    rc = SUCCESS
+    if not (args.debug and (skipped)):
+        return rc
+    with open(args.debug, 'w') as f:
+        if skipped:
+            f.writelines(f'{line}{newline}' for line in skipped)
 
 # Main
 if __name__ == '__main__':
