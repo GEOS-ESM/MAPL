99cffa0ff82f05bb3823ef3652ac2e0cc96fdeed
Darian Boggs <william.d.boggs@nasa.gov>
2025-12-01 11:33:22 -0500

    Optional parameter destroy_contents for FieldBundleDestroy

--- field_bundle/FieldBundleDestroy.F90
+++ field_bundle/FieldBundleDestroy.F90
@@ -18,20 +18,28 @@ module mapl3g_FieldBundleDestroy
 
 contains
 
-   subroutine destroy_bundle(bundle, rc)
+   subroutine destroy_bundle(bundle, unusable, destroy_contents, rc)
       type(ESMF_FieldBundle), intent(inout) :: bundle
+      class(KeywordEnforcer), optional, intent(in) :: unusable
+      logical, optional, intent(in) :: destroy_contents
       integer, optional, intent(out) :: rc
       integer :: status
       type(ESMF_Field), allocatable :: fieldList(:)
       character(len=ESMF_MAXSTR) :: name
+      logical :: destroying_contents
 
-      call remove_bundle_fields(bundle, fieldList, _RC)
-      call FieldsDestroy(fieldList, _RC)
+      destroying_contents = .FALSE.
+      if(present(destroy_contents)) destroying_contents = destroy_contents
+      if(destroying_contents) then
+         call remove_bundle_fields(bundle, fieldList, _RC)
+         call FieldsDestroy(fieldList, _RC)
+      end if
       call ESMF_FieldBundleGet(bundle, name=name, _RC)
       call ESMF_FieldBundleDestroy(bundle, _RC)
       call ESMF_FieldBundleValidate(bundle, rc=status)
       _ASSERT(status /= ESMF_SUCCESS, 'Bundle "' // trim(name) // '" was not destroyed.')
       _RETURN(_SUCCESS)
+      _UNUSED_DUMMY(unusable)
 
    end subroutine destroy_bundle
 
