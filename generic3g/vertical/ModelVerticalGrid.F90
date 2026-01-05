4a87ddfb2ee92366801c1cc7cb855e905bd282f7
Tom Clune <thomas.l.clune@nasa.gov>
2026-01-05 13:32:33 -0500

    Added constructor for ModelVerticalGridSpec
    Necessary for use with VerticalGridManager.

--- generic3g/vertical/ModelVerticalGrid.F90
+++ generic3g/vertical/ModelVerticalGrid.F90
@@ -74,6 +74,9 @@ module mapl3g_ModelVerticalGrid
       procedure :: create_grid_from_spec
    end type ModelVerticalGridFactory
 
+   interface ModelVerticalGridSpec
+      procedure new_ModelVerticalGridSpec
+   end interface ModelVerticalGridSpec
 
    interface ModelVerticalGrid
       procedure new_ModelVerticalGrid_basic
@@ -92,6 +95,18 @@ module mapl3g_ModelVerticalGrid
 
 contains
 
+   function new_ModelVerticalGridSpec(names, physical_dimensions, num_levels) result(spec)
+      type(ModelVerticalGridSpec) :: spec
+      type(StringVector), intent(in) :: names
+      type(StringVector), intent(in) :: physical_dimensions
+      integer, intent(in) ::  num_levels
+
+      spec%names = names
+      spec%physical_dimensions = physical_dimensions
+      spec%num_levels = num_levels
+
+   end function new_ModelVerticalGridSpec
+
    function new_ModelVerticalGrid_basic(physical_dimension, short_name, num_levels) result(vgrid)
       type(ModelVerticalGrid) :: vgrid
       character(*), intent(in) :: physical_dimension
@@ -102,6 +117,7 @@ contains
       call vgrid%spec%names%push_back(short_name)
       call vgrid%spec%physical_dimensions%push_back(physical_dimension)
    end function new_ModelVerticalGrid_basic
+
 
    integer function get_num_levels(this) result(num_levels)
       class(ModelVerticalGrid), intent(in) :: this
