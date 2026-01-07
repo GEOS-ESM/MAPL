3178f76eb868b50e079c5cb266558624ba2374eb
Tom Clune <thomas.l.clune@nasa.gov>
2026-01-07 13:50:14 -0500

    Simplified support for RegridderParam
    Retrieving params in FieldSetGet was causing cyclic dependencies.  So
    now we put the burden on the client code to translate to/from an info
    object.

--- generic3g/specs/GeomAspect.F90
+++ generic3g/specs/GeomAspect.F90
@@ -12,9 +12,11 @@ module mapl3g_GeomAspect
    use mapl3g_NullTransform
    use mapl3g_Field_API
    use mapl3g_FieldBundle_API
+   use mapl3g_EsmfRegridder
    use mapl_ErrorHandling
    use ESMF, only: esmf_Geom
    use ESMF, only: esmf_Field, esmf_FieldBundle, esmf_State
+   use ESMF, only: esmf_Info
    implicit none
    private
 
@@ -27,7 +29,7 @@ module mapl3g_GeomAspect
    end interface to_GeomAspect
 
    type, extends(StateItemAspect) :: GeomAspect
-!#      private
+      private
       type(ESMF_Geom), allocatable :: geom
       type(EsmfRegridderParam), allocatable :: regridder_param
       type(HorizontalDimsSpec) :: horizontal_dims_spec = HORIZONTAL_DIMS_GEOM ! none, geom
@@ -267,13 +269,20 @@ contains
       integer, optional, intent(out) :: rc
 
       integer :: status
+      type(esmf_Info), allocatable :: regridder_param_info
 
       _RETURN_UNLESS(present(field) .or. present(bundle))
 
       if (present(field)) then
-         call mapl_FieldGet(field, geom=this%geom, _RC)
+         call mapl_FieldGet(field, geom=this%geom, regridder_param_info=regridder_param_info, _RC)
       else if (present(bundle)) then
-         call mapl_FieldBundleGet(bundle, geom=this%geom, _RC)
+         call mapl_FieldBundleGet(bundle, geom=this%geom, regridder_param_info=regridder_param_info, _RC)
+      end if
+
+      if (allocated(regridder_param_info)) then
+         this%regridder_param = make_EsmfRegridderParam(regridder_param_info, _RC)
+      else
+         if (allocated(this%regridder_param)) deallocate(this%regridder_param)
       end if
 
       call this%set_mirror(.not. allocated(this%geom))
@@ -289,13 +298,23 @@ contains
       integer, optional, intent(out) :: rc
 
       integer :: status
+      type(esmf_Info), allocatable :: regridder_param_info
 
       _RETURN_UNLESS(present(field) .or. present(bundle))
 
+      if (allocated(this%regridder_param)) then
+         regridder_param_info = this%regridder_param%make_info(_RC)
+      end if
+      block
+        use esmf
+        _HERE
+      end block
       if (present(field)) then
-         call mapl_FieldSet(field, geom=this%geom, _RC)
+         _HERE
+         call mapl_FieldSet(field, geom=this%geom, regridder_param_info=regridder_param_info, _RC)
+         _HERE
       else if (present(bundle)) then
-         call mapl_FieldBundleSet(bundle, geom=this%geom, _RC)
+         call mapl_FieldBundleSet(bundle, geom=this%geom, regridder_param_info=regridder_param_info, _RC)
       end if
 
       _RETURN(_SUCCESS)
@@ -308,6 +327,7 @@ contains
       integer, optional, intent(out) :: rc
 
       _HERE, file, line, this%is_mirror(), allocated(this%geom)
+      _HERE, file, line, this%is_mirror(), allocated(this%regridder_param)
          
       
       _RETURN(_SUCCESS)
