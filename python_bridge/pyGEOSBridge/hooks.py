b6476ad1cf8044009b8f476bc5eb1ea69e9bf247
Florian Deconinck <deconinck.florian@gmail.com>
2026-02-10 11:27:55 -0500

    Fix ABC use for user code base class Add a larger bypass IEEE importer with other packages used in DSL

--- python_bridge/pyGEOSBridge/hooks.py
+++ python_bridge/pyGEOSBridge/hooks.py
@@ -2,9 +2,12 @@ import importlib
 import sys
 from pyGEOSBridge import GEOSInterfaceCode
 
-from pyGEOSBridge.types import FFI
+from pyGEOSBridge.types import FFI, CVoidPointer
 from pyGEOSBridge.memory import get_fortran_python_converter
 from pyGEOSBridge.mapl import set_MAPLPy
+
+# See file for explanation
+from pyGEOSBridge.ieee_import_bypass import *  # noqa: F403
 
 # - - - DSL Grid Comp Interfacing - - - #
 
@@ -45,19 +48,34 @@ def _get_code_object_from_package_name(c_package_name: FFI.CData) -> GEOSInterfa
     return CODE
 
 
-def named_init(c_package_name: FFI.CData, grid_comp, import_state, export_state):
+def named_init(
+    c_package_name: FFI.CData,
+    mapl_state: CVoidPointer,
+    import_state: CVoidPointer,
+    export_state: CVoidPointer,
+):
     """Python hook for Fortran's `gc_init`."""
     CODE = _get_code_object_from_package_name(c_package_name)
-    CODE.init(grid_comp, import_state, export_state)
+    CODE.init(mapl_state, import_state, export_state)
 
 
-def named_run(c_package_name: FFI.CData, grid_comp, import_state, export_state):
+def named_run(
+    c_package_name: FFI.CData,
+    mapl_state: CVoidPointer,
+    import_state: CVoidPointer,
+    export_state: CVoidPointer,
+):
     """Python hook for Fortran's `gc_run`."""
     CODE = _get_code_object_from_package_name(c_package_name)
-    CODE.run(grid_comp, import_state, export_state)
+    CODE.run(mapl_state, import_state, export_state)
 
 
-def named_finalize(c_package_name: FFI.CData, grid_comp, import_state, export_state):
+def named_finalize(
+    c_package_name: FFI.CData,
+    mapl_state: CVoidPointer,
+    import_state: CVoidPointer,
+    export_state: CVoidPointer,
+):
     """Python hook for Fortran's `gc_finalize`."""
     CODE = _get_code_object_from_package_name(c_package_name)
-    CODE.finalize(grid_comp, import_state, export_state)
+    CODE.finalize(mapl_state, import_state, export_state)
