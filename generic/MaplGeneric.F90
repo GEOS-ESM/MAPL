36a02cfc7ae9c99198a65a72b923b488df49f75d
Purnendu Chakraborty <purnendu.chakraborty@nasa.gov>
2026-02-25 22:18:00 -0500

    Building fewer files in generic/

--- generic/MaplGeneric.F90
+++ generic/MaplGeneric.F90
@@ -2,6 +2,13 @@ module MaplGeneric
    use mapl_AbstractComponent
    use mapl_VarSpecMiscMod
    use mapl_VarSpecVector
+<<<<<<< HEAD
+=======
+   use mapl_VarConnType
+   use mapl_VarConnVector
+   use mapl_VarConn
+   use mapl_maplgenericcomponent
+>>>>>>> 5a9b0039 (Building fewer files in generic/)
 
    implicit none
 end module MaplGeneric
