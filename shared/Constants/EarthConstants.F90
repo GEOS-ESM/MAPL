d056f173efbb06694c388ab5fec91b48bdf7bc3e
Tom Clune <thomas.l.clune@nasa.gov>
2026-03-25 16:15:35 -0400

    Introduce descriptive planet-independent constant names
    Add verbose, self-documenting names to all three physical/Earth
    constant modules.  Old names retained as DEPRECATED aliases.
    
    PhysicalConstants:
      MAPL_STEFAN_BOLTZMANN, MAPL_AVOGADRO, MAPL_UNIVERSAL_GAS_CONSTANT
    
    EarthConstants (planet-independent names, no EARTH_ prefix):
      MAPL_GRAVITY, MAPL_MEAN_DRY_SURFACE_PRESSURE, MAPL_ROTATION_RATE,
      MAPL_ECCENTRICITY, MAPL_SEMIMAJOR_AXIS, MAPL_KM_PER_DEGREE,
      MAPL_DEGREE_PER_KM
    
    EarthAtmosphericConstants:
      MAPL_MOLECULAR_WEIGHT_{DRY_AIR,WATER,OZONE},
      MAPL_GAS_CONSTANT_{DRY_AIR,WATER_VAPOR},
      MAPL_SPECIFIC_HEAT_{DRY_AIR,WATER_VAPOR}_CONST_{PRESSURE,VOLUME},
      MAPL_MOLECULAR_WEIGHT_RATIO, MAPL_CP/CV_VAPOR_OVER_DRY_AIR,
      MAPL_ADIABATIC_INDEX, MAPL_LATENT_HEAT_{VAPORIZATION,FUSION,SUBLIMATION},
      MAPL_REFERENCE_PRESSURE, MAPL_MEAN_SURFACE_PRESSURE, MAPL_FREEZING_POINT,
      MAPL_SPECIFIC_HEAT_{ICE,WATER}, MAPL_DENSITY_{LIQUID_WATER,SEAWATER,SEAICE,SNOW},
      MAPL_KINEMATIC_VISCOSITY_AIR, MAPL_VON_KARMAN, MAPL_MINIMUM_WIND_SPEED

--- shared/Constants/EarthConstants.F90
+++ shared/Constants/EarthConstants.F90
@@ -7,22 +7,33 @@ module MAPL_EarthConstants
    implicit none
 
    ! Earth geometric and orbital constants.
-   ! These are Earth-specific and would differ for other planets.
+   ! Names are planet-independent: client code using a future MAPL_MarsConstants
+   ! module will reference the same names without modification.
 
-   real(kind=REAL64), parameter :: MAPL_PSDRY                    = 98305.0_REAL64  ! mean dry surface pressure          Pa
-   real,              parameter :: MAPL_SECONDS_PER_SIDEREAL_DAY = 86164.0         ! length of Earth sidereal day        s
-   real,              parameter :: MAPL_GRAV                     = 9.80665         ! mean surface gravitational accel    m/s^2
-   real,              parameter :: MAPL_RADIUS                   = 6371.0E3        ! mean radius of Earth                m
-   real(kind=REAL64), parameter :: MAPL_OMEGA_R8                 = &
-        2.0_REAL64 * MAPL_PI_R8 / MAPL_SECONDS_PER_SIDEREAL_DAY                   ! Earth rotation rate (REAL64)        1/s
-   real(kind=REAL32), parameter :: MAPL_OMEGA                    = &
-        2.0 * MAPL_PI / MAPL_SECONDS_PER_SIDEREAL_DAY                             ! Earth rotation rate (REAL32)        1/s
-   real(kind=REAL64), parameter :: MAPL_EARTH_ECCENTRICITY       = &
-        8.181919084262200d-2                                                        ! WGS84 first eccentricity            --
-   real(kind=REAL64), parameter :: MAPL_EARTH_SEMIMAJOR_AXIS     = 6378137.0_REAL64 ! WGS84 semi-major axis              m
-   real(kind=REAL64), parameter :: MAPL_KM_PER_DEG               = &
-        (1.0_REAL64 / (MAPL_RADIUS / 1000.0_REAL64)) * MAPL_RADIANS_TO_DEGREES    ! km per degree of arc                km/deg
-   real(kind=REAL64), parameter :: MAPL_DEG_PER_KM               = &
-        (MAPL_RADIUS / 1000.0_REAL64) * MAPL_DEGREES_TO_RADIANS_R8                ! degrees per km of arc               deg/km
+   real(kind=REAL64), parameter :: MAPL_MEAN_DRY_SURFACE_PRESSURE = 98305.0_REAL64  ! Pa
+   real,              parameter :: MAPL_SECONDS_PER_SIDEREAL_DAY  = 86164.0          ! s
+   real,              parameter :: MAPL_GRAVITY                   = 9.80665          ! mean surface gravitational acceleration  m/s^2
+   real(kind=REAL64), parameter :: MAPL_RADIUS                    = 6371.0E3_REAL64  ! mean radius  m
+   real(kind=REAL64), parameter :: MAPL_ROTATION_RATE_R8          = &
+        2.0_REAL64 * MAPL_PI_R8 / MAPL_SECONDS_PER_SIDEREAL_DAY                     ! rotation rate (REAL64)  1/s
+   real(kind=REAL32), parameter :: MAPL_ROTATION_RATE             = &
+        real(MAPL_ROTATION_RATE_R8, REAL32)                                          ! rotation rate (REAL32)  1/s
+   real(kind=REAL64), parameter :: MAPL_ECCENTRICITY              = &
+        8.181919084262200d-2                                                          ! WGS84 first eccentricity  --
+   real(kind=REAL64), parameter :: MAPL_SEMIMAJOR_AXIS            = 6378137.0_REAL64 ! WGS84 semi-major axis  m
+   real(kind=REAL64), parameter :: MAPL_KM_PER_DEGREE             = &
+        (1.0_REAL64 / (MAPL_RADIUS / 1000.0_REAL64)) * MAPL_RADIANS_TO_DEGREES      ! km per degree of arc  km/deg
+   real(kind=REAL64), parameter :: MAPL_DEGREE_PER_KM             = &
+        (MAPL_RADIUS / 1000.0_REAL64) * MAPL_DEGREES_TO_RADIANS_R8                  ! degrees per km of arc  deg/km
+
+   ! Deprecated aliases
+   real(kind=REAL64), parameter :: MAPL_PSDRY              = MAPL_MEAN_DRY_SURFACE_PRESSURE ! DEPRECATED
+   real,              parameter :: MAPL_GRAV               = MAPL_GRAVITY                   ! DEPRECATED
+   real(kind=REAL64), parameter :: MAPL_OMEGA_R8           = MAPL_ROTATION_RATE_R8          ! DEPRECATED
+   real(kind=REAL32), parameter :: MAPL_OMEGA              = MAPL_ROTATION_RATE             ! DEPRECATED
+   real(kind=REAL64), parameter :: MAPL_EARTH_ECCENTRICITY = MAPL_ECCENTRICITY              ! DEPRECATED
+   real(kind=REAL64), parameter :: MAPL_EARTH_SEMIMAJOR_AXIS = MAPL_SEMIMAJOR_AXIS          ! DEPRECATED
+   real(kind=REAL64), parameter :: MAPL_KM_PER_DEG         = MAPL_KM_PER_DEGREE            ! DEPRECATED
+   real(kind=REAL64), parameter :: MAPL_DEG_PER_KM         = MAPL_DEGREE_PER_KM            ! DEPRECATED
 
 end module MAPL_EarthConstants
--- shared/Constants/PhysicalConstants.F90
+++ shared/Constants/PhysicalConstants.F90
@@ -4,20 +4,25 @@ module MAPL_PhysicalConstants
 
    ! Universal physical constants.  These are independent of the choice of planet.
    !
-   ! CODATA 2018 note: MAPL_AVOGAD and MAPL_RUNIV became exact (defined) values in
-   ! the 2019 SI redefinition.  MAPL_STFBOL is derivable from other defined constants
-   ! (sigma = 2 pi^5 k^4 / 15 h^3 c^2) and is thus also exact under CODATA 2018.
+   ! CODATA 2018 note: MAPL_AVOGADRO and MAPL_UNIVERSAL_GAS_CONSTANT became exact
+   ! (defined) values in the 2019 SI redefinition.  MAPL_STEFAN_BOLTZMANN is
+   ! derivable from other defined constants (sigma = 2 pi^5 k^4 / 15 h^3 c^2)
+   ! and is thus also exact under CODATA 2018.
    ! Build with -DUSE_CODATA_2018_CONSTANTS to use those more precise values.
 
 #if defined(CODATA_2018_CONSTANTS)
-   real, parameter :: MAPL_STFBOL = 5.670374419E-8  ! Stefan-Boltzmann constant  W/(m^2 K^4) [exact]
-   real, parameter :: MAPL_AVOGAD = 6.02214076E26   ! Avogadro constant           1/kmol      [exact]
-   real, parameter :: MAPL_RUNIV  = 8314.462618      ! Universal gas constant       J/(Kmole K) [exact]
+   real, parameter :: MAPL_STEFAN_BOLTZMANN      = 5.670374419E-8  ! Stefan-Boltzmann constant  W/(m^2 K^4) [exact]
+   real, parameter :: MAPL_AVOGADRO              = 6.02214076E26   ! Avogadro constant           1/kmol      [exact]
+   real, parameter :: MAPL_UNIVERSAL_GAS_CONSTANT = 8314.462618    ! Universal gas constant       J/(Kmole K) [exact]
 #else
-   real, parameter :: MAPL_STFBOL = 5.6734E-8        ! Stefan-Boltzmann constant  W/(m^2 K^4)
-   real, parameter :: MAPL_AVOGAD = 6.023E26          ! Avogadro constant           1/kmol
-   real, parameter :: MAPL_RUNIV  = 8314.47           ! Universal gas constant       J/(Kmole K)
+   real, parameter :: MAPL_STEFAN_BOLTZMANN      = 5.6734E-8       ! Stefan-Boltzmann constant  W/(m^2 K^4)
+   real, parameter :: MAPL_AVOGADRO              = 6.023E26        ! Avogadro constant           1/kmol
+   real, parameter :: MAPL_UNIVERSAL_GAS_CONSTANT = 8314.47        ! Universal gas constant       J/(Kmole K)
 #endif
 
-end module MAPL_PhysicalConstants
+   ! Deprecated aliases
+   real, parameter :: MAPL_STFBOL = MAPL_STEFAN_BOLTZMANN       ! DEPRECATED: use MAPL_STEFAN_BOLTZMANN
+   real, parameter :: MAPL_AVOGAD = MAPL_AVOGADRO               ! DEPRECATED: use MAPL_AVOGADRO
+   real, parameter :: MAPL_RUNIV  = MAPL_UNIVERSAL_GAS_CONSTANT  ! DEPRECATED: use MAPL_UNIVERSAL_GAS_CONSTANT
 
+end module MAPL_PhysicalConstants
