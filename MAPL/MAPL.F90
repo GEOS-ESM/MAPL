5a5770701b0a0c0481a75b5fa236828d21fdd741
Tom Clune <thomas.l.clune@nasa.gov>
2026-04-09 08:43:38 -0400

    Remove MAPL_SUPPORT_MAPL3 build option
    Removes the MAPL_SUPPORT_MAPL3 CMake option and all associated
    MAPL_TARGET_PREFIX variable logic (GH-4633).
    
    The dual-mount MAPL2+MAPL3 approach has been abandoned due to
    vtable mismatches and symbol ambiguity from 23+ shared source
    files with identical Fortran module names and C symbols. The
    strategy is superseded by a single-mount MAPL3-only approach.
    
    Changes:
    - Remove option() declaration and if/else blocks in top-level
      CMakeLists.txt; all previously gated subdirectories (Apps,
      generic, gridcomps, docs, benchmarks, Python, Tests) are now
      unconditionally added
    - Replace ${MAPL_TARGET_PREFIX} with literal 'MAPL' across all
      component CMakeLists.txt files
    - Remove MAPL_SUPPORT_MAPL3 guards from MAPL/CMakeLists.txt and
      base/tests/CMakeLists.txt
    - Remove #ifdef MAPL_SUPPORT_MAPL3 preprocessor guards from
      MAPL/MAPL.F90; module is now unconditionally 'module MAPL'
    - Update CHANGELOG.md

--- MAPL/MAPL.F90
+++ MAPL/MAPL.F90
@@ -1,49 +1,26 @@
 ! This module re-exports the public entities
 ! of the underlying packages.
-#ifdef MAPL_SUPPORT_MAPL3
-module MAPL2
-#else
 module MAPL
-#endif
    use MAPLBase_mod
-#ifndef MAPL_SUPPORT_MAPL3
    use MAPL_GenericMod
    use MAPL_VarSpecMiscMod
-#endif
    use ESMF_CFIOMod
    use pFIO
-#ifndef MAPL_SUPPORT_MAPL3
    use MAPL_GridCompsMod
    use mapl_StubComponent
-#endif
    use MAPL_ESMFFieldBundleRead
    use MAPL_ESMFFieldBundleWrite
-#ifndef MAPL_SUPPORT_MAPL3
    use MAPL_OpenMP_Support, only : MAPL_get_current_thread => get_current_thread
    use MAPL_OpenMP_Support, only : MAPL_get_num_threads => get_num_threads
    use MAPL_OpenMP_Support, only : MAPL_find_bounds => find_bounds
    use MAPL_OpenMP_Support, only : MAPL_Interval => Interval
-#endif
    use MAPL_Profiler, initialize_profiler =>initialize, finalize_profiler =>finalize
    use MAPL_FieldUtils
    use MAPL_StateUtils
-#ifndef MAPL_SUPPORT_MAPL3
    use MAPL_PythonBridge
-#endif
    implicit none
-#ifdef MAPL_SUPPORT_MAPL3
-end module MAPL2
-#else
 end module MAPL
-#endif
 
-#ifdef MAPL_SUPPORT_MAPL3
-module MAPL2_Mod
-   use MAPL2
-end module MAPL2_Mod
-#else
 module MAPL_Mod
    use MAPL
 end module MAPL_Mod
-#endif
-   
