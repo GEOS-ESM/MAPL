c9159b37b4b139917d4a6e4d767e31035561957b
Matt Thompson <matthew.thompson@nasa.gov>
2026-04-20 12:59:53 -0400

    Update shared/MAPL_LoadBalance.F90
    Co-authored-by: Tom Clune <thomas.l.clune@nasa.gov>

--- shared/MAPL_LoadBalance.F90
+++ shared/MAPL_LoadBalance.F90
@@ -216,9 +216,8 @@ contains
     logical :: SEND, RECV
     integer, pointer :: NOP(:,:)
 
-    if (Idim == 0) then
-       Jdim = 1
-    else
+    Jdim = 1
+    if (Idim /= 0) then
        Jdim = size(A)/Idim
     end if
 
