5c6fe74b5da2b805dd0a01a24787653c56ac7f2b
Tom Clune <thomas.l.clune@nasa.gov>
2026-04-22 08:38:41 -0400

    Refactor DynamicMask into R4/R8 sub-types
    Replace DynamicMaskSpec with DynamicMask_R4 and
    DynamicMask_R8, each holding its own sentinel values
    and ESMF_DynamicMask.  DynamicMask holds allocatable
    instances of each sub-type; allocation indicates which
    precision is active.  Eliminates cross-precision slots
    and allocatable-optional guard complexity.

--- regridder_mgr/DynamicMask.F90
+++ regridder_mgr/DynamicMask.F90
@@ -11,33 +11,41 @@ module mapl3g_DynamicMask
 
 
    public :: DynamicMask
+   public :: DynamicMask_R4
+   public :: DynamicMask_R8
 
    public :: operator(==)
    public :: operator(/=)
 
-   type :: DynamicMaskSpec
-      character(:), allocatable :: mask_type
-      logical :: handleAllElements = .false.
-      real(kind=ESMF_KIND_R4), allocatable :: src_mask_value_r4
-      real(kind=ESMF_KIND_R4), allocatable :: dst_mask_value_r4
-      real(kind=ESMF_KIND_R8), allocatable :: src_mask_value_r8
-      real(kind=ESMF_KIND_R8), allocatable :: dst_mask_value_r8
-   end type DynamicMaskSpec
+   type :: DynamicMask_R4
+      real(kind=ESMF_KIND_R4) :: src_mask_value
+      real(kind=ESMF_KIND_R4), allocatable :: dst_mask_value
+      type(ESMF_DynamicMask), allocatable :: esmf_mask
+   end type DynamicMask_R4
+
+   type :: DynamicMask_R8
+      real(kind=ESMF_KIND_R8) :: src_mask_value
+      real(kind=ESMF_KIND_R8), allocatable :: dst_mask_value
+      type(ESMF_DynamicMask), allocatable :: esmf_mask
+   end type DynamicMask_R8
 
    type DynamicMask
-      type(DynamicMaskSpec) :: spec
-      type(ESMF_DynamicMask), allocatable :: esmf_mask_r4
-      type(ESMF_DynamicMask), allocatable :: esmf_mask_r8
+      character(:), allocatable :: mask_type
+      logical :: handleAllElements = .false.
+      type(DynamicMask_R4), allocatable :: mask_r4
+      type(DynamicMask_R8), allocatable :: mask_r8
    end type DynamicMask
 
    interface operator(==)
       procedure :: equal_to
-      procedure :: equal_to_spec
+      procedure :: equal_to_r4
+      procedure :: equal_to_r8
    end interface operator(==)
 
    interface operator(/=)
       procedure :: not_equal_to
-      procedure :: not_equal_to_spec
+      procedure :: not_equal_to_r4
+      procedure :: not_equal_to_r8
    end interface operator(/=)
 
    interface match
@@ -48,7 +56,6 @@ module mapl3g_DynamicMask
    interface DynamicMask
       procedure :: new_DynamicMask_r4
       procedure :: new_DynamicMask_r8
-      procedure :: new_DynamicMask_r4r8
    end interface DynamicMask
 
    abstract interface
@@ -81,21 +88,22 @@ contains
       integer, optional, intent(out) :: rc
 
       integer :: status
-      type(DynamicMaskSpec) :: spec
+      procedure(I_r4r8r4), pointer :: mask_routine_r4
 
-      spec%mask_type = mask_type
-      if (present(handleAllElements)) spec%handleAllElements = handleAllElements
+      mask%mask_type = mask_type
+      if (present(handleAllElements)) mask%handleAllElements = handleAllElements
 
-      spec%src_mask_value_r4 = src_mask_value
-      spec%src_mask_value_r8 = src_mask_value
+      allocate(mask%mask_r4)
+      mask%mask_r4%src_mask_value = src_mask_value
+      if (present(dst_mask_value)) mask%mask_r4%dst_mask_value = dst_mask_value
 
-      ! No default for dst_mask_value.  Usually left unallocated
-      if (present(dst_mask_value)) then
-         spec%dst_mask_value_r4 = dst_mask_value
-         spec%dst_mask_value_r8 = dst_mask_value
-      end if
-
-      mask = DynamicMask(spec, _RC)
+      allocate(mask%mask_r4%esmf_mask)
+      mask_routine_r4 => get_mask_routine_r4(mask_type, _RC)
+      call ESMF_DynamicMaskSetR4R8R4V(mask%mask_r4%esmf_mask, mask_routine_r4, &
+           dynamicSrcMaskValue=mask%mask_r4%src_mask_value, &
+           dynamicDstMaskValue=mask%mask_r4%dst_mask_value, &
+           handleAllElements=mask%handleAllElements, &
+           _RC)
 
       _RETURN(_SUCCESS)
    end function new_DynamicMask_r4
@@ -103,57 +111,31 @@ contains
    function new_DynamicMask_r8(mask_type, src_mask_value, dst_mask_value, handleAllElements, rc) result(mask)
       type(DynamicMask) :: mask
       character(*), intent(in) :: mask_type
-      real(kind=ESMF_KIND_R8), optional, intent(in) :: src_mask_value
+      real(kind=ESMF_KIND_R8), intent(in) :: src_mask_value
       real(kind=ESMF_KIND_R8), optional, intent(in) :: dst_mask_value
       logical, optional :: handleAllElements
       integer, optional, intent(out) :: rc
 
       integer :: status
-      type(DynamicMaskSpec) :: spec
+      procedure(I_r8r8r8), pointer :: mask_routine_r8
 
-      spec%mask_type = mask_type
-      if (present(handleAllElements)) spec%handleAllElements = handleAllElements
+      mask%mask_type = mask_type
+      if (present(handleAllElements)) mask%handleAllElements = handleAllElements
 
-      if (present(src_mask_value)) spec%src_mask_value_r8 = src_mask_value
+      allocate(mask%mask_r8)
+      mask%mask_r8%src_mask_value = src_mask_value
+      if (present(dst_mask_value)) mask%mask_r8%dst_mask_value = dst_mask_value
 
-      ! No default for dst_mask_value.  Usually left unallocated
-      if (present(dst_mask_value)) spec%dst_mask_value_r8 = dst_mask_value
-
-      mask = DynamicMask(spec, _RC)
+      allocate(mask%mask_r8%esmf_mask)
+      mask_routine_r8 => get_mask_routine_r8(mask_type, _RC)
+      call ESMF_DynamicMaskSetR8R8R8V(mask%mask_r8%esmf_mask, mask_routine_r8, &
+           dynamicSrcMaskValue=mask%mask_r8%src_mask_value, &
+           dynamicDstMaskValue=mask%mask_r8%dst_mask_value, &
+           handleAllElements=mask%handleAllElements, &
+           _RC)
 
       _RETURN(_SUCCESS)
    end function new_DynamicMask_r8
-
-   function new_DynamicMask_r4r8(spec, rc) result(mask)
-      type(DynamicMask) :: mask
-      type(DynamicMaskSpec), intent(in) :: spec
-      integer, optional, intent(out) :: rc
-
-      integer :: status
-
-      procedure(I_r4r8r4), pointer :: mask_routine_r4
-      procedure(I_r8r8r8), pointer :: mask_routine_r8
-
-      mask%spec = spec
-
-      allocate(mask%esmf_mask_r4)
-      mask_routine_r4 => get_mask_routine_r4(spec%mask_type, _RC)
-      call ESMF_DynamicMaskSetR4R8R4V(mask%esmf_mask_r4, mask_routine_r4, &
-           dynamicSrcMaskValue=spec%src_mask_value_r4, &
-           dynamicDstMaskValue=spec%dst_mask_value_r4, &
-           handleAllElements=spec%handleAllElements, &
-           _RC)
-
-      allocate(mask%esmf_mask_r8)
-      mask_routine_r8 => get_mask_routine_r8(spec%mask_type, _RC)
-      call ESMF_DynamicMaskSetR8R8R8V(mask%esmf_mask_r8, mask_routine_r8, &
-           dynamicSrcMaskValue=spec%src_mask_value_r8, &
-           dynamicDstMaskValue=spec%dst_mask_value_r8, &
-           handleAllElements=spec%handleAllElements, &
-           _RC)
-
-      _RETURN(_SUCCESS)
-   end function new_DynamicMask_r4r8
 
    function get_mask_routine_r4(mask_type, rc) result(mask_routine)
       procedure(I_r4r8r4), pointer :: mask_routine
@@ -456,10 +438,14 @@ contains
       integer,                       intent(out)          :: rc
 
       integer :: i, j, k, n
-      real(ESMF_KIND_R4) :: mask_value
+      real(ESMF_KIND_R4) :: dst_fill
       real(ESMF_KIND_R4), allocatable  :: renorm(:)
 
-      if (present(dynamicSrcMaskValue)) mask_value = dynamicSrcMaskValue
+      if (present(dynamicDstMaskValue)) then
+         dst_fill = dynamicDstMaskValue
+      else if (present(dynamicSrcMaskValue)) then
+         dst_fill = dynamicSrcMaskValue
+      end if
 
       if (associated(dynamicMaskList)) then
          n = size(dynamicMaskList(1)%srcElement(1)%ptr)
@@ -481,7 +467,7 @@ contains
             end do
             where (renorm > 0.d0)
             elsewhere
-               dynamicMaskList(i)%dstElement = mask_value
+               dynamicMaskList(i)%dstElement = dst_fill
             end where
          enddo
       endif
@@ -556,8 +542,33 @@ contains
       type(DynamicMask), intent(in) :: a
       type(DynamicMask), intent(in) :: b
 
-      equal_to = a%spec == b%spec
+      equal_to = allocated(a%mask_type) .eqv. allocated(b%mask_type)
       if (.not. equal_to) return
+
+      if (.not. allocated(a%mask_type)) then
+         equal_to = .true.
+         return
+      end if
+
+      equal_to = a%mask_type == b%mask_type
+      if (.not. equal_to) return
+
+      equal_to = a%handleAllElements .eqv. b%handleAllElements
+      if (.not. equal_to) return
+
+      equal_to = allocated(a%mask_r4) .eqv. allocated(b%mask_r4)
+      if (.not. equal_to) return
+      if (allocated(a%mask_r4)) then
+         equal_to = a%mask_r4 == b%mask_r4
+         if (.not. equal_to) return
+      end if
+
+      equal_to = allocated(a%mask_r8) .eqv. allocated(b%mask_r8)
+      if (.not. equal_to) return
+      if (allocated(a%mask_r8)) then
+         equal_to = a%mask_r8 == b%mask_r8
+      end if
+
    end function equal_to
 
    impure elemental logical function not_equal_to(a, b)
@@ -567,60 +578,47 @@ contains
       not_equal_to = .not. (a == b)
    end function not_equal_to
 
-   logical function equal_to_spec(a, b) result(equal_to)
-      type(DynamicMaskSpec), intent(in) :: a
-      type(DynamicMaskSpec), intent(in) :: b
+   logical function equal_to_r4(a, b) result(equal_to)
+      type(DynamicMask_R4), intent(in) :: a
+      type(DynamicMask_R4), intent(in) :: b
 
-      equal_to = allocated(a%mask_type) .eqv. allocated(b%mask_type)
+      equal_to = a%src_mask_value == b%src_mask_value
       if (.not. equal_to) return
 
-      if (.not. allocated(a%mask_type)) then
-         equal_to = .true. ! uninit
-         return
+      equal_to = allocated(a%dst_mask_value) .eqv. allocated(b%dst_mask_value)
+      if (.not. equal_to) return
+      if (allocated(a%dst_mask_value)) then
+         equal_to = a%dst_mask_value == b%dst_mask_value
       end if
+   end function equal_to_r4
 
-      equal_to = a%mask_type == b%mask_type
-      if (.not. equal_to) return
-
-      ! src_mask_value_r4/r8 are allocatable and may be unset when only one
-      ! precision was supplied to the constructor.  Check allocation status
-      ! before dereferencing.
-      equal_to = allocated(a%src_mask_value_r4) .eqv. allocated(b%src_mask_value_r4)
-      if (.not. equal_to) return
-      if (allocated(a%src_mask_value_r4)) then
-         equal_to = a%src_mask_value_r4 == b%src_mask_value_r4
-         if (.not. equal_to) return
-      end if
-
-      equal_to = allocated(a%dst_mask_value_r4) .eqv. allocated(b%dst_mask_value_r4)
-      if (.not. equal_to) return
-
-      if (allocated(a%dst_mask_value_r4)) then
-         equal_to = a%dst_mask_value_r4 == b%dst_mask_value_r4
-      end if
-      if (.not. equal_to) return
-
-      equal_to = allocated(a%src_mask_value_r8) .eqv. allocated(b%src_mask_value_r8)
-      if (.not. equal_to) return
-      if (allocated(a%src_mask_value_r8)) then
-         equal_to = a%src_mask_value_r8 == b%src_mask_value_r8
-         if (.not. equal_to) return
-      end if
-
-      equal_to = allocated(a%dst_mask_value_r8) .eqv. allocated(b%dst_mask_value_r8)
-      if (.not. equal_to) return
-
-      if (allocated(a%dst_mask_value_r8)) then
-         equal_to = a%dst_mask_value_r8 == b%dst_mask_value_r8
-      end if
-   end function equal_to_spec
-
-   logical function not_equal_to_spec(a, b) result(not_equal_to)
-      type(DynamicMaskSpec), intent(in) :: a
-      type(DynamicMaskSpec), intent(in) :: b
+   logical function not_equal_to_r4(a, b) result(not_equal_to)
+      type(DynamicMask_R4), intent(in) :: a
+      type(DynamicMask_R4), intent(in) :: b
 
       not_equal_to = .not. (a == b)
-   end function not_equal_to_spec
+   end function not_equal_to_r4
+
+   logical function equal_to_r8(a, b) result(equal_to)
+      type(DynamicMask_R8), intent(in) :: a
+      type(DynamicMask_R8), intent(in) :: b
+
+      equal_to = a%src_mask_value == b%src_mask_value
+      if (.not. equal_to) return
+
+      equal_to = allocated(a%dst_mask_value) .eqv. allocated(b%dst_mask_value)
+      if (.not. equal_to) return
+      if (allocated(a%dst_mask_value)) then
+         equal_to = a%dst_mask_value == b%dst_mask_value
+      end if
+   end function equal_to_r8
+
+   logical function not_equal_to_r8(a, b) result(not_equal_to)
+      type(DynamicMask_R8), intent(in) :: a
+      type(DynamicMask_R8), intent(in) :: b
+
+      not_equal_to = .not. (a == b)
+   end function not_equal_to_r8
 
    logical function match_r4(missing, b)
       real(kind=ESMF_KIND_R4), intent(in), optional :: missing
