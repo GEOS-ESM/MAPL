5280c57fe7dd507b0dc29d32ad368ad7bdb0172c
Benjamin Auer <benjamin.m.auer@nasa.gov>
2026-04-27 16:20:03 -0400

    remove commented out code

--- gridcomps/StatisticsGridComp/StatisticsGridComp.F90
+++ gridcomps/StatisticsGridComp/StatisticsGridComp.F90
@@ -359,40 +359,6 @@ contains
       _UNUSED_DUMMY(exportState)
    end subroutine custom_write_restart
 
-   !function get_checkpoint_subdir(hconfig, currTime, rc) result(subdir)
-      !character(:), allocatable :: subdir
-      !type(esmf_HConfig), intent(in) :: hconfig
-      !type(esmf_Time), intent(in) :: currTime
-      !integer, optional, intent(out) :: rc
-
-      !integer :: status
-      !character(ESMF_MAXSTR) :: iso_time
-      !logical :: has_checkpointing, has_path
-      !character(:), allocatable :: checkpoint_dir
-      !character(:), allocatable :: timestamp_dir
-      !type(esmf_HConfig) :: checkpointing_cfg
-
-      !subdir = ''
-
-      !call esmf_TimeGet(currTime, timeStringISOFrac=iso_time, _RC)
-      !timestamp_dir = trim(iso_time)
-
-      !checkpoint_dir = 'checkpoint'
-      !has_checkpointing = esmf_HConfigIsDefined(hconfig, keystring='checkpointing', _RC)
-      !if (has_checkpointing) then
-         !checkpointing_cfg = esmf_HConfigCreateAt(hconfig, keystring='checkpointing', _RC)
-         !has_path = esmf_HConfigIsDefined(checkpointing_cfg, keystring='path', _RC)
-         !if (has_path) then
-            !checkpoint_dir = esmf_HConfigAsString(checkpointing_cfg, keystring='path', _RC)
-         !end if
-         !call esmf_HConfigDestroy(checkpointing_cfg, _RC)
-      !end if
-
-      !subdir = mapl_PathJoin(checkpoint_dir, iso_time)
-
-      !_RETURN(_SUCCESS)
-   !end function get_checkpoint_subdir
-
 end module mapl3g_StatisticsGridComp
 
 subroutine setServices(gridComp, rc)
