0c5d767df900f1a9570ae35b0c8b9341b5df621a
Darian Boggs <william.d.boggs@nasa.gov>
2026-04-27 14:00:20 -0400

    Add repeatCount.

--- gridcomps/cap3g/Cap.F90
+++ gridcomps/cap3g/Cap.F90
@@ -19,6 +19,7 @@ module mapl3g_Cap
    character(len=*), parameter :: KEY_RESTART = 'restart'
    character(len=*), parameter :: KEY_CLOCK = 'clock'
    character(len=*), parameter :: KEY_CURRTIME = 'currTime'
+   character(len=*), parameter :: KEY_REPEATCOUNT = 'repeatCount'
 
    type CheckpointOptions
       logical :: is_enabled = .false.
@@ -59,7 +60,7 @@ contains
       call mapl_DriverInitializePhases(driver, phases=GENERIC_INIT_PHASE_SEQUENCE, _RC)
       call integrate(driver, hconfig, options%checkpointing, options%lgr, _RC)
       call driver%finalize(_RC)
-      call update_restart_currTime(hconfig, clock, _RC)
+      call update_restart(hconfig, clock, _RC)
 
       _RETURN(_SUCCESS)
       _UNUSED_DUMMY(unusable)
@@ -598,32 +599,32 @@ contains
       _RETURN(_SUCCESS)
    end subroutine make_symlink
 
-   subroutine update_restart_currTime(hconfig, clock, rc)
+   subroutine update_restart(hconfig, clock, rc)
       type(ESMF_HConfig), intent(in) :: hconfig
       type(ESMF_Clock), intent(inout) :: clock
       integer, optional, intent(out) :: rc
       integer :: status
-      character(len=:), allocatable :: currTimeString
       type(ESMF_Time) :: currTime
+      integer(kind=ESMF_KIND_I8) :: repeatCount
       logical :: restart_is_defined
       character(:), allocatable :: cap_restart_file
       type(ESMF_HConfig) :: restart_cfg
-      integer, parameter :: ISOSTRING_LENGTH=20
+      integer, parameter :: ISOSTRING_LENGTH = 20
       character(len=ISOSTRING_LENGTH) :: timeString
 
-
-      call ESMF_ClockGet(clock, currTime=currTime, _RC)
+      call ESMF_ClockGet(clock, currTime=currTime, repeatCount=repeatCount, _RC)
       call ESMF_TimeGet(currTime, timeString=timeString, _RC)
-      currTimeString = trim(timeString)
       restart_is_defined = ESMF_HConfigIsDefined(hconfig, keyString=KEY_RESTART, _RC)
       _ASSERT(restart_is_defined, 'Unable to get restart filename')
       cap_restart_file = ESMF_HConfigAsString(hconfig, keyString=KEY_RESTART, _RC)
-      restart_cfg = ESMF_HConfigCreate(filename=cap_restart_file, _RC)
-      call ESMF_HConfigSet(restart_cfg, content=timeString, keyString=KEY_CURRTIME, _RC)
+!      restart_cfg = ESMF_HConfigCreate(filename=cap_restart_file, _RC)
+      restart_cfg = ESMF_HConfigCreate(_RC)
+      call ESMF_HConfigAdd(restart_cfg, content=timeString, addKeyString=KEY_CURRTIME, _RC)
+      call ESMF_HConfigAdd(restart_cfg, content=repeatCount, addKeyString=KEY_REPEATCOUNT, _RC)
       call ESMF_HConfigFileSave(restart_cfg, cap_restart_file, _RC)
       call ESMF_HConfigDestroy(restart_cfg, _RC)
       _RETURN(_SUCCESS)
 
-   end subroutine update_restart_currTime
+   end subroutine update_restart
 
 end module mapl3g_Cap
