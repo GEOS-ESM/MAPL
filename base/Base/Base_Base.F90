84933f8b48f37d0581c60efde83111a841d9ba46
Tom Clune <thomas.l.clune@nasa.gov>
2026-05-11 14:30:33 -0400

    Delete MAPL_Base module; use MAPL_Constants directly in MAPL_BaseMod
    MAPL_Base only re-exported MAPL_UNDEF from MAPL_Constants. Replace
    the use in MAPL_BaseMod with a direct use of MAPL_Constants.
    External callers in GEOSsurface_GridComp are currently inactive in CMake.
    
    Refs #4855, #4847

--- base/Base/Base_Base.F90
+++ base/Base/Base_Base.F90
@@ -15,20 +15,8 @@
 ! The module `MAPL_Base` provides a collection assorted
 ! utilities and constants used throughout the MAPL Library.
 !
-module MAPL_Base
-
-  use MAPL_Constants, only: MAPL_UNDEF
-  use MAPL_ExceptionHandling
-  use, intrinsic :: iso_fortran_env, only: REAL64
-  implicit NONE
-  private
-
-  public :: MAPL_UNDEF
-
-end module MAPL_Base
-
 module MAPL_BaseMod
-  use MAPL_Base
+  use MAPL_Constants, only: MAPL_UNDEF
   use MAPL_RangeMod, only: MAPL_Range
   use mapl_MaplGrid, only: MAPL_GridGet, MAPL_DistGridGet, MAPL_GetImsJms, MAPL_GridHasDE
   use MAPL_Constants
