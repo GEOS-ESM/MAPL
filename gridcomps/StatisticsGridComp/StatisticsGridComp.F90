adda19e4b943392e4f49d02a6874e748f547bd91
Darian Boggs <william.d.boggs@nasa.gov>
2026-05-12 12:28:03 -0400

    Add TimeAccumulate with unit tests.

--- gridcomps/StatisticsGridComp/StatisticsGridComp.F90
+++ gridcomps/StatisticsGridComp/StatisticsGridComp.F90
@@ -13,6 +13,7 @@ module mapl3g_StatisticsGridComp
    use mapl3g_TimeAverage
    use mapl3g_TimeMin
    use mapl3g_TimeMax
+   use mapl3g_TimeAccumulate
    use mapl3g_State_API
    use pflogger, only: Logger
    use mapl_OS
@@ -111,6 +112,12 @@ contains
                has_deferred_aspects=.true., _RC)
           call MAPL_GridCompAddVarSpec(gridcomp, varspec, _RC)
           call advertise_time_max_internal_fields(gridcomp, name, _RC)
+       case ('accumulate')
+          period = mapl_HConfigAsTimeInterval(hconfig, keystring='period', _RC)
+          varspec = make_VariableSpec(ESMF_STATEINTENT_EXPORT, name, &
+               has_deferred_aspects=.true., _RC)
+          call MAPL_GridCompAddVarSpec(gridcomp, varspec, _RC)
+          call advertise_time_accumulate_internal_fields(gridcomp, name, _RC)
        case default
           _FAIL('unsupported action: '//action)
       end select
@@ -201,6 +208,9 @@ contains
           case ('max')
              deallocate(stat) ! gfortran workaround
              stat = make_max_stat(name, iter, alarm, _RC)
+          case ('accumulate')
+             deallocate(stat) ! gfortran workaround
+             stat = make_accumulate_stat(name, iter, alarm, _RC)
           case default
              _FAIL('unsupported statistics class: '//action)
           end select
@@ -262,6 +272,25 @@ contains
            _RETURN(_SUCCESS)
         end function make_max_stat
 
+
+       function make_accumulate_stat(name, iter, alarm, rc) result(accum_stat)
+           type(TimeAccumulate) :: accum_stat
+           character(*), intent(in) :: name
+           type(esmf_HConfigIter), intent(in) :: iter
+           type(SimpleAlarm), intent(in) :: alarm
+           integer, optional, intent(out) :: rc
+
+           integer :: status
+           type(esmf_Field) :: f_in, f_out
+
+           call esmf_StateGet(importState, itemName=name, field=f_in, _RC)
+           call esmf_StateGet(exportState, itemName=name, field=f_out, _RC)
+
+           accum_stat = TimeAccumulate(gridcomp=gridcomp, f=f_in, accum_f=f_out, alarm=alarm, _RC)
+
+           _RETURN(_SUCCESS)
+           _UNUSED_DUMMY(iter)
+        end function make_accumulate_stat
 
        function make_alarm(clock, iter, rc) result(alarm)
            type(SimpleAlarm) :: alarm
