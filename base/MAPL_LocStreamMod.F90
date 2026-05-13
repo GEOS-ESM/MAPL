71ebb9dba3bd91bb3e767f6f9d097a119c0e9d75
Tom Clune <thomas.l.clune@nasa.gov>
2026-05-13 11:51:23 -0400

    Remove base/ thin wrappers for FileIOSharedMod, NCIOMod, MAPL_LocStreamMod; move c_mapl_locstream_F.c to base3g/ (closes #4874)

--- base/MAPL_LocStreamMod.F90
+++
@@ -1,7 +0,0 @@
-! Thin compatibility wrapper: MAPL_LocStreamMod has moved to base3g/.
-! This wrapper re-exports all public symbols under the original name.
-! It will be removed in a future PR.
-module MAPL_LocStreamMod
-   use mapl_LocStreamMod_impl
-   implicit none
-end module MAPL_LocStreamMod
--- base/NCIO.F90
+++
@@ -1,7 +0,0 @@
-! Thin compatibility wrapper: NCIOMod has moved to base3g/.
-! This wrapper re-exports all public symbols under the original name.
-! It will be removed once all callers migrate to 'use MAPL'.
-module NCIOMod
-   use mapl_NCIO
-   implicit none
-end module NCIOMod
