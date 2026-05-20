55a3ce6039c04415959a8f8c7083aed78968fe3c
Thomas L.  Clune <tclune@gs6101-bucy.gsfc.nasa.gov>
2026-05-20 15:16:21 -0400

    Move mapl3g_EsmfUtils_API to its own file to fix CMake module stamp issue
    Having two modules in infrastructure/esmf/API.F90 confused CMake's Fortran
    module dependency scanner, causing it to fail copying comms/API.F90's module.
    Split into separate EsmfUtils_API.F90 placed after UngriddedDims.F90 in srcs.

--- infrastructure/esmf/API.F90
+++ infrastructure/esmf/API.F90
@@ -1,15 +1,3 @@
 module mapl3g_VM_API
    use mapl3g_vm
 end module mapl3g_VM_API
-
-module mapl3g_EsmfUtils_API
-   use mapl3g_UngriddedDim, only: UngriddedDim, make_UngriddedDim => make_ungriddedDim
-   use mapl3g_UngriddedDims, only: UngriddedDims
-
-   implicit none(type,external)
-   private
-
-   public :: UngriddedDim, make_UngriddedDim
-   public :: UngriddedDims
-
-end module mapl3g_EsmfUtils_API
