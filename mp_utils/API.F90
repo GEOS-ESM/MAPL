a37b75065e7669c493473089a59eafcb81507bdd
Tom Clune <thomas.l.clune@nasa.gov>
2026-05-22 12:54:40 -0400

    Re-export PackedDateCreate, PackedTimeCreate, and StrTemplate from MAPL umbrella (#4964)
    * Re-export PackedDateCreate, PackedTimeCreate, StrTemplate from MAPL umbrella (fixes #4963)
    
    Add mapl_PackedTimeMod and MAPL_StringTemplate to the mapl_mp_utils
    API so that PackedDateCreate, PackedTimeCreate, PackedDateTimeCreate,
    and StrTemplate are accessible via USE MAPL, eliminating the need for
    client code to reference internal MAPL submodules directly.
    
    * Add CHANGELOG entry for #4963

--- mp_utils/API.F90
+++ mp_utils/API.F90
@@ -7,6 +7,8 @@ module mapl_mp_utils
    use mapl_MemInfo, only: MAPL_MemInfoWrite => MemInfoWrite
    use mapl_TimeUtilities, only: MAPL_PackTime => PackTime, MAPL_UnpackTime => UnpackTime
    use mapl_OSUtilities, only: MAPL_GetCheckpointSubdir => get_checkpoint_subdir
+   use MAPL_PackedTimeMod, only: PackedDateCreate, PackedTimeCreate, PackedDateTimeCreate
+   use MAPL_StringTemplate, only: StrTemplate
 
    ! We use default PUBLIC to avoid explicitly listing exports from
    ! the other layers.  When the dust settles and such micro
