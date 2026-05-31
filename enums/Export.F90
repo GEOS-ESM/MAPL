68a0e3b3ff9f20866bbe7d822c7eb73a124e7fb1
Tom Clune <thomas.l.clune@nasa.gov>
2026-05-31 17:21:55 -0400

    Add MAPL_FieldBundleType_Flag and MAPL_VectorBasisKind to Enums export

--- enums/Export.F90
+++ enums/Export.F90
@@ -30,6 +30,16 @@ module mapl_Enums_export
    public :: operator(==), operator(/=), operator(<), operator(>=)
 
    ! FieldBundleType_Flag
+   public :: MAPL_FieldBundleType_Flag
+   public :: MAPL_FIELDBUNDLETYPE_BASIC, MAPL_FIELDBUNDLETYPE_VECTOR
+   public :: MAPL_FIELDBUNDLETYPE_BRACKET, MAPL_FIELDBUNDLETYPE_VECTORBRACKET
+   public :: MAPL_FIELDBUNDLETYPE_SERVICE, MAPL_FIELDBUNDLETYPE_SERVICE_AGGREGATE
+   public :: MAPL_FIELDBUNDLETYPE_SERVICE_SEPARATE, MAPL_FIELDBUNDLETYPE_INVALID
+
+   ! VectorBasisKind
+   public :: MAPL_VectorBasisKind
+   public :: MAPL_VECTOR_BASIS_KIND_INVALID, MAPL_VECTOR_BASIS_KIND_GRID
+   public :: MAPL_VECTOR_BASIS_KIND_NS
 
    ! VerticalStaggerLoc
 
