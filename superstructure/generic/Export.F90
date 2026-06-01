66e5ea89b7f63747c43d36ca0806e8fceb78f78e
Tom Clune <thomas.l.clune@nasa.gov>
2026-06-01 19:28:03 -0400

    Publishing proper mapl names.
    Some client code is using rename to attach mapl suffix. Cant skip that
    rename until MAPL publishes the new name.

--- superstructure/generic/Export.F90
+++ superstructure/generic/Export.F90
@@ -3,12 +3,19 @@
 module mapl_generic_export
 
    use mapl_UserSetServices_mod, only: user_setservices, AbstractUserSetServices, DSOSetServices
-
+   use mapl_OpenMP_Support_mod, only: mapl_find_bounds => find_bounds, mapl_get_num_threads => get_num_threads
+   use mapl_OpenMP_Support_mod, only: mapl_get_num_threads => get_num_threads
+   use mapl_OpenMP_Support_mod, only: mapl_get_current_thread => get_current_thread
+   
    implicit none
    private
 
    public :: user_setservices
    public :: AbstractUserSetServices
    public :: DSOSetServices
+
+   public :: mapl_find_bounds
+   public :: mapl_get_num_threads
+   public :: mapl_get_current_thread
 
 end module mapl_generic_export
