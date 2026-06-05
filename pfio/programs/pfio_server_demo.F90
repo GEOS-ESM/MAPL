12b85b56e5813c58d11c076101ea6e9d253e06f4
Amidu Oloso <amidu.o.oloso@nasa.gov>
2026-06-05 13:17:23 -0400

    Minor clean up to resolve conflicts

--- pfio/programs/pfio_server_demo.F90
+++ pfio/programs/pfio_server_demo.F90
@@ -120,10 +120,7 @@ end module server_demo_CLI
 !#undef I_AM_MAIN
 #include "MAPL_ErrLog.h"
 module FakeExtDataMod_server
-<<<<<<< Updated upstream
    use mapl_ErrorHandling_mod
-=======
->>>>>>> Stashed changes
    use server_demo_CLI
    use pFIO
    use gFTL2_StringVector
@@ -277,10 +274,7 @@ program main
    use pFIO
    use server_demo_CLI
    use FakeExtDataMod_server
-<<<<<<< Updated upstream
    use mapl_ErrorHandling_mod
-=======
->>>>>>> Stashed changes
    implicit none
 
    integer :: rank, npes, ierror, provided
