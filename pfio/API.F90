d24bdd39610379abb603ed2f99aed5b98b4eb440
Tom Clune <thomas.l.clune@nasa.gov>
2026-06-09 13:07:08 -0400

    No idea why this passed on laptop before.

--- pfio/API.F90
+++ pfio/API.F90
@@ -1,4 +1,9 @@
 module mapl_pfio_api
-  use pfio, only: mapl_FileMetadata => FileMetadata
+  use pfio, only: FileMetadata
   use pfio, only: i_clients
+  use pfio, only: o_clients
+  use pfio, only: NetCDF4_FileFormatter
+  use pfio, only: pfio_read
+  use pfio, only: ArrayReference
+  use pfio, only: string_in_stringvector
 end module mapl_pfio_api
