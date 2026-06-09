4b8470651f27ac6e4029e6be6ed71a058008b765
Tom Clune <thomas.l.clune@nasa.gov>
2026-06-09 14:30:15 -0400

    Missing items needed in GEOS

--- pfio/API.F90
+++ pfio/API.F90
@@ -6,4 +6,8 @@ module mapl_pfio_api
   use pfio, only: pfio_read
   use pfio, only: ArrayReference
   use pfio, only: string_in_stringvector
+
+  use pfio, only: StringVariableMap
+  use pfio, only: StringVariableMapIterator
+  use pfio, only: operator(==), operator(/=)
 end module mapl_pfio_api
