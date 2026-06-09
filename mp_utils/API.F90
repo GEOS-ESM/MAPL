5f1bf1be421623c086658498da0ff0e914b8bcc5
Benjamin Auer <benjamin.m.auer@nasa.gov>
2026-06-09 13:02:40 -0400

    split lines

--- mp_utils/API.F90
+++ mp_utils/API.F90
@@ -16,7 +16,24 @@ module mapl_mp_utils_api
                                        PackedDateCreate, PackedTimeCreate, PackedDateTimeCreate, &
                                        ESMFTimeFromPacked, UnpackDate, UnpackTime, UnpackDateTime
    use mapl_StringTemplate_mod,  only: StrTemplate, fill_grads_template, fill_grads_template_esmf
-   use mapl_Shmem_mod, only: MAPL_GetNodeInfo => GetNodeInfo, MAPL_CoresPerNodeGet => CoresPerNodeGet, MAPL_InitializeShmem => InitializeShmem, MAPL_FinalizeShmem => FinalizeShmem, MAPL_AllocNodeArray => AllocNodeArray, MAPL_DeAllocNodeArray => DeAllocNodeArray, MAPL_ShmemAmOnFirstNode => ShmemAmOnFirstNode, MAPL_SyncSharedMemory => SyncSharedMemory, MAPL_BroadcastToNodes => BroadcastToNodes, MAPL_AllocateShared => AllocateShared, GetSharedMemory, ReleaseSharedMemory, MAPL_GetNewRank => GetNewRank, MAPL_NodeComm, MAPL_NodeRootsComm, MAPL_MyNodeNum, MAPL_AmNodeRoot, MAPL_ShmInitialized
+    use mapl_Shmem_mod, only: MAPL_GetNodeInfo => GetNodeInfo
+    use mapl_Shmem_mod, only: MAPL_CoresPerNodeGet => CoresPerNodeGet
+    use mapl_Shmem_mod, only: MAPL_InitializeShmem => InitializeShmem
+    use mapl_Shmem_mod, only: MAPL_FinalizeShmem => FinalizeShmem
+    use mapl_Shmem_mod, only: MAPL_AllocNodeArray => AllocNodeArray
+    use mapl_Shmem_mod, only: MAPL_DeAllocNodeArray => DeAllocNodeArray
+    use mapl_Shmem_mod, only: MAPL_ShmemAmOnFirstNode => ShmemAmOnFirstNode
+    use mapl_Shmem_mod, only: MAPL_SyncSharedMemory => SyncSharedMemory
+    use mapl_Shmem_mod, only: MAPL_BroadcastToNodes => BroadcastToNodes
+    use mapl_Shmem_mod, only: MAPL_AllocateShared => AllocateShared
+    use mapl_Shmem_mod, only: GetSharedMemory
+    use mapl_Shmem_mod, only: ReleaseSharedMemory
+    use mapl_Shmem_mod, only: MAPL_GetNewRank => GetNewRank
+    use mapl_Shmem_mod, only: MAPL_NodeComm
+    use mapl_Shmem_mod, only: MAPL_NodeRootsComm
+    use mapl_Shmem_mod, only: MAPL_MyNodeNum
+    use mapl_Shmem_mod, only: MAPL_AmNodeRoot
+    use mapl_Shmem_mod, only: MAPL_ShmInitialized
    use mapl_LoadBalance_mod, only: MAPL_BalanceWork => BalanceWork, &
                                    MAPL_BalanceCreate => BalanceCreate, &
                                    MAPL_BalanceDestroy => BalanceDestroy, &
