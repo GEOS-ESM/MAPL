b782505c284f4aa27a43e9250158aa704e8cc9fa
Amidu O.  Oloso <aoloso@gs6101-bucy.gsfc.nasa.gov>
2026-06-23 12:43:02 -0400

    "use only" added to select modeuls to make external in API.F90

--- python/API.F90
+++ python/API.F90
@@ -1,5 +1,9 @@
 module mapl_PythonBridge_api
 
-   use mapl_PythonBridge_mod
+   use mapl_PythonBridge_mod, only: initialize_python_bridge, &
+                                    MAPL_pybridge_gcrun, &
+                                    MAPL_pybridge_gcrun_with_internal, &
+                                    MAPL_pybridge_gcinit, &
+                                    MAPL_pybridge_gcfinalize
 
 end module mapl_PythonBridge_api
