848282c2da0f51e479d56161b2762323713f8a9f
Tom Clune <thomas.l.clune@nasa.gov>
2026-07-15 08:28:05 -0400

    Fixes #5255
    Lots of work remains.

---
+++ nuopc/GenericNuopcModel.F90
@@ -0,0 +1,216 @@
+#include "MAPL.h"
+
+module mapl_GenericNuopcModel_mod
+   use :: NuopcMetaModel
+   use :: mapl_GriddedComponentDriver_mod
+   use esmf
+   use nuopc
+   use NUOPC_Model, modelSS    => SetServices
+   use :: mapl_KeywordEnforcer_mod, only: KeywordEnforcer
+   use :: mapl_ErrorHandling_mod
+   implicit none(type,external)
+   private
+
+   ! Procedures
+   public :: NuopcSetServices
+
+   interface mapl_NuopcModelCreate
+      procedure create_model
+   end interface Mapl_NuopcModelCreate
+
+contains
+
+   recursive subroutine NuopcSetServices(model, rc)
+      type(ESMF_GridComp) :: model
+      integer, intent(out) :: rc
+
+      integer :: status
+      type(NuopcMetaModel), pointer :: meta_model
+      type(GriddedComponentDriver) :: user_gc_driver
+      type(esmf_GridComp) :: user_gridcomp
+      type(DSoSetServices) :: user_SetServices
+
+      type(esmf_HConfig) :: hconfig
+      character(:), allocatable :: sharedObj, userRoutine
+
+      call NUOPC_CompDerive(model, modelSS, _RC)
+      
+      call esmf_GridCompGet(model, hconfig=hconfig, _RC)
+      sharedObj = esmf_HConfigAsString(hconfig, keystring='sharedObj', _RC)
+      userRoutine = esmf_HConfigAsString(hconfig, keystring='userRoutine', _RC)
+      user_setServices = DsoSetServices(sharedObj, userRoutine)
+      
+      user_gridcomp = ESMF_GridCompCreate(name=name, petlist=petlist, contextFlag=contextFlag, _RC)
+      call set_subclass(user_gridcomp,subclass='user_gridcomp', _RC)
+      user_gc_driver = GriddedComponentDriver(user_gridcomp)
+         
+      call attach_meta_model(model, _RC)
+      meta_model => get_meta_model(model, _RC)
+
+#ifndef __GFORTRAN__
+      meta_model = NuopcMetaModel(model, user_gc_driver, user_SetServices, hconfig)
+#else
+      ! GFortran 12 & 13 cannot directly assign to outer_meta.  But
+      ! the assignment works for an object without the POINTER
+      ! attribute.  An internal procedure is a workaround, but
+      ! ... ridiculous.
+      call ridiculous(meta_model, NuopcMetaModel(model, user_gc_driver, set_services, config))
+#endif
+      call meta_model%init(_RC)
+
+      call meta_model%setServices(_RC)
+      call set_entry_points(model, _RC)
+
+      _RETURN(ESMF_SUCCESS)
+
+   contains
+
+      subroutine set_entry_points(model, rc)
+         type(ESMF_GridComp), intent(inout) :: model
+         integer, intent(out) :: rc
+         integer :: status
+         integer :: phase_idx
+
+
+         ! Entry points
+         ! Initialize specs
+         call NUOPC_CompSpecialize(model, specLabel=label_Advertise, specRoutine=Initialize, _RC)
+         call NUOPC_CompSpecialize(model, specLabel=label_ModifyAdvertise, specRoutine=Initialize, _RC)
+         call NUOPC_CompSpecialize(model, specLabel=label_RealizeAccept, specRoutine=Initialize, _RC)
+         call NUOPC_CompSpecialize(model, specLabel=label_RealizeProvided, specRoutine=Iniitialize, _RC)
+         call NUOPC_CompSpecialize(model, specLabel=label_DataInitialize, specRoutine=Initialize, _RC)
+         
+         ! Supported initialize phases
+
+
+         ! Run specs
+         call NUOPC_CompSpecialize(model, specLabel=label_AdvanceClock, specRoutine=AdvanceClock, _RC)
+         associate (phases => outer_meta%get_phases(ESMF_METHOD_RUN))
+           do phase_idx = 1, phases%size()
+              associate(phase_label => phases%of(phase_idx)
+                call NUOPC_CompSpecialize(model, specLabel=phaseIndex, specRoutine=Advance, specPhaseLabel=phase_label, _RC)
+              end associate
+           end do
+         end associate
+
+         ! Finalize specs
+         call NUOPC_CompSpecialize(model, specLabel=label_Finalize, specRoutine=Finalize, _RC)
+
+         _RETURN(ESMF_SUCCESS)
+      end subroutine set_entry_points
+
+   contains
+
+      subroutine ridiculous(a, b)
+         type(NuopcMetaModel), intent(out) :: a
+         type(NuopcMetaModel), intent(in) :: b
+         a = b
+      end subroutine ridiculous
+#endif
+   end subroutine GenericSetServices
+
+
+   recursive subroutine Initialize(model, rc)
+      type(ESMF_GridComp) :: model
+      integer, intent(out) :: rc
+
+      integer :: status
+      type(NuopcMetaModel), pointer :: meta_model
+      integer :: phaseIndex
+      character(ESMF_MAXSTR) :: phaseLabel
+
+      meta_model => get_meta_model(model, _RC)
+      call esmf_GridCompGet(model, currentPhase=phaseIndex, _RC)      
+      call nuopc_GridCompSearchRevPhaseMap(model, ESMF_METHOD_INITIALIZE, &
+           phaseIndex, phaseLabel, _RC)
+
+      select case (trim(q => phaseLabel))
+      case (label_Advertise)
+         call meta_model%advertise(_RC)
+      case (label_ModifyAdvertise
+         call meta_model%modify_advertise(_RC)
+      case (label_RealizeAccept
+         call meta_model%realize_accept(_RC)
+      case (label_RealizeProvided
+         call meta_model%realize_provided(_RC)
+      case (label_DataInitialize)
+         call meta_model%data_initialize(_RC)
+      case default
+         _FAIL('unknown nuopc init phase:: ' // q)
+      end select
+
+      _RETURN(_SUCCESS)
+   end subroutine Initialize
+
+   recursive subroutine Advance(model, rc)
+      type(ESMF_GridComp) :: model
+      integer, intent(out) :: rc
+
+      integer :: status
+      type(NuopcMetaModel), pointer :: meta_model
+      integer :: phaseIndex
+      character(ESMF_MAXSTR) :: phaseLabel
+
+      meta_model => get_meta_model(model, _RC)
+      call esmf_GridCompGet(model, currentPhase=phaseIndex, _RC)      
+      call nuopc_GridCompSearchRevPhaseMap(model, ESMF_METHOD_INITIALIZE, &
+           phaseIndex, phaseLabel, _RC)
+
+      call meta_model%advance(trim(phaseLabel), _RC)
+
+      _RETURN(_SUCCESS)
+   end subroutine Advance
+
+   recursive subroutine AdvanceClock(model, rc)
+      type(ESMF_GridComp) :: model
+      integer, intent(out) :: rc
+
+      integer :: status
+      type(NuopcMetaModel), pointer :: meta_model
+
+      meta_model => get_meta_model(model, _RC)
+      call meta_model%advance_clock(_RC)
+
+      _RETURN(_SUCCESS)
+   end subroutine AdvanceClock
+
+   recursive subroutine Finalize(model, rc)
+      type(ESMF_GridComp) :: model
+      integer, intent(out) :: rc
+
+      integer :: status
+      type(NuopcMetaModel), pointer :: meta_model
+
+      meta_model => get_meta_model(model, _RC)
+      call meta_model%finalize(_RC)
+
+      _RETURN(_SUCCESS)
+   end subroutine Finalize
+
+
+   ! Parent components name their children, but such names should
+   ! apply to the (inner) user grid comp.  The MAPL wrapper gridcomp,
+   ! has a different name derived from that name.
+   ! "A" -->   "[A]"
+   function outer_name(inner_name)
+      character(:), allocatable :: outer_name
+      character(*), intent(in) :: inner_name
+
+      outer_name = "[" // inner_name // "]"
+   end function outer_name
+
+   subroutine set_subclass_nuopc(model, subclass, rc)
+      type(ESMF_GridComp), intent(inout) :: model
+      character(*), intent(in) :: subclass
+      integer, optional, intent(out) :: rc
+
+      integer :: status
+      type(ESMF_Info) :: info
+
+      call ESMF_InfoGetFromHost(model, info, _RC)
+      call ESMF_InfoSet(info, key='mapl/subclass', value=subclass, _RC)
+
+      _RETURN(_SUCCESS)
+   end subroutine set_subclass_nuopc
+
+end module mapl_GenericGridComp_mod
