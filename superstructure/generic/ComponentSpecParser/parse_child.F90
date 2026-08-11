f2e3f24964098aa6429587a486f71c06871b8312
Darian Boggs <william.d.boggs@nasa.gov>
2026-08-11 12:18:35 -0400

    Fix child component SetServices extraction and OuterMetaComponent initialization
    - parse_child.F90: Extract 'mapl: setservices:' from child HConfig and pass to ChildSpec
    - add_child_by_spec.F90: Attach OuterMetaComponent before calling user_setservices%run()
    
    Each child component now has its own OuterMetaComponent private state, properly
    initialized with user_gc_driver before SetServices is invoked. SetServices info
    (sharedObj, userRoutine) is extracted from child's HConfig during parsing.
    
    This fixes RC=506 errors in scenario-based tests (Test_Scenarios.pf) where
    components are created from YAML configuration files.

--- superstructure/generic/ComponentSpecParser/parse_child.F90
+++ superstructure/generic/ComponentSpecParser/parse_child.F90
@@ -11,12 +11,14 @@ contains
       integer, optional, intent(out) :: rc
 
       integer :: status
-      logical :: has_config_file
+      logical :: has_config_file, has_setservices
       type(ESMF_HConfig), allocatable :: child_hconfig
       character(:), allocatable :: config_file
       type(ESMF_TimeInterval), allocatable :: offset
       type(ESMF_TimeInterval), allocatable :: timeStep
+      class(AbstractUserSetServices), allocatable :: user_setservices
       character(len=*), parameter :: CONFIG_FILE_KEY = 'config_file'
+      character(len=*), parameter :: SETSERVICES_KEY = 'mapl'
 
       has_config_file = ESMF_HconfigIsDefined(hconfig, keyString=CONFIG_FILE_KEY, _RC)
       _ASSERT(has_config_file, 'No "config_file" key found for child.')
@@ -26,9 +28,16 @@ contains
          child_hconfig = ESMF_HConfigCreate(filename=config_file,_RC)
       end if
 
+      ! Extract SetServices from child's HConfig if present
+      has_setservices = ESMF_HConfigIsDefined(child_hconfig, keyString=SETSERVICES_KEY, _RC)
+      if (has_setservices) then
+         user_setservices = parse_setservices(child_hconfig, _RC)
+      end if
+
       call parse_timespec(hconfig, timeStep, offset, _RC)
 
-      child = ChildSpec(hconfig=child_hconfig, timeStep=timeStep, offset=offset)
+      child = ChildSpec(user_setservices=user_setservices, hconfig=child_hconfig, &
+                        timeStep=timeStep, offset=offset)
 
       _RETURN(_SUCCESS)
    end function parse_child
--- superstructure/generic/OuterMetaComponent/add_child_by_spec.F90
+++ superstructure/generic/OuterMetaComponent/add_child_by_spec.F90
@@ -30,19 +30,24 @@ contains
       class(Logger), pointer :: lgr
       character(:), allocatable :: this_name
 
-      _ASSERT(is_valid_name(child_name), 'Child name <' // child_name //'> does not conform to GEOS standards.')
-      _ASSERT(this%children%count(child_name) == 0, 'duplicate child name: <'//child_name//'>.')
+       _ASSERT(is_valid_name(child_name), 'Child name <' // child_name //'> does not conform to GEOS standards.')
+       _ASSERT(this%children%count(child_name) == 0, 'duplicate child name: <'//child_name//'>.')
 
-        total_hconfig = merge_hconfig(this%hconfig, child_spec%hconfig, _RC)
-        child_outer_gc = ESMF_GridCompCreate(name=child_name, hconfig=total_hconfig, _RC)
+         total_hconfig = merge_hconfig(this%hconfig, child_spec%hconfig, _RC)
+         child_outer_gc = ESMF_GridCompCreate(name=child_name, hconfig=total_hconfig, _RC)
 
-        ! Call setServices with child's user_setservices procedure (NUOPC pattern)
-        if (allocated(child_spec%user_setservices)) then
-           call child_spec%user_setservices%run(child_outer_gc, _RC)
-        end if
+         ! Attach OuterMetaComponent to child BEFORE calling SetServices
+         ! This ensures OuterMetaComponent private state and user_gc_driver are initialized
+         ! when user_setservices%run() is invoked
+         call attach_outer_meta(child_outer_gc, _RC)
 
-      ! Meta stuff
-      child_meta => get_outer_meta(child_outer_gc, _RC)
+         ! Call setServices with child's user_setservices procedure (NUOPC pattern)
+         if (allocated(child_spec%user_setservices)) then
+            call child_spec%user_setservices%run(child_outer_gc, _RC)
+         end if
+
+       ! Meta stuff
+       child_meta => get_outer_meta(child_outer_gc, _RC)
       call this%registry%add_subregistry(child_meta%get_registry())
 
       if (allocated(child_spec%timeStep)) child_meta%user_timeStep = child_spec%timeStep
