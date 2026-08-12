6b0bfd21d67ae06b8039c80ed2d0f95f321f67e2
Tom Clune <thomas.l.clune@nasa.gov>
2026-08-12 08:58:37 -0400

    feat(generic): add realize-provided phase

---
+++ superstructure/generic/OuterMetaComponent/initialize_realize_provided.F90
@@ -0,0 +1,32 @@
+#include "MAPL.h"
+
+submodule (mapl_OuterMetaComponent_mod) initialize_realize_provided_smod
+   use mapl_enums_api, only: MAPL_GENERIC_INIT_REALIZE_PROVIDED
+   use mapl_ErrorHandling_mod
+   implicit none(type,external)
+
+contains
+
+   module recursive subroutine initialize_realize_provided(this, importState, exportState, clock, unusable, rc)
+      class(OuterMetaComponent), target, intent(inout) :: this
+      type(ESMF_State) :: importState
+      type(ESMF_State) :: exportState
+      type(ESMF_Clock) :: clock
+      class(KE), optional, intent(in) :: unusable
+      integer, optional, intent(out) :: rc
+
+      integer :: status
+      character(*), parameter :: PHASE_NAME = 'GENERIC::INIT_REALIZE_PROVIDED'
+
+      call this%run_custom(ESMF_METHOD_INITIALIZE, PHASE_NAME, _RC)
+      call recurse(this, phase_idx=MAPL_GENERIC_INIT_REALIZE_PROVIDED, _RC)
+
+      _RETURN(_SUCCESS)
+
+      _UNUSED_DUMMY(importState)
+      _UNUSED_DUMMY(exportState)
+      _UNUSED_DUMMY(clock)
+      _UNUSED_DUMMY(unusable)
+   end subroutine initialize_realize_provided
+
+end submodule initialize_realize_provided_smod
